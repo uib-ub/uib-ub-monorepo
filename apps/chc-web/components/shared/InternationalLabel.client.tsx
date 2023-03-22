a37c475a41a7af5eb4d2ceb32bba789bdc87ad0c
Tarje Lavik <tarje.lavik@gmail.com>
2023-03-22 10:00:41 +0100

    fix(chc-web): fix intl label component

--- apps/chc-web/components/shared/InternationalLabel.client.tsx
+++ apps/chc-web/components/shared/InternationalLabel.client.tsx
@@ -35,26 +35,36 @@ export const InternationalLabel = (
     ...omit(label, [lang])
   }
 
+  if (Object.keys(label).length > 1) {
+    return (
+      <div className="my-4 ring-2 ring-offset-8 ring-blue-700 rounded-sm before:content-['InternationalLabel'] before:relative before:bg-white before:-top-[22px] before:px-2">
+        <Accordion.Root type="single" collapsible>
+          <Accordion.Item value="item-1">
+            <AccordionTrigger>
+              <span className='text-4xl'>
+                {label[lang]}
+              </span>
+            </AccordionTrigger>
+            <Accordion.Content className="overflow-hidden">
+              {rest ? (
+                <>
+                  {Object.entries(rest).map((value: any, key: any) => (
+                    <div key={key} className="py-1">{value[1]}</div>
+                  ))}
+                </>
+              ) : null}
+            </Accordion.Content>
+          </Accordion.Item>
+        </Accordion.Root>
+      </div>
+    )
+  }
+
   return (
     <div className="my-4 ring-2 ring-offset-8 ring-blue-700 rounded-sm before:content-['InternationalLabel'] before:relative before:bg-white before:-top-[22px] before:px-2">
-      <Accordion.Root type="single" collapsible>
-        <Accordion.Item value="item-1">
-          <AccordionTrigger>
-            <span className='text-4xl'>
-              {label[lang]}
-            </span>
-          </AccordionTrigger>
-          <Accordion.Content className="overflow-hidden">
-            {rest ? (
-              <>
-                {Object.entries(rest).map((value: any, key: any) => (
-                  <div key={key} className="py-1">{value[1]}</div>
-                ))}
-              </>
-            ) : null}
-          </Accordion.Content>
-        </Accordion.Item>
-      </Accordion.Root>
+      <div className='text-4xl font-bold'>
+        {label[lang] ?? Object.values(label)[0]}
+      </div>
     </div>
   );
 }
