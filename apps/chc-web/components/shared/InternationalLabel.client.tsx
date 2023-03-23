d0f2151cf92fe2baffdaea5f1075b0694f581a23
Tarje Lavik <tarje.lavik@gmail.com>
2023-03-23 09:51:20 +0100

    feat(chc-web): add i18n icon from heroicons

--- apps/chc-web/components/shared/InternationalLabel.client.tsx
+++ apps/chc-web/components/shared/InternationalLabel.client.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React from 'react';
 import * as Accordion from '@radix-ui/react-accordion';
-import { ChevronDownIcon, } from '@radix-ui/react-icons';
+import { LanguageIcon, ChevronDownIcon } from '@heroicons/react/24/solid'
 import { omit } from 'lodash'
 import { Boundary } from './Boundary';
 
@@ -16,7 +16,9 @@ const AccordionTrigger = React.forwardRef(({ children, className, ...props }: Ac
       <span className='font-bold'>
         {children}
       </span>
-      <ChevronDownIcon className="ml-3" aria-hidden />
+
+      <LanguageIcon className="ml-3 w-4 h-4" aria-hidden />
+      <ChevronDownIcon className="w-3 h-3" aria-hidden />
     </Accordion.Trigger>
   </Accordion.Header>
 ));
@@ -38,37 +40,33 @@ export const InternationalLabel = (
 
   if (Object.keys(label).length > 1) {
     return (
-      <Boundary color='cyan' labels={['InternationalLabel']} size='small'>
-        <div>
-          <Accordion.Root type="single" collapsible>
-            <Accordion.Item value="item-1">
-              <AccordionTrigger>
-                <span className='text-4xl'>
-                  {label[lang]}
-                </span>
-              </AccordionTrigger>
-              <Accordion.Content className="overflow-hidden">
-                {rest ? (
-                  <>
-                    {Object.entries(rest).map((value: any, key: any) => (
-                      <div key={key} className="py-1">{value[1]}</div>
-                    ))}
-                  </>
-                ) : null}
-              </Accordion.Content>
-            </Accordion.Item>
-          </Accordion.Root>
-        </div>
+      <Boundary color='orange' labels={['InternationalLabel (client)']} size='small'>
+        <Accordion.Root type="single" collapsible>
+          <Accordion.Item value="item-1">
+            <AccordionTrigger>
+              <span className='text-4xl'>
+                {label[lang]}
+              </span>
+            </AccordionTrigger>
+            <Accordion.Content className="overflow-hidden">
+              {rest ? (
+                <>
+                  {Object.entries(rest).map((value: any, key: any) => (
+                    <div key={key} className="py-1">{value[1]}</div>
+                  ))}
+                </>
+              ) : null}
+            </Accordion.Content>
+          </Accordion.Item>
+        </Accordion.Root>
       </Boundary>
     )
   }
 
   return (
-    <Boundary color='cyan' labels={['InternationalLabel']} size='small'>
-      <div>
-        <div className='text-4xl font-bold'>
-          {label[lang] ?? Object.values(label)[0]}
-        </div>
+    <Boundary color='orange' labels={['InternationalLabel (client)']} size='small'>
+      <div className='text-4xl font-bold'>
+        {label[lang] ?? Object.values(label)[0]}
       </div>
     </Boundary>
   );
