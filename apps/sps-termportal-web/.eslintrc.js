a3112e665f6ece95fcf6c433e1f355ef39b2954d
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-04-04 11:07:05 +0200

    fix(tp): use nuxt linting config

--- apps/sps-termportal-web/.eslintrc.js
+++ apps/sps-termportal-web/.eslintrc.js
@@ -1,10 +1,6 @@
 module.exports = {
   root: true,
   plugins: ["eslint-plugin-tsdoc"],
-  extends: ["@nuxtjs/eslint-config-typescript", "prettier"],
-  rules: {
-    "@typescript-eslint/no-unused-vars": ["off"],
-    "tsdoc/syntax": "warn",
-    "import/default": "warn",
-  },
+  extends: ["custom-nuxt"],
+  rules: {},
 };
