2e3df79bcb49b8f2273194ad58536a2f9825dd0f
Tarje Lavik <tarje.lavik@gmail.com>
2023-04-04 13:33:27 +0200

    fix(api): :bug: add working pipeling for extracting coordinates from spatial

--- apps/api/lib/request/search/pipelines/marcusDemo.ts
+++ apps/api/lib/request/search/pipelines/marcusDemo.ts
@@ -1,27 +1,22 @@
 export const marcus_demo = {
-  "id": "marcus-demo",
-  "version": 1,
-  "description": "Marcus-next pipeline",
-  "processors": [
+  id: "marcus-demo",
+  version: 1,
+  description: "Marcus-next pipeline",
+  processors: [
     {
-      "rename": {
-        "field": "title.none",
-        "target_field": "title_no",
-        "ignore_missing": true
+      script: {
+        source: `
+          if (ctx.containsKey('label') && ctx.label.containsKey('no')) {
+            ctx.label_no = ctx.label.no
+          }
+        `
       }
     },
     {
-      "rename": {
-        "field": "title.no",
-        "target_field": "title_no",
-        "ignore_missing": true
-      }
-    },
-    {
-      "rename": {
-        "field": "title.en",
-        "target_field": "title_en",
-        "ignore_missing": true
+      script: {
+        lang: "painless",
+        source: "if (ctx.containsKey('spatial') && ctx.spatial != null) { for (def i = 0; i < ctx.spatial.size(); i++) { def obj = ctx.spatial[i]; if (obj.lat != null && obj.long != null) { if (!ctx.containsKey('coordinates')) { ctx.coordinates = [] } ctx.coordinates.add([obj.lat, obj.long]) } } }",
+        ignore_failure: true
       }
     }
   ]
