e76b1cc1b97a4811a5ed17567182907b5448a608
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-04-04 10:59:38 +0200

    feat(packages): add dedicated linting config for next

---
+++ packages/eslint-config-custom-next/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ["next/core-web-vitals", "custom"],
+  rules: {
+    "@next/next/no-html-link-for-pages": "off",
+    "react/jsx-key": "off",
+  },
+  plugins: [],
+};
