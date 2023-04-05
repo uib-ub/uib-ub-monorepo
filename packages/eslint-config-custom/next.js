bf126f619ee406453b865d70c5cec35d0d844d2a
Tarje Lavik <tarje.lavik@gmail.com>
2023-04-05 11:04:26 +0200

    fix: use stricter key rule and fix error

--- packages/eslint-config-custom/next.js
+++ packages/eslint-config-custom/next.js
@@ -2,7 +2,6 @@ module.exports = {
   extends: ["next/core-web-vitals", "./base.js"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
   },
   plugins: [],
 };
