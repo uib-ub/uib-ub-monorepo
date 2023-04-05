7456cc31128c63fb9fcb97e75f3e735454f20b24
Tarje Lavik <tarje.lavik@gmail.com>
2023-04-05 11:05:43 +0200

    fix: :fire: remove unused eslint config packages

--- packages/eslint-config-custom-next/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ["next/core-web-vitals", "custom"],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
-  },
-  plugins: [],
-};
--- packages/eslint-config-custom-nuxt/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ["@nuxtjs/eslint-config-typescript", "custom"],
-  rules: {
-    "@typescript-eslint/no-unused-vars": ["off"],
-    "tsdoc/syntax": "warn",
-    "import/default": "warn",
-  },
-  plugins: ["simple-import-sort"],
-};
