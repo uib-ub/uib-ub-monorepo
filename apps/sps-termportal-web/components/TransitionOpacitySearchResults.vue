39ffe41cb05838530ad79bd0a16dca73b4176913
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-04-05 12:21:32 +0200

    feat(tp): add component for results opacity transition

---
+++ apps/sps-termportal-web/components/TransitionOpacitySearchResults.vue
@@ -0,0 +1,19 @@
+<template>
+  <Transition>
+    <slot></slot>
+  </Transition>
+</template>
+
+<style>
+.v-enter-active {
+  transition: opacity 0.5s ease;
+}
+.v-leave-active {
+  transition: opacity 0.075s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
