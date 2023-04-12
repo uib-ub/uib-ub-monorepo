7f929ea3951f8ea945e9a0bc0d6751759e5ac864
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-04-12 15:18:17 +0200

    feat(tp): add compacting and termbase query generation to api utils

---
+++ apps/sps-termportal-web/server/utils/genTermbaseQuery.ts
@@ -0,0 +1,34 @@
+export default function (namespace: string): string {
+  const runtimeConfig = useRuntimeConfig();
+  const log = {
+    type: "termbase",
+    timestamp: new Date().toJSON(),
+    termbase: namespace,
+  };
+
+  const query = `#log: ${JSON.stringify(log)}
+  
+PREFIX dct: <http://purl.org/dc/terms/>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX skosp: <http://www.data.ub.uib.no/ns/spraksamlingene/skos#>
+PREFIX vcard: <http://www.w3.org/2006/vcard/ns#>
+PREFIX base: <${runtimeConfig.public.base}>
+  
+CONSTRUCT  {
+  base:${namespace}-3A${namespace} ?p ?o.
+  ?o ?p2 ?o2.}
+WHERE {
+  GRAPH ?G {
+    {
+      base:${namespace}-3A${namespace} ?p ?o.
+      OPTIONAL {?o ?p2 ?o2.}
+      FILTER NOT EXISTS {
+        ?o a skos:Concept
+      }
+    }
+  }
+}
+`;
+  return query;
+}
