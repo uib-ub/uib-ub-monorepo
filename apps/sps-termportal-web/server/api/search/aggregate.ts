9b59094f3a2967e4ee6b0783400d13806cbc99be
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-04-14 17:20:54 +0200

    feat(tp): add api route for search aggregate data

---
+++ apps/sps-termportal-web/server/api/search/aggregate.ts
@@ -0,0 +1,25 @@
+import { genSearchQuery } from "~~/server/utils/genSearchQuery";
+
+export default defineEventHandler(async (event) => {
+  const url = useRuntimeConfig().public.endpointUrl;
+  const body = await readBody(event);
+  const query = genSearchQuery(
+    body.searchOptions,
+    "aggregate",
+    body.matching,
+    body.situation
+  );
+  const data = await $fetch(url, {
+    method: "post",
+    body: query,
+    headers: {
+      "Content-type": "application/sparql-query",
+      Referer: "termportalen.no", // TODO Referer problem
+      Accept: "application/json",
+    },
+  });
+  return data.results.bindings.reduce(
+    (o, key) => parseAggregateData(o, key),
+    {}
+  );
+});
