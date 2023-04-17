9fa83a1c6eeeab32f917e06dc649a251f0044462
Tarje Lavik <tarje.lavik@gmail.com>
2023-04-17 11:01:20 +0200

    feat(docs): add mermaid to nextra

---
+++ apps/docs/next.config.mjs
@@ -0,0 +1,14 @@
+import { remarkMermaid } from 'remark-mermaid-nextra';
+import nextra from 'nextra';
+
+const withNextra = nextra({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+  mdxOptions: {
+    remarkPlugins: [remarkMermaid],
+  }
+})
+
+export default withNextra({
+  transpilePackages: ['tailwind-ui']
+})
