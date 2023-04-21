1a3a229c6533fbe7a91eff63194771a9e3f69fa6
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-04-21 16:51:14 +0200

    fix(tp): update genSearchOptions for new matching pattern

--- apps/sps-termportal-web/composables/useGenSearchOptions.ts
+++ apps/sps-termportal-web/composables/useGenSearchOptions.ts
@@ -1,45 +1,43 @@
 export default function (situation: string, options?: Object) {
-    const searchOptions = useSearchOptions();
-    const searchFilterData = useSearchFilterData();
+  const searchOptions = useSearchOptions();
+  const searchFilterData = useSearchFilterData();
 
-    const newOptions = {
-      type: "search",
-      subtype: undefined,
-      situation,
-      // searchOptionsInterface
-      term: searchOptions.value.searchTerm || "",
-      language: [searchOptions.value.searchLanguage],
-      translate: searchOptions.value.searchTranslate,
-      termbase: [searchOptions.value.searchBase],
-      domain: searchOptions.value.searchDomain,
-      // default data
-      predicate: searchOptionsInfo.predicate.default,
-      matching: searchOptionsInfo.matching.default,
-      limit: searchOptionsInfo.limit.default,
-      offset: searchOptionsInfo.offset.default,
-    };
+  const newOptions = {
+    type: "search",
+    subtype: undefined,
+    situation,
+    // searchOptionsInterface
+    term: searchOptions.value.searchTerm || "",
+    language: [searchOptions.value.searchLanguage],
+    translate: searchOptions.value.searchTranslate,
+    termbase: [searchOptions.value.searchBase],
+    domain: searchOptions.value.searchDomain,
+    // default data
+    predicate: searchOptionsInfo.predicate.default,
+    matching: searchOptionsInfo.matching.default,
+    limit: searchOptionsInfo.limit.default,
+    offset: searchOptionsInfo.offset.default,
+  };
 
-    if (newOptions.term.length === 0) {
-      newOptions.matching = [["all"]];
-    }
-
-    if (situation === "filter" || situation === "further") {
-      console.log("filtering...");
-
-      if (searchFilterData.value.samling.length > 0) {
-        newOptions.termbase = searchFilterData.value.samling;
-      }
-      if (searchFilterData.value.lang.length > 0) {
-        newOptions.language = searchFilterData.value.lang;
-      }
-      if (searchFilterData.value.predicate.length > 0) {
-        newOptions.predicate = searchFilterData.value.predicate;
-      }
-      if (searchFilterData.value.matching.length > 0) {
-        newOptions.matching = searchFilterData.value.matching;
-      }
-    }
-    if (options) {
-      return { ...newOptions, ...options };
-    } else return newOptions;
+  if (newOptions.term.length === 0) {
+    newOptions.matching = [["all"]];
   }
+
+  if (situation === "filter" || situation === "further") {
+    if (searchFilterData.value.samling.length > 0) {
+      newOptions.termbase = searchFilterData.value.samling;
+    }
+    if (searchFilterData.value.lang.length > 0) {
+      newOptions.language = searchFilterData.value.lang;
+    }
+    if (searchFilterData.value.predicate.length > 0) {
+      newOptions.predicate = searchFilterData.value.predicate;
+    }
+    if (searchFilterData.value.matching.length > 0) {
+      newOptions.matching = searchFilterData.value.matching.map((e) => [e]);
+    }
+  }
+  if (options) {
+    return { ...newOptions, ...options };
+  } else return newOptions;
+}
