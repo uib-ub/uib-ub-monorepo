c607bfa5acee21297ef359edf132a95117577f8d
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-05-03 14:09:17 +0200

    fix(tp): redo default layout with header etc

--- apps/sps-termportal-web/layouts/default.vue
+++ apps/sps-termportal-web/layouts/default.vue
@@ -1,17 +1,20 @@
 <template>
-  <div class="flex h-screen flex-col">
-    <NavBar />
-    <div
-      class="xs:pb-4 xs:px-4 container mx-auto max-w-screen-xl flex-1 bg-white px-2 pb-2 sm:px-5 sm:pb-5 md:px-7 md:pb-7 lg:px-9 lg:pb-9 xl:px-10 xl:pb-10"
-    >
-      <h1
-        v-if="route.path === '/'"
-        class="xs:text-6xl pt-[0.2em] text-5xl font-light tracking-tight text-black/75 antialiased sm:text-7xl md:text-8xl"
-      >
-        Termportalen
-      </h1>
-      <SearchBar />
-      <slot />
+  <div class="flex min-h-screen flex-col">
+    <HeaderMain />
+    <div class="flex grow" :class="{ 'justify-center': route.path === '/' }">
+      <div
+        v-if="route.path !== '/'"
+        class="h-7 w-0 xl:w-[10vw] xl:min-w-[10em] xl:max-w-[13em]"
+      ></div>
+      <div class="xs:pb-4 container max-w-screen-xl flex-1 px-4 pt-2 xl:pl-0">
+        <h1
+          v-if="route.path === '/'"
+          class="xs:text-6xl pt-[0.2em] text-5xl font-light tracking-tight text-black/75 antialiased sm:text-7xl md:text-8xl"
+        >
+          Termportalen
+        </h1>
+        <slot />
+      </div>
     </div>
     <ButtonScrollTop />
     <FooterTP />
