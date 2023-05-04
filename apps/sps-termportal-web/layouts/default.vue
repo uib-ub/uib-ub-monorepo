cb4fda16eaa5754a5219666aebd51cafee01e66f
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-05-04 14:22:56 +0200

    fix(tp): redo def layout for landing page

--- apps/sps-termportal-web/layouts/default.vue
+++ apps/sps-termportal-web/layouts/default.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="flex min-h-screen flex-col">
     <HeaderMain />
-    <div class="flex grow" :class="{ 'justify-center': route.path === '/' }">
+    <div
+      class="flex grow"
+      :class="{ 'items-center justify-center': route.path === '/' }"
+    >
       <div
         v-if="route.path !== '/'"
-        class="h-7 w-0 xl:w-[10vw] xl:min-w-[10em] xl:max-w-[13em]"
+        class="w-0 xl:w-[10vw] xl:min-w-[10em] xl:max-w-[13em]"
       ></div>
-      <div class="xs:pb-4 container max-w-screen-xl flex-1 px-4 pt-2 xl:pl-0">
+      <div
+        class="container flex-1"
+        :class="{
+          'xs:pb-4 max-w-screen-xl pt-2  xl:pl-0': route.path !== '/',
+          'mb-[10vh] max-w-5xl p-5': route.path === '/',
+        }"
+      >
         <h1
           v-if="route.path === '/'"
-          class="xs:text-6xl pt-[0.2em] text-5xl font-light tracking-tight text-black/75 antialiased sm:text-7xl md:text-8xl"
+          class="xs:text-6xl pb-9 pt-[0.2em] text-5xl font-light tracking-tight text-black/75 antialiased sm:text-7xl md:text-8xl"
         >
           Termportalen
         </h1>
