bc4a3b0f9e6013f918e13abd86151376977b58ab
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-05-04 17:13:22 +0200

    feat(tp): add component for navbar page links

---
+++ apps/sps-termportal-web/components/NavBarPageLink.vue
@@ -0,0 +1,17 @@
+<template>
+  <li>
+    <AppLink
+      :to="to"
+      class="decoration-inherit hover:text-black"
+      @click="navMenuExpanded = false"
+    >
+      <slot></slot
+    ></AppLink>
+  </li>
+</template>
+
+<script setup lang="ts">
+const navMenuExpanded = useNavMenuExpanded();
+
+const props = defineProps({ to: { type: String, required: true } });
+</script>
