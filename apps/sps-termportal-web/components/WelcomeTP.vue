1c6ed511a5951e00a9f32b24b6044b5d76b43161
HildeJohansen <131660374+HildeJohansen@users.noreply.github.com>
2023-05-10 09:51:38 +0200

    Update WelcomeTP.vue

--- apps/sps-termportal-web/components/WelcomeTP.vue
+++ apps/sps-termportal-web/components/WelcomeTP.vue
@@ -45,9 +45,9 @@
           fagområde via éi felles søkerute. Du kan søke etter termar på tvers
           av domene, termbasar og språk. Termbasane er grupperte i overordna
           domene og du kan søke etter termar enten i heile Termportalen.no, på
-          eitt spesifikt domene eller i eit utval av termbasane. Du kan óg velje 
+          eitt spesifikt domene eller i eit utval av termbasane. Du kan òg velje 
           søkespråk på førehand. Dei fleste termbasane har terminologi på
-          bokmål, nynorsk og engelsk, mens ein del termbasar også har terminologi
+          bokmål, nynorsk og engelsk, mens ein del termbasar òg har terminologi
           på andre språk.</p>
         <p>
           I tillegg til søkesida til Termportalen har det blitt utvikla ein
@@ -74,16 +74,16 @@
       </h2>
       <div class="space-y-2">
         <p> Termportalen provides free access to Norwegian terminology in many different 
-          subject fields through one shared search field. It is possible to search for terms 
+          subject fields through one search field. It is possible to search for terms 
           across different domains, termbases and languages. The termbases are grouped into
-          overall domains and it is possible to search for terms in either the whole 
+          overall domains and you may search for terms in either the whole 
           Termportalen.no, in one specific domain, or in a selection of termbases.
-          You can also choose a search language in advance. Most of the termbases contain 
-          terminology in Norwegian bokmål, Norwegian nynorsk and English, and some
-          termbases also contain terminology in additional languages. </p>
+          You may also choose a search language in advance. Most of the termbases contain 
+          terminology in Norwegian Bokmål, Norwegian Nynorsk and English, while some
+          termbases also contain terminology in other languages as well. </p>
         <p>
-        In addition to Termportalen's search interface, an application for registration
-        of terminology has been developed. Contact us on
+        In addition to Termportalen's search interface, we have developed a terminology management 
+        application for registration of terminology. Please contact us on
           <AppLink
             class="underline hover:decoration-2"
             to="mailto:termportalen@uib.no"
@@ -92,9 +92,9 @@
           </AppLink>
           if you want to get started with terminology work in your own subject field.
         </p>
-        <p>Termportalen is a part of The Norwegian Language Collections at The University of Bergen,
-          and the terminology work is done in consultation with The Language Council of Norway.
-          Termportalen is funded by The Ministry of Education and Research as well as 
+        <p>Termportalen is a part of The Norwegian Language Collections (Språksamlingane) at the University of Bergen,
+          and the terminology work is done in consultation with the Language Council of Norway.
+          Termportalen is funded by the Ministry of Education and Research and the
           the the Ministry of Culture and Equality.</p>
       </div>
     </template>
