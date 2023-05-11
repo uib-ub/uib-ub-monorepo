40f1176f43703a1c8066e312acc9adb53e58b28a
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-05-11 11:20:43 +0200

    feat(tp): expand composable with locale related stuff

--- apps/sps-termportal-web/composables/locale.ts
+++ apps/sps-termportal-web/composables/locale.ts
@@ -1,7 +1,110 @@
 import { useI18n } from "vue-i18n";
-import { LocalLangCode } from "~/utils/vars-language";
+import { Samling } from "~/utils/vars-termbase";
 
-export function useLocale() {
+export type LocalLangCode = "en" | "nb" | "nn";
+
+export type LangCode =
+  | "ar"
+  | "da"
+  | "de"
+  | "en"
+  | "fa-af"
+  | "fi"
+  | "fr"
+  | "it"
+  | "la"
+  | "nb"
+  | "nn"
+  | "pl"
+  | "ru"
+  | "so"
+  | "es"
+  | "sv"
+  | "ti";
+
+export const useLocale = () => {
   const i18n = useI18n();
   return i18n.locale.value as LocalLangCode;
+};
+
+export const useLocales = () => {
+  return Object.keys(languageOrder) as LocalLangCode[];
+};
+
+export const useLocaleLangOrder = () => {
+  const locale = useLocale();
+  return languageOrder[locale];
+};
+
+const languageOrder: { [key in LocalLangCode]: LangCode[] } = {
+  nb: [
+    "nb",
+    "nn",
+    "en",
+    "ar",
+    "da",
+    "fi",
+    "fr",
+    "it",
+    "la",
+    "pl",
+    "ru",
+    "so",
+    "es",
+    "sv",
+    "ti",
+    "de",
+  ],
+  nn: [
+    "nn",
+    "nb",
+    "en",
+    "ar",
+    "da",
+    "fi",
+    "fr",
+    "it",
+    "la",
+    "pl",
+    "ru",
+    "so",
+    "es",
+    "sv",
+    "ti",
+    "de",
+  ],
+  en: [
+    "en",
+    "nb",
+    "nn",
+    "ar",
+    "da",
+    "fi",
+    "fr",
+    "de",
+    "it",
+    "la",
+    "pl",
+    "ru",
+    "so",
+    "es",
+    "sv",
+    "ti",
+  ],
+};
+
+function deriveLanguageInfo(languages: LangCode[]): {
+  [key in LangCode]: Samling[];
+} {
+  return Object.assign(
+    {},
+    ...languages.map((lang) => ({
+      [lang]: termbaseOrder.filter((base) =>
+        termbaseInfo[base].includes(lang as LangCode)
+      ),
+    }))
+  );
 }
+
+export const languageInfo = deriveLanguageInfo(languageOrder.nb);
+export const languageRtoL: Set<LangCode> = new Set(["ar"]);
