c39ea176054c88de05467f9b9da497c63e8928e2
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-05-12 10:32:43 +0200

    ops(vercel): add var for tp

--- ignore-build.sh
+++ ignore-build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+tp = '*-tp-*'
 
-if [[ "$VERCEL_GIT_COMMIT_REF" =~ *-tp-* ]] ; then
+if [[ "$VERCEL_GIT_COMMIT_REF" =~ $tp ]] ; then
     echo "- Build can proceed"
     exit 1;
 else
