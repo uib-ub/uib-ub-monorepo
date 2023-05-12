716095748b8f7775280613ff4c7aa7c79d9aec71
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-05-12 10:35:05 +0200

    ops(vercel): move ignore build script to app folder

--- apps/sps-termportal-web/ignore-build.sh
+++ apps/sps-termportal-web/ignore-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
-tp = '*-tp-*'
+tp = '*-[tp]-*'
 
 if [[ "$VERCEL_GIT_COMMIT_REF" =~ $tp ]] ; then
     echo "- Build can proceed"
