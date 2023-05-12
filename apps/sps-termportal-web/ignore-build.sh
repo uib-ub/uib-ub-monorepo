0dcd9ac60029632546f064a47078e282a9b5c21f
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-05-12 10:55:01 +0200

    ops(vercel): fix regex add turbo ignore

--- apps/sps-termportal-web/ignore-build.sh
+++ apps/sps-termportal-web/ignore-build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
-TP='*-[tp]-*'
+TP='*-(tp)-*'
 
 if [[ "$VERCEL_GIT_COMMIT_REF" =~ $TP ]] ; then
-    echo "- Build can proceed"
-    exit 1;
+    echo "- Build can proceed, calling turbo-ignore"
+    npx turbo-ignore
 else
     echo "- Build cancelled"
     exit 0;
