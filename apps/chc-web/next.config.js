0d81faeb001385f7171b4d3aa49d31953f72f295
Tarje Lavik <tarje.lavik@gmail.com>
2023-05-16 09:54:01 +0200

    refactor(chc-web): :fire: remove sanity

--- apps/chc-web/next.config.js
+++ apps/chc-web/next.config.js
@@ -13,13 +13,6 @@ const config = {
   experimental: {
     appDir: true,
   },
-
-  images: {
-    remotePatterns: [
-      { hostname: 'cdn.sanity.io' },
-      { hostname: 'source.unsplash.com' },
-    ],
-  },
   typescript: {
     // Set this to false if you want production builds to abort if there's type errors
     ignoreBuildErrors: process.env.VERCEL_ENV === 'production',
