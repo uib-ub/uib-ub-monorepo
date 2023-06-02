0cbfa9dafdd74c2aceeb3c18b1b0ca3c40db64e4
Henrik Askjer <Henrik.Askjer@uib.no>
2023-06-02 13:43:20 +0200

    fix(ob): adjust suggestion padding

--- apps/sps-ordbokene-web/components/SuggestResults.vue
+++ apps/sps-ordbokene-web/components/SuggestResults.vue
@@ -1,9 +1,9 @@
 <template>
 <div v-if="suggestions && suggestions.length" class="suggestions py-2 px-1">
     <h2><slot/></h2>
-    <ul class="nav nav-pills flex-column md:flex md:flex-wrap md:gap-8 py-6 pt-4 md:py-8">
+    <ul class="nav nav-pills flex-column md:flex md:flex-wrap md:gap-8 pt-4 md:py-8">
         <li class="nav-item flex" v-for="(item, idx) in suggestions" :key="idx">
-            <NuxtLink class="suggest-link py-3 md:py-0 md w-full" :to="suggest_link(item[0])"><Icon name="bi:search" class="mr-3 mb-1"/><span class="link-content">{{item[0]}}</span></NuxtLink>
+            <NuxtLink class="suggest-link py-3 md:py-0 w-full" :to="suggest_link(item[0])"><Icon name="bi:search" class="mr-3 mb-1"/><span class="link-content">{{item[0]}}</span></NuxtLink>
         </li>
     </ul>
 </div>
