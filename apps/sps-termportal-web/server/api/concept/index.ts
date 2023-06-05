46510f5dcdde0d7ebb489861b67591634df442fa
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-06-05 13:28:24 +0200

    feat(tp): check for apiKey in server api routes

--- apps/sps-termportal-web/server/api/concept/index.ts
+++ apps/sps-termportal-web/server/api/concept/index.ts
@@ -1,34 +1,41 @@
 export default defineEventHandler(async (event) => {
-  const url = useRuntimeConfig().public.endpointUrl;
-  const body = await readBody(event);
-  const concept = body.termbase === "FBK" ? body.base + body.concept : body.concept;
-  const query = genConceptQuery(body.base, body.termbase, body.concept);
-  
-  const controller = new AbortController();
-  const timer = setTimeout(() => {
-    controller.abort();
-  }, 7000);
+  const runtimeConfig = useRuntimeConfig();
+  // Check if event provides api key should be injected by server middleware
+  if (event.context.auth === runtimeConfig.apiKey) {
+    const url = runtimeConfig.public.endpointUrl;
+    const body = await readBody(event);
+    const concept =
+      body.termbase === "FBK" ? body.base + body.concept : body.concept;
+    const query = genConceptQuery(body.base, body.termbase, body.concept);
 
-  try {
-    const data = await $fetch(url, {
-      method: "post",
-      body: query,
-      signal: controller.signal,
-      headers: {
-        "Content-type": "application/sparql-query",
-        Referer: "termportalen.no", // TODO Referer problem
-        Accept: "application/ld+json",
-      },
-    }).then((value) => {
-      clearTimeout(timer);
-      return value;
-    });
+    const controller = new AbortController();
+    const timer = setTimeout(() => {
+      controller.abort();
+    }, 7000);
 
-    return frameData(data, "skos:Concept").then((result) => {
-      delete result["@context"];
-      return parseConceptData(result, concept);
-    });
-  } catch (e) {
-    // console.log(e)
+    try {
+      const data = await $fetch(url, {
+        method: "post",
+        body: query,
+        signal: controller.signal,
+        headers: {
+          "Content-type": "application/sparql-query",
+          Referer: "termportalen.no", // TODO Referer problem
+          Accept: "application/ld+json",
+        },
+      }).then((value) => {
+        clearTimeout(timer);
+        return value;
+      });
+
+      return frameData(data, "skos:Concept").then((result) => {
+        delete result["@context"];
+        return parseConceptData(result, concept);
+      });
+    } catch (e) {
+      // console.log(e)
+    }
+  } else {
+    return "Access denied."
   }
 });
