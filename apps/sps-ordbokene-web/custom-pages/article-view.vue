199e5228964fae3a555d3be6c9607a1824e9d893
Henrik Askjer <Henrik.Askjer@uib.no>
2023-06-09 10:16:35 +0200

    fix article view navigation bug

--- apps/sps-ordbokene-web/custom-pages/article-view.vue
+++ apps/sps-ordbokene-web/custom-pages/article-view.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class ="py-2 article-view">
-        <Article single :article_id="parseInt($route.params.article_id)" :dict="$route.params.dict"/>
+        <Article single :article_id="parseInt($route.params.article_id)" :dict="dict"/>
     </div>
   </div>
 </template>
@@ -9,8 +9,9 @@
 <script setup>
 import { useStore } from '~/stores/searchStore'
 const store = useStore()
+const route = useRoute()
+const dict = route.params.dict
 </script>
-
 
 <style scoped>
 
