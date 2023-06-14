91efade614c7955acd634500b45fd72a473b5de1
Henrik Askjer <Henrik.Askjer@uib.no>
2023-06-14 14:55:11 +0200

    adjust error icon position

--- apps/sps-ordbokene-web/components/Error.vue
+++ apps/sps-ordbokene-web/components/Error.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="callout error lg:p-4">
-    <h2><Icon name="bi:exclamation-triangle-fill" class="mr-3"/>{{title}}</h2>
+    <h2><Icon name="bi:exclamation-triangle-fill" class="mr-3 mb-1"/>{{title}}</h2>
     <p>Feil feil feil!</p>{{error}}
   </div>
 
