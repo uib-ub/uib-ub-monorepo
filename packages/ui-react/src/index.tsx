4bd93e7149b7e227a9fc7172068d46c20f7ca36b
Tarje Lavik <tarje.lavik@gmail.com>
2023-06-21 22:32:48 +0200

    add shadcn based ui-react package

---
+++ packages/ui-react/src/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+// Components export
+export * from './components/badge';
+export * from './components/button';
+export * from './components/card';
+export * from './components/checkbox';
+export * from './components/dropdown-menu';
+export * from './components/popover';
+export * from './components/scroll-area';
+export * from './components/shimmer';
