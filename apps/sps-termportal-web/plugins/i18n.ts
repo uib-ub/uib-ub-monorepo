eecf7609d4b46adffcd155ed17a33b0bfabffa6c
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-06-30 14:52:44 +0200

    feat(tp): set locale cookie from i18n plugin

--- apps/sps-termportal-web/plugins/i18n.ts
+++ apps/sps-termportal-web/plugins/i18n.ts
@@ -2,12 +2,19 @@ import { createI18n } from "vue-i18n";
 import en from "../locales/en.json";
 import nb from "../locales/nb.json";
 import nn from "../locales/nn.json";
+import { LocalLangCode } from "~/composables/locale";
+import { cookieLocaleOptions } from "~/utils/vars";
 
 export default defineNuxtPlugin(({ vueApp }) => {
+  const cookie = useCookie("locale", cookieLocaleOptions);
+  const locale: LocalLangCode = (cookie.value as LocalLangCode) || "nb";
+  cookie.value = "";
+  cookie.value = locale;
+
   const i18n = createI18n({
     legacy: false,
     globalInjection: true,
-    locale: "nb",
+    locale,
     messages: {
       en,
       nb,
