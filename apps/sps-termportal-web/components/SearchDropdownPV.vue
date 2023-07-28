467da7b8fd089b2ca143dda60b4959ae006076c2
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-07-28 12:21:01 +0200

    feat(tp): add primevue dropdown component and upd/fix css

---
+++ apps/sps-termportal-web/components/SearchDropdownPV.vue
@@ -0,0 +1,17 @@
+<template>
+  <Dropdown
+    v-model="searchInterface[dropdown]"
+    :options="options"
+    optionLabel="label"
+    optionValue="value"
+    class="flex h-8 items-center justify-between"
+  />
+</template>
+
+<script setup>
+const searchInterface = useSearchInterface();
+const props = defineProps({
+  dropdown: { type: String, required: true },
+  options: { type: Array, required: true },
+});
+</script>
