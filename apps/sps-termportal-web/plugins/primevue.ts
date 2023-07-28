95bbb761f5b55b8d0bcdf30d44dd213f4e2a209c
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-07-28 15:06:38 +0200

    feat(tp): add primevue plugin

---
+++ apps/sps-termportal-web/plugins/primevue.ts
@@ -0,0 +1,13 @@
+import PrimeVue from "primevue/config";
+import Accordion from "primevue/accordion";
+import Button from "primevue/button";
+import Dropdown from "primevue/dropdown";
+import Menu from "primevue/menu";
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(PrimeVue, { ripple: true });
+  nuxtApp.vueApp.component("Accordion", Accordion);
+  nuxtApp.vueApp.component("Button", Button);
+  nuxtApp.vueApp.component("Dropdown", Dropdown);
+  nuxtApp.vueApp.component("Menu", Menu);
+});
