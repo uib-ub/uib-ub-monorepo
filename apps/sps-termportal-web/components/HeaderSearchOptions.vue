25cbba436e918c2dcd4bd578b908a427bb16680c
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-07-29 17:29:58 +0200

    feat(tp): use primvue dropdown for translate, termbase dropdown

--- apps/sps-termportal-web/components/HeaderSearchOptions.vue
+++ apps/sps-termportal-web/components/HeaderSearchOptions.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-wrap gap-x-3">
+  <div class="flex flex-wrap gap-x-6">
     <SearchDropdownWrapper target="language">
       <DropdownPV
         :id="`languageDropdown`"
@@ -8,31 +8,22 @@
         class="min-w-[11rem]"
       />
     </SearchDropdownWrapper>
-
-    <SearchBarDropdown dropdown="translate" dd-width="5.5em">
-      <option value="none">
-        {{ $t("global.lang.none") }}
-      </option>
-      <option
-        v-for="lc in intersectUnique(localeLangOrder, filteredTranslationLangs)"
-        :key="'translationlang_' + lc"
-        :value="lc"
-      >
-        {{ $t("global.lang." + lc) }}
-      </option>
-    </SearchBarDropdown>
-    <SearchBarDropdown dropdown="termbase" dd-width="20em">
-      <option value="all">
-        {{ $t("global.samling.all") }} ({{ filteredTermbases.length }})
-      </option>
-      <option
-        v-for="samling in filteredTermbases"
-        :key="'searchsamling_' + samling"
-        :value="samling"
-      >
-        {{ $t("global.samling." + samling) }}
-      </option>
-    </SearchBarDropdown>
+    <SearchDropdownWrapper target="translate">
+      <DropdownPV
+        :id="`translateDropdown`"
+        v-model="searchInterface.translate"
+        :options="optionsTranslate"
+        class="min-w-[6rem]"
+      />
+    </SearchDropdownWrapper>
+    <SearchDropdownWrapper target="termbase">
+      <DropdownPV
+        :id="`termbaseDropdown`"
+        v-model="searchInterface.termbase"
+        :options="optionsTermbase"
+        class="min-w-[20rem]"
+      />
+    </SearchDropdownWrapper>
   </div>
 </template>
 
@@ -58,30 +49,35 @@ const optionsLanguage = computed(() => {
   return options;
 });
 
-// intersectUnique(localeLangOrder, filteredSearchLangs)
-const filteredSearchLangs = computed(() => {
-  return deriveSearchOptions("language", "all");
-});
-const optionsLanguag = computed(() => {
-  const lst = [
+const optionsTranslate = computed(() => {
+  const filteredTranslate = deriveSearchOptions("translate", "none");
+  const intersection = intersectUnique(localeLangOrder, filteredTranslate);
+  const options = [
     {
-      label: i18n.t("global.lang.all") + ` (${props.optionsLang.length})`,
-      value: "all",
+      label: i18n.t("global.lang.none"),
+      value: "none",
     },
   ].concat(
-    props.optionsLang.map((lang) => {
+    intersection.map((lang) => {
       return { label: i18n.t("global.lang." + lang), value: lang };
     })
   );
-  return lst;
+  return options;
 });
 
-const filteredTranslationLangs = computed(() => {
-  return deriveSearchOptions("translate", "none");
-});
-
-const filteredTermbases = computed(() => {
-  return deriveSearchOptions("termbase", "all");
+const optionsTermbase = computed(() => {
+  const filteredTermbases = deriveSearchOptions("termbase", "all");
+  const options = [
+    {
+      label: i18n.t("global.samling.all"),
+      value: "all",
+    },
+  ].concat(
+    filteredTermbases.map((tb) => {
+      return { label: i18n.t("global.samling." + tb), value: tb };
+    })
+  );
+  return options;
 });
 
 function filterTermbases(termbases, filterTermbases, option, defaultValue) {
