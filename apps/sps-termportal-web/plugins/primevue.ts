4cf24c25293bbbcde3ff9292ee0445558644fdde
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-08-07 14:02:32 +0200

    fix(tp): don't import dropdown

--- apps/sps-termportal-web/plugins/primevue.ts
+++ apps/sps-termportal-web/plugins/primevue.ts
@@ -3,13 +3,11 @@ import Accordion from "primevue/accordion";
 import Button from "primevue/button";
 import Dropdown from "primevue/dropdown";
 import Menu from "primevue/menu";
-import AutoComplete from "primevue/autocomplete";
 
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(PrimeVue, { ripple: true });
   nuxtApp.vueApp.component("Accordion", Accordion);
-  nuxtApp.vueApp.component("AutoComplete", AutoComplete);
   nuxtApp.vueApp.component("Button", Button);
   nuxtApp.vueApp.component("Dropdown", Dropdown);
   nuxtApp.vueApp.component("Menu", Menu);
