9aa9119cec4bca4e5948a09db9f018223ff8f682
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-08-09 15:16:05 +0200

    feat(tp): add language switcher to navbar

--- apps/sps-termportal-web/components/NavBar.vue
+++ apps/sps-termportal-web/components/NavBar.vue
@@ -20,46 +20,99 @@
           <SearchField class="max-w-[51em]" />
         </div>
       </div>
-      <div ref="navPageLinks" class="px-4 text-lg font-semibold text-gray-500">
-        <div class="md:hidden">
-          <button
-            class="ml-auto mr-0 flex rounded border px-2 py-1"
-            :aria-label="
-              navMenuExpanded
-                ? $t('navBar.menuButtonHide')
-                : $t('navBar.menuButtonShow')
-            "
-            :title="
-              navMenuExpanded
-                ? $t('navBar.menuButtonHide')
-                : $t('navBar.menuButtonShow')
-            "
-            @click="navMenuExpanded = !navMenuExpanded"
-          >
-            <Icon name="tabler:menu-2" aria-hidden="true" size="1.2em" />
-          </button>
-        </div>
-        <ul
-          id="NavMenuContent"
-          class="xs:gap-4 xs:pt-0 xs:space-y-0 xs:mt-0 xs:rounded-none xs:border-none xs:p-0 right-0 mt-[8.5px] space-y-2 rounded-b border border-t-0 p-4 md:flex"
-          :class="{ hidden: !navMenuExpanded }"
+      <div class="flex items-center">
+        <button
+          :aria-label="`${$t('navBar.language')}`"
+          aria-haspopup="true"
+          aria-controls="overlayLangMenu"
+          @click="langMenu.toggle"
         >
-          <NavBarPageLink to="/om">
-            {{ $t("navBar.om") }}
-          </NavBarPageLink>
-          <NavBarPageLink to="/innstillinger">
-            {{ $t("navBar.innstillinger") }}
-          </NavBarPageLink>
-        </ul>
+          <Icon
+            class="text-gray-500 hover:text-black"
+            name="material-symbols:language"
+            size="1.4em"
+          />
+        </button>
+        <Menu
+          id="overlayLangMenu"
+          ref="langMenu"
+          :model="langOptions"
+          :popup="true"
+        />
+        <div
+          ref="navPageLinks"
+          class="px-4 text-lg font-semibold text-gray-500"
+        >
+          <div class="md:hidden">
+            <button
+              class="ml-auto mr-0 flex rounded border px-2 py-1"
+              :aria-label="
+                navMenuExpanded
+                  ? $t('navBar.menuButtonHide')
+                  : $t('navBar.menuButtonShow')
+              "
+              :title="
+                navMenuExpanded
+                  ? $t('navBar.menuButtonHide')
+                  : $t('navBar.menuButtonShow')
+              "
+              @click="navMenuExpanded = !navMenuExpanded"
+            >
+              <Icon name="tabler:menu-2" aria-hidden="true" size="1.2em" />
+            </button>
+          </div>
+          <ul
+            id="NavMenuContent"
+            class="right-0 mt-[8.5px] space-y-2 rounded-b border border-t-0 p-4 xs:mt-0 xs:gap-4 xs:space-y-0 xs:rounded-none xs:border-none xs:p-0 xs:pt-0 md:flex"
+            :class="{ hidden: !navMenuExpanded }"
+          >
+            <NavBarPageLink to="/om">
+              {{ $t("navBar.om") }}
+            </NavBarPageLink>
+            <NavBarPageLink to="/innstillinger">
+              {{ $t("navBar.innstillinger") }}
+            </NavBarPageLink>
+          </ul>
+        </div>
       </div>
     </div>
   </nav>
 </template>
 
 <script setup lang="ts">
+import { useI18n } from "vue-i18n";
+
+const i18n = useI18n();
 const navMenuExpanded = useNavMenuExpanded();
 const navBar = ref<HTMLElement | null>(null);
 const navPageLinks = ref<HTMLElement | null>(null);
+
+const langMenu = ref();
+const langOptions = computed(() => [
+  {
+    label: `${i18n.t("navBar.language")}`,
+    items: [
+      {
+        label: `${i18n.t("global.lang.nb")}`,
+        command: () => {
+          i18n.locale.value = "nb";
+        },
+      },
+      {
+        label: `${i18n.t("global.lang.nn")}`,
+        command: () => {
+          i18n.locale.value = "nn";
+        },
+      },
+      {
+        label: `${i18n.t("global.lang.en")}`,
+        command: () => {
+          i18n.locale.value = "en";
+        },
+      },
+    ],
+  },
+]);
 
 defineExpose({ navBar });
 
