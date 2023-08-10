86d8ec6bd3b302ea4303bcf1bfc5e0781f6036f7
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-08-10 13:50:00 +0200

    feat(tp): redo instillinger with custom icons spacing, hl

--- apps/sps-termportal-web/pages/innstillinger.vue
+++ apps/sps-termportal-web/pages/innstillinger.vue
@@ -3,17 +3,59 @@
     <Head>
       <Title>{{ $t("innstillinger.title") }} | {{ $t("index.title") }}</Title>
     </Head>
-    <h1 id="main" class="pb-2 pt-5 text-3xl">
-      <AppLink to="#main">{{ $t("innstillinger.title") }}</AppLink>
+    <h1 id="main" class="pb-3 pt-6">
+      <AppLink to="#main" class="tp-hover-focus px-2 py-1 text-3xl">{{
+        $t("innstillinger.title")
+      }}</AppLink>
     </h1>
     <section>
       <h2 id="global" class="pb-2 text-2xl">
-        <AppLink to="#global">{{ $t("innstillinger.global") }}</AppLink>
+        <AppLink to="#global" class="tp-hover-focus px-2 py-1">{{
+          $t("innstillinger.global")
+        }}</AppLink>
       </h2>
-      <label for="locale-select" class="text-lg"
-        >{{ $t("global.language") }}:
-      </label>
+      <div class="flex flex-col gap-0.5">
+        <div
+          v-for="lang in locales"
+          :key="'rbv' + lang"
+          class="align-items-center group flex"
+        >
+          <input
+            :id="'rb-' + lang"
+            v-model="i18n.locale.value"
+            type="radio"
+            class="peer outline-none"
+            :value="lang"
+            @keydown.enter="setLocale(lang)"
+          />
+          <label
+            :for="'rb-' + lang"
+            class="flex gap-2 rounded-[7px] border border-transparent px-1.5 py-1.5 pr-3 transition-shadow group-hover:cursor-pointer group-hover:border group-hover:border-tpblue-300 peer-focus:border peer-focus:border-tpblue-300 peer-focus:shadow-tphalo"
+            @click="setLocale(lang)"
+          >
+            <Icon
+              v-if="i18n.locale.value === lang"
+              class="text-tpblue-400"
+              name="akar-icons:radio-fill"
+              aria-hidden="true"
+              size="1.5em"
+            />
+            <Icon
+              v-else
+              name="akar-icons:radio"
+              class="text-tpblue-400"
+              aria-hidden="true"
+              size="1.5em"
+            />
+            <span class="text-base text-black">
+              {{ $t("global.lang." + lang) }}</span
+            ></label
+          >
+        </div>
+      </div>
+
       <select
+        v-if="false"
         id="locale-select"
         v-model="i18n.locale.value"
         class="tp-search-dd cursor-pointer px-2 py-1 text-lg"
@@ -24,30 +66,47 @@
       </select>
     </section>
     <section>
-      <h2 id="conceptview" class="pb-2 pt-3 text-2xl">
-        <AppLink to="#conceptview">{{
+      <h2 id="conceptview" class="pb-2 pt-6 text-2xl">
+        <AppLink to="#conceptview" class="tp-hover-focus px-2 py-1">{{
           $t("innstillinger.conceptview")
         }}</AppLink>
       </h2>
       <fieldset>
         <legend id="dataDispLang" class="pb-1 text-xl">
-          <AppLink to="#dataDispLang">{{
+          <AppLink to="#dataDispLang" class="tp-hover-focus px-2 py-1">{{
             $t("innstillinger.dataDispLang")
           }}</AppLink>
         </legend>
-        <div class="grid w-72 grid-flow-col grid-rows-6 gap-x-3 gap-y-2">
-          <div v-for="lang in localeLangOrder" :key="lang">
+        <div class="grid w-fit grid-flow-col grid-rows-6 gap-x-6 gap-y-0.5">
+          <div v-for="lang in localeLangOrder" :key="lang" class="group flex">
             <input
-              :id="`ddl-${lang}`"
+              :id="'ddl-' + lang"
               v-model="dataDisplayLanguages"
-              class="cursor-pointer checked:bg-tpblue-400"
+              class="peer cursor-pointer outline-none"
               type="checkbox"
               :value="lang"
+              @keydown.enter="toggleLang(lang)"
             />
+
             <label
-              class="cursor-pointer px-1 py-1 text-lg"
-              :for="`ddl-${lang}`"
-              >{{ $t("global.lang." + lang) }}</label
+              class="flex cursor-pointer items-center gap-x-1.5 rounded-[7px] border border-transparent px-2 py-1 pr-3 text-lg group-hover:border-tpblue-300 peer-focus:border-tpblue-300 peer-focus:shadow-tphalo"
+              :for="'ddl-' + lang"
+            >
+              <Icon
+                v-if="dataDisplayLanguages.includes(lang)"
+                name="mdi:checkbox-marked-outline"
+                size="1.4em"
+                class="text-tpblue-400"
+                aria-hidden="true"
+              />
+              <Icon
+                v-else
+                name="mdi:checkbox-blank-outline"
+                size="1.4em"
+                class="text-tpblue-400"
+                aria-hidden="true"
+              />
+              <span>{{ $t("global.lang." + lang) }}</span></label
             >
           </div>
         </div>
@@ -68,4 +127,29 @@ watch(i18n.locale, () => {
   const locale = useCookie("locale", cookieLocaleOptions);
   locale.value = i18n.locale.value;
 });
+
+function setLocale(language) {
+  i18n.locale.value = language;
+}
+
+function toggleLang(language) {
+  const index = dataDisplayLanguages.value.indexOf(language);
+  if (index !== -1) {
+    dataDisplayLanguages.value.splice(index, 1);
+  } else {
+    dataDisplayLanguages.value.push(language);
+  }
+}
 </script>
+
+<style scoped>
+input[type="radio"] {
+  -webkit-appearance: none;
+  appearance: none;
+}
+
+input[type="checkbox"] {
+  -webkit-appearance: none;
+  appearance: none;
+}
+</style>
