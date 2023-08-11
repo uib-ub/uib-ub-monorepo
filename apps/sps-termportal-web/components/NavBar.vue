952da1742304f43b217eaa7a85b8b69599837a15
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-08-11 11:22:16 +0200

    feat(tp): add menu, improve focus, hover for navbar

--- apps/sps-termportal-web/components/NavBar.vue
+++ apps/sps-termportal-web/components/NavBar.vue
@@ -6,75 +6,81 @@
   >
     <div class="mx-auto flex h-full grow items-center justify-between">
       <div class="grow">
-        <div v-if="context !== 'minimal'" class="flex grow">
-          <AppLink id="anchor" to="/"
-            ><div
-              class="flex w-12 justify-center font-medium lg:w-40 xl:w-[10vw] xl:min-w-[10rem] xl:max-w-[13rem]"
+        <div v-if="context !== 'minimal'" class="flex grow items-center">
+          <div
+            class="w-12 px-2 lg:w-40 xl:w-[10vw] xl:min-w-[10rem] xl:max-w-[13rem]"
+          >
+            <AppLink
+              id="anchor"
+              to="/"
+              aria-label="Termportalen.no"
+              class="tp-hover-focus flex justify-center px-3 py-0.5 text-[1.4em]"
             >
-              <span class="text-[1.4em] font-semibold lg:font-normal">T</span
-              ><span class="hidden text-[1.4em] transition lg:inline"
+              <span class="font-semibold lg:font-normal" aria-hidden="true"
+                >T</span
+              ><span
+                class="hidden font-normal transition lg:inline"
+                aria-hidden="true"
                 >ermportalen</span
               >
-            </div></AppLink
-          >
+            </AppLink>
+          </div>
           <SearchField class="max-w-[51em]" />
         </div>
       </div>
-      <div class="flex items-center">
-        <button
-          class="pl-4"
-          :aria-label="`${$t('navBar.language')}`"
-          aria-haspopup="true"
-          aria-controls="overlayLangMenu"
-          @click="langMenu.toggle"
+      <div class="hidden lg:flex">
+        <ul
+          class="flex items-center gap-x-1 px-4 text-lg font-semibold text-gray-500"
         >
-          <Icon
-            class="text-gray-500 hover:text-black"
-            name="material-symbols:language"
-            size="1.4em"
-          />
-        </button>
-        <Menu
-          id="overlayLangMenu"
-          ref="langMenu"
-          :model="langOptions"
-          :popup="true"
-        />
-        <div
-          ref="navPageLinks"
-          class="px-4 text-lg font-semibold text-gray-500"
-        >
-          <div class="md:hidden">
+          <div>
             <button
-              class="ml-auto mr-0 flex rounded border px-2 py-1"
-              :aria-label="
-                navMenuExpanded
-                  ? $t('navBar.menuButtonHide')
-                  : $t('navBar.menuButtonShow')
-              "
-              :title="
-                navMenuExpanded
-                  ? $t('navBar.menuButtonHide')
-                  : $t('navBar.menuButtonShow')
-              "
-              @click="navMenuExpanded = !navMenuExpanded"
+              class="tp-hover-focus group px-1.5 pb-1 pt-0.5"
+              :aria-label="`${$t('navBar.language')}`"
+              aria-haspopup="menu"
+              aria-controls="overlayLangMenu"
+              @click="langMenu.toggle"
             >
-              <Icon name="tabler:menu-2" aria-hidden="true" size="1.2em" />
+              <Icon
+                class="text-gray-500 group-hover:text-black"
+                name="material-symbols:language"
+                size="1.4em"
+              />
             </button>
+            <Menu
+              id="overlayLangMenu"
+              ref="langMenu"
+              :model="langOptions"
+              :popup="true"
+            />
           </div>
-          <ul
-            id="NavMenuContent"
-            class="right-0 mt-[8.5px] space-y-2 rounded-b border border-t-0 p-4 xs:mt-0 xs:gap-4 xs:space-y-0 xs:rounded-none xs:border-none xs:p-0 xs:pt-0 md:flex"
-            :class="{ hidden: !navMenuExpanded }"
-          >
-            <NavBarPageLink to="/om">
-              {{ $t("navBar.om") }}
-            </NavBarPageLink>
-            <NavBarPageLink to="/innstillinger">
-              {{ $t("navBar.innstillinger") }}
-            </NavBarPageLink>
-          </ul>
-        </div>
+          <NavBarPageLink to="/om">
+            {{ $t("navBar.om") }}
+          </NavBarPageLink>
+          <NavBarPageLink to="/innstillinger">
+            {{ $t("navBar.innstillinger") }}
+          </NavBarPageLink>
+        </ul>
+      </div>
+      <div class="px-3 lg:hidden">
+        <button
+          class="tp-hover-focus ml-auto mr-0 flex rounded border border-black p-1"
+          aria-haspopup="true"
+          aria-controls="overlayMenu"
+          :aria-label="
+            navMenuExpanded
+              ? $t('navBar.menuButtonHide')
+              : $t('navBar.menuButtonShow')
+          "
+          :title="
+            navMenuExpanded
+              ? $t('navBar.menuButtonHide')
+              : $t('navBar.menuButtonShow')
+          "
+          @click="menu.toggle"
+        >
+          <Icon name="tabler:menu-2" aria-hidden="true" size="2em" />
+        </button>
+        <Menu id="overlayMenu" ref="menu" :model="menuOptions" :popup="true" />
       </div>
     </div>
   </nav>
@@ -87,6 +93,40 @@ const i18n = useI18n();
 const navMenuExpanded = useNavMenuExpanded();
 const navBar = ref<HTMLElement | null>(null);
 const navPageLinks = ref<HTMLElement | null>(null);
+
+const menu = ref();
+const menuOptions = computed(() => [
+  {
+    label: `${i18n.t("navBar.navigation")}`,
+    items: [
+      { label: `${i18n.t("navBar.om")}`, to: "/om" },
+      { label: `${i18n.t("navBar.innstillinger")}`, to: "/innstillinger" },
+    ],
+  },
+  {
+    label: `${i18n.t("navBar.language")}`,
+    items: [
+      {
+        label: `${i18n.t("global.lang.nb")}`,
+        command: () => {
+          i18n.locale.value = "nb";
+        },
+      },
+      {
+        label: `${i18n.t("global.lang.nn")}`,
+        command: () => {
+          i18n.locale.value = "nn";
+        },
+      },
+      {
+        label: `${i18n.t("global.lang.en")}`,
+        command: () => {
+          i18n.locale.value = "en";
+        },
+      },
+    ],
+  },
+]);
 
 const langMenu = ref();
 const langOptions = computed(() => [
