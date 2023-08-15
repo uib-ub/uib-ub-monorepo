192244e1087fcab6d1f47ffe6b3c084bbfb23c06
Henrik Askjer <Henrik.Askjer@uib.no>
2023-08-15 16:07:11 +0200

    cleanup

--- apps/sps-ordbokene-web/middleware/endpoint-middleware.global.ts
+++ apps/sps-ordbokene-web/middleware/endpoint-middleware.global.ts
@@ -6,7 +6,6 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
     //console.log("MIDDLEWARE\nFROM: ", from, "\nTO: ", to, "\nREDIRECTED FROM:",to.redirectedFrom)
     
     const get_concepts = async (server, env) => {
-      console.log("GETTING CONCEPTS")
       await Promise.all([fetch(`https://${server}.uib.no/opal/${env}/bm/concepts.json`).then(r => r.json()), fetch(`https://${server}.uib.no/opal/${env}/nn/concepts.json`).then(r => r.json())]).then(response => {
         store.concepts_bm = response[0].concepts
         store.concepts_nn = response[1].concepts
