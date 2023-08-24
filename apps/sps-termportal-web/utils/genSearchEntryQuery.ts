77208fcdbfd8a1027cb04a180649287a804fae77
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-08-24 20:21:59 +0200

    fix(tp): reduce entries query, rm highlighting etc

--- apps/sps-termportal-web/utils/genSearchEntryQuery.ts
+++ apps/sps-termportal-web/utils/genSearchEntryQuery.ts
@@ -1,10 +1,5 @@
 import { Matching, LabelPredicate, SearchOptions } from "./vars";
 
-const htmlHighlight = {
-  open: "<mark class='tp-shighlight'>",
-  close: "</mark>",
-};
-
 export function sanitizeTerm(term: string) {
   return term
     .replace(/-|\(|\)|<|>|\[|\]|\/|,\s*$|\*|~|'|"|_/g, " ")
@@ -14,10 +9,7 @@ export function sanitizeTerm(term: string) {
     .trim();
 }
 
-export function getTermData(
-  term: string,
-  highlight: { [key: string]: string }
-) {
+export function getTermData(term: string) {
   return {
     term,
     sanitized: () => sanitizeTerm(term),
@@ -33,26 +25,6 @@ export function getTermData(
         .map((t: string) => "*" + t + "*")
         .join(" AND ");
     },
-    termHL: () =>
-      term.replace(/((?!, )[\p{Letter}\p{Mark}\d,])+/giu, function (x) {
-        return highlight.open + x + highlight.close;
-      }),
-    termHLstart: function () {
-      if (this.termHL().endsWith(highlight.close)) {
-        return this.termHL().slice(0, -highlight.close.length);
-      } else {
-        return this.termHL();
-      }
-    },
-    termHLend: function () {
-      if (this.termHL().startsWith(highlight.open)) {
-        return this.termHL().slice(highlight.open.length);
-      } else {
-        return this.termHL();
-      }
-    },
-    queryHighlight: () =>
-      `highlight:s:${highlight.open} | e:${highlight.close}`,
   };
 }
 
@@ -85,56 +57,69 @@ function getLanguageWhere(
   }
 }
 
-function getPredicateValues(predicate: LabelPredicate[]): string {
-  const predLength = predicate.length;
-  if (predLength === 0 || predLength === 3) {
-    return "";
-  } else {
-    const filterPred = predicate.map((p) => `skosxl:${p}`).join(" ");
-    return `VALUES ?predicate { ${filterPred} }`;
-  }
+export function getPredicateValues(predicates: LabelPredicate[]): string {
+  return predicates.map((p) => `skosxl:${p}`).join("|");
 }
 
 export function genSearchEntryQuery(searchOptions: SearchOptions): string {
-  const termData = getTermData(searchOptions.term, htmlHighlight);
+  const runtimeConfig = useRuntimeConfig();
+  const termData = getTermData(searchOptions.term);
   const language = getLanguageData(searchOptions.language);
   const predFilter = getPredicateValues(searchOptions.predicate);
+
+  const context = () => {
+    const pred = searchOptions.useDomain ? "skosp:domene" : "skosp:memberOf";
+    const lst = searchOptions.useDomain
+      ? searchOptions.domain
+      : searchOptions.termbase;
+    const values = lst
+      .map((d: string) => {
+        return `base:${d}`;
+      })
+      .join(", ");
+
+    if (lst.length !== 0) {
+      return [`FILTER ( ?con IN ( ${values} ) )`, pred];
+    } else {
+      return ["", pred];
+    }
+  };
 
   const subqueries = (subEntry: string) => {
     const content = {
       "full-cs": {
         score: 500,
-        where: `{ (?label ?sc ?lit) text:query ("\\"${termData.sanitized()}\\"" "${termData.queryHighlight()}" {language}). }`,
-        filter: `FILTER ( str(?lit) = "${termData.termHL()}" ).`,
+        where: `{ (?label ?sc ?lit) text:query (skosxl:literalForm "\\"${termData.sanitized()}\\"" {language}). }`,
+        filter: `FILTER ( str(?lit) = "${termData.term}" ).`,
       },
       "full-ci": {
         score: 400,
-        where: `{ (?label ?sc ?lit) text:query ("\\"${termData.sanitized()}\\"" "${termData.queryHighlight()}" {language}). }`,
-        filter: `FILTER ( lcase(str(?lit)) = lcase("${termData.termHL()}") &&
-                       str(?lit) != "${termData.termHL()}" ).`,
+        where: `{ (?label ?sc ?lit) text:query (skosxl:literalForm "\\"${termData.sanitized()}\\"" {language}). }`,
+        filter: `FILTER ( lcase(str(?lit)) = lcase("${termData.term}") &&
+                       str(?lit) != "${termData.term}" ).`,
       },
       "startsWith-ci": {
         score: 300,
-        where: `{ (?label ?sc ?lit) text:query ("${termData.starred()}" "${termData.queryHighlight()}" {language}). }`,
-        filter: `FILTER ( strStarts(lcase(?lit), lcase("${termData.termHLstart()}") ) &&
-                       lcase(str(?lit)) != lcase("${termData.termHL()}") ).`,
+        where: `{ (?label ?sc ?lit) text:query (skosxl:literalForm "${termData.starred()}" {language}). }`,
+        filter: `FILTER ( strStarts(lcase(?lit), lcase("${termData.term}") ) &&
+                       lcase(str(?lit)) != lcase("${termData.term}") ).`,
       },
       "endsWith-ci": {
         score: 200,
-        where: `{ (?label ?sc ?lit) text:query ("${termData.doubleStarred()}" "${termData.queryHighlight()}" {language}). }`,
-        filter: `FILTER ( strEnds(lcase(?lit), lcase("${termData.termHLend()}") ) &&
-                       lcase(str(?lit)) != lcase("${termData.termHL()}") ).`,
+        where: `{ (?label ?sc ?lit) text:query (skosxl:literalForm "${termData.doubleStarred()}" {language}). }`,
+        filter: `FILTER ( strEnds(lcase(?lit), lcase("${termData.term}") ) &&
+                       lcase(str(?lit)) != lcase("${termData.term}") ).`,
       },
       "subWord-ci": {
         score: 100,
-        where: `{ (?label ?sc ?lit) text:query ("${termData.starred()}" "${termData.queryHighlight()}" {language}). }`,
-        filter: `FILTER ( !strStarts(lcase(?lit), lcase("${termData.termHLstart()}")) &&
-                       !strEnds(lcase(?lit), lcase("${termData.termHL()}")) ).`,
+        where: `{ (?label ?sc ?lit) text:query (skosxl:literalForm "${termData.starred()}" {language}). }`,
+        filter: `FILTER ( !strStarts(lcase(?lit), lcase("${termData.term}")) &&
+                       !strEnds(lcase(?lit), lcase("${termData.term}")) ).`,
       },
       "contains-ci": {
         score: 0,
-        where: `{ (?label ?sc ?lit) text:query ("(${termData.doubleStarred()}) NOT (${termData.starred()})" "${termData.queryHighlight()}" {language}). }`,
-        filter: `FILTER ( !strEnds(lcase(?lit), lcase("${termData.termHLend()}")) ).`,
+        where: `{ (?label ?sc ?lit) text:query (skosxl:literalForm "(${termData.doubleStarred()}) NOT (${termData.starred()})" {language}). }`,
+        filter: `FILTER ( !strEnds(lcase(?lit), lcase("${termData.term}")) ).`,
       },
     };
     return content[subEntry];
@@ -155,24 +140,30 @@ export function genSearchEntryQuery(searchOptions: SearchOptions): string {
   ) => {
     const subquery = `
         {
-          SELECT ?label ?literal ?l (?sc + ${
+          SELECT ?label ?literal ?l ?context (?sc + ${
             subqueries(match)?.score
-          } as ?score) ?uri ?predicate ?samling ${translate}
+          } as ?score) ?uri ?predicate ${translate}
                  ("${match}" as ?matching)
           WHERE {
-            ${where}
-            ${subqueries(match)?.filter}
-            ${predFilter}
-            ?uri ?predicate ?label;
-                 skosp:memberOf ?s.
-            ${translateOptional}
+            { SELECT * {
+                ${where}
+                ${subqueries(match)?.filter}
+                ?uri ${predFilter} ?label ;
+                     ${context()[1]} ?con .
+                ${context()[0]}
+                ${translateOptional}
+              }
+              ORDER BY DESC(?score) lcase(?literal)
+              LIMIT ${searchOptions.limit}
+              OFFSET ${searchOptions.offset?.[match as Matching] || 0}
+            }
+            ?uri ?predicate ?label .
             BIND ( lang(?lit) as ?l ).
             BIND ( str(?lit) as ?literal ).
-            BIND ( replace(str(?s), "http://.*wiki.terminologi.no/index.php/Special:URIResolver/.*-3A", "") as ?samling).
+            BIND ( replace(str(?con), "${
+              runtimeConfig.public.base
+            }", "") as ?context).
           }
-          ORDER BY DESC(?score) lcase(?literal)
-          LIMIT ${searchOptions.limit}
-          OFFSET ${searchOptions.offset?.[match as Matching] || 0}
         }`;
 
     return subquery;
@@ -195,13 +186,14 @@ export function genSearchEntryQuery(searchOptions: SearchOptions): string {
     PREFIX skosxl: <http://www.w3.org/2008/05/skos-xl#>
     PREFIX skosp: <http://www.data.ub.uib.no/ns/spraksamlingene/skos#>
     PREFIX text: <http://jena.apache.org/text#>
+    PREFIX base: <${runtimeConfig.public.base}>
     PREFIX ns: <http://spraksamlingane.no/terminlogi/named/>`;
 
   const queryEntries = () => `
     #log: ${JSON.stringify(searchOptions)}
     ${queryPrefix()}
   
-    SELECT DISTINCT ?uri ?predicate ?literal ?samling ?score ${translate}
+    SELECT DISTINCT ?uri ?predicate ?literal ?score ?context ${translate}
            (group_concat( ?l; separator="," ) as ?lang)
            ?matching
     WHERE {
@@ -210,7 +202,7 @@ export function genSearchEntryQuery(searchOptions: SearchOptions): string {
         }
       }
     }
-    GROUP BY ?uri ?predicate ?literal ?samling ?score ?matching ${translate}
+    GROUP BY ?uri ?predicate ?literal ?score ?matching ?context ${translate}
     ORDER BY DESC(?score) lcase(?literal) DESC(?predicate)
     LIMIT ${searchOptions.limit}`;
 
