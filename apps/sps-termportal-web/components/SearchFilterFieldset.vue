43a41e4ce706854148b9fe4773eca5a16ef5faac
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-08-28 12:02:27 +0200

    fix(tp): heading of context filter

--- apps/sps-termportal-web/components/SearchFilterFieldset.vue
+++ apps/sps-termportal-web/components/SearchFilterFieldset.vue
@@ -1,12 +1,30 @@
 <template>
   <fieldset class="flex flex-col gap-y-1">
-    <legend class="text-xl">{{ title }}</legend>
+    <legend class="text-xl">{{ displaytitle() }}</legend>
     <slot />
   </fieldset>
 </template>
 
 <script setup lang="ts">
+import { useI18n } from "vue-i18n";
+
+const i18n = useI18n();
+const searchInterface = useSearchInterface();
+
 const props = defineProps({
   title: { type: String, required: true },
+  fkey: { type: String, default: null },
 });
+
+const displaytitle = () => {
+  if (props.fkey === "context") {
+    if (searchInterface.value.useDomain) {
+      return `${i18n.t("global.domain.domain")}`;
+    } else {
+      return `${i18n.t("global.termbase")}`;
+    }
+  } else {
+    return props.title;
+  }
+};
 </script>
