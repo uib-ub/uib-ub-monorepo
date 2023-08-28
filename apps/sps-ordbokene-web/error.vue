a3dbb26310793f3461f9737ef329432fbc9b6d87
Henrik Askjer <Henrik.Askjer@uib.no>
2023-08-28 14:41:47 +0200

    remove unnecessary error handling

--- apps/sps-ordbokene-web/error.vue
+++ apps/sps-ordbokene-web/error.vue
@@ -3,12 +3,11 @@
     <img v-if="error.statusCode == 404" alt="404" src="assets/images/error-illustration.svg"/>
 
     <h1 v-if="error.statusCode == 404" class="mt-10" >{{$t('error.404.title')}}</h1>
-    <h1 v-else-if="error.statuscode==503">{{$t('error.503.title')}}</h1>
-    <h1 v-else>{{$t('error.generic.title')}}</h1>
+    <h1 v-else>{{error.statusCode}}: {{$t('error.generic.title')}}</h1>
     <p class="m-3 text-center">
-      {{$t(`error.${error.statusCode == 404 && 404 || error.statusCode == 503 && 503 || 'generic'}.description`)}}
+      {{$t(`error.${error.statusCode == 404 && 404 || 'generic'}.description`)}}
     </p>
-    <NuxtLink v-if="error.statusCode == 404" class="m-5" to="/">{{$t('home')}}</NuxtLink>
+    <NuxtLink class="m-5" to="/">{{$t('home')}}</NuxtLink>
     </div>
 
 </template>
