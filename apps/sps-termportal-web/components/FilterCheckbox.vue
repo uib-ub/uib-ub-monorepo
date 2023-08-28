ddd5b4c72ea934fb0897d8923379d49ba12f695c
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-08-28 14:09:16 +0200

    fix(tp): display context correctly

--- apps/sps-termportal-web/components/FilterCheckbox.vue
+++ apps/sps-termportal-web/components/FilterCheckbox.vue
@@ -8,26 +8,37 @@
       :value="fvalue"
     />
     <label class="cursor-pointer" :for="`filter-${ftype}-${fvalue}`"
-      >{{ $t("global." + ftype + "." + fvalue) }} ({{
+      >{{ label() }} ({{
         searchDataStats[ftype as keyof SearchDataStats][
           fvalue as LangCode | Samling | Matching | LabelPredicate
         ]
-
       }})</label
     >
   </div>
 </template>
 
 <script setup lang="ts">
+import { useI18n } from "vue-i18n";
 import { SearchDataStats } from "~~/composables/states";
 import { Matching, LabelPredicate } from "~~/utils/vars";
 import { LangCode } from "~/composables/locale";
 import { Samling } from "~~/utils/vars-termbase";
 
+const i18n = useI18n();
 const searchFilterData = useSearchFilterData();
 const searchDataStats = useSearchDataStats();
+const locale = useLocale();
+const lalo = useLazyLocales();
 const props = defineProps({
   ftype: { type: String, required: true },
   fvalue: { type: String, required: true },
 });
+
+const label = () => {
+  if (props.ftype === "context") {
+    return lalo.value[locale.value][props.fvalue];
+  } else {
+    return i18n.t(`global.${props.ftype}.${props.fvalue}`);
+  }
+};
 </script>
