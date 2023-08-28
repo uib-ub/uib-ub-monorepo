7031be98ca2f7823b048ce56bfb18f582a111d51
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-08-28 12:01:51 +0200

    fix(tp): display of searchfilter

--- apps/sps-termportal-web/components/SearchFilter.vue
+++ apps/sps-termportal-web/components/SearchFilter.vue
@@ -2,37 +2,47 @@
   <section v-if="showSearchFilter" id="filterCard" class="mt-2 xl:pr-5">
     <h2 class="pb-2 pt-1 text-2xl">{{ $t("searchFilter.filter") }}</h2>
     <div
-      class="xs:grid-cols-2 grid grid-cols-1 gap-4 rounded border border-gray-300 p-2 md:grid-cols-4 xl:w-[16em] xl:grid-cols-1"
+      class="grid grid-cols-1 gap-4 rounded border border-gray-300 p-2 xs:grid-cols-2 md:grid-cols-4 xl:w-[16em] xl:grid-cols-1"
     >
       <template
         v-for="{ title, key, data } in [
-                { title: $t('global.language'),
-                  key: 'lang',
-                  data: intersectUnique(
-                        languageOrder[$i18n.locale as LocalLangCode],
-                        Object.keys(searchDataStats.lang || {})) },
-                { title: $t('global.termbase'),
-                  key:'samling',
-                  data: Object.keys(searchDataStats.samling || {}).sort()},
-                { title: $t('searchFilter.termproperty'),
-                  key: 'predicate',
-                  data: intersectUnique(
-                    predicateOrder,
-                    Object.keys(searchDataStats.predicate || {})
-                  )
-                },
-                { title: $t('searchFilter.matching'),
-                  key: 'matching',
-                  data: intersectUnique(
-                        matchingOrder,
-                        Object.keys(searchDataStats.matching || {})
-                  )}
-              ]"
+          {
+            title: $t('global.language'),
+            key: 'lang',
+            data: intersectUnique(
+              localeLangOrder,
+              Object.keys(searchDataStats.lang || {})
+            ),
+          },
+          {
+            title: $t('global.context'),
+            key: 'context',
+            fkey: 'context',
+            data: Object.keys(searchDataStats.context || {}).sort(),
+          },
+          {
+            title: $t('searchFilter.termproperty'),
+            key: 'predicate',
+            data: intersectUnique(
+              predicateOrder,
+              Object.keys(searchDataStats.predicate || {})
+            ),
+          },
+          {
+            title: $t('searchFilter.matching'),
+            key: 'matching',
+            data: intersectUnique(
+              matchingOrder,
+              Object.keys(searchDataStats.matching || {})
+            ),
+          },
+        ]"
         :key="title"
       >
         <SearchFilterFieldset
           v-if="data.length > 0"
           :title="title"
+          :fkey="key"
           class="pt-1"
         >
           <FilterCheckbox
@@ -49,8 +59,7 @@
 </template>
 
 <script setup lang="ts">
-import { LocalLangCode } from "~~/utils/vars-language";
-
 const showSearchFilter = useShowSearchFilter();
 const searchDataStats = useSearchDataStats();
+const localeLangOrder = useLocaleLangOrder();
 </script>
