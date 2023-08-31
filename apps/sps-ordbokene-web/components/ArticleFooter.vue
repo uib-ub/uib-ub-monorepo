0102523c972139432d627833dca435ffb27a63e1
Henrik Askjer <Henrik.Askjer@uib.no>
2023-08-31 13:58:54 +0200

    tweak copy button accessibility

--- apps/sps-ordbokene-web/components/ArticleFooter.vue
+++ apps/sps-ordbokene-web/components/ArticleFooter.vue
@@ -2,7 +2,7 @@
 <div class="flex mt-4 mb-4 md:mb-0 flex-wrap gap-y-6">
   <client-only>
     <div role="toolbar" class="flex justify-center sm:justify-normal gap-2 flex-wrap gap-y-2">
-    <button v-if="showLinkCopy && (!webShareApiSupported || $route.name== 'article')" class="btn btn-borderless px-3" @click="copy_link">
+    <button v-if="showLinkCopy" class="btn btn-borderless px-3" @click="copy_link" v-bind:class="{'hidden xl:block': store.dict == 'bm,nn' && $route.name!= 'article', 'hidden md:block': store.dict != 'bm,nn' && $route.name != 'article'}">
       <Icon :name="store.copied == create_link() ? 'bi:clipboard-check-fill' : 'bi:clipboard'" class="mr-3 mb-1 text-primary"/>
       <span>{{ store.copied == create_link() ? $t('article.link_copied') : $t('article.copy_link', 1, { locale: content_locale }) }} </span>
     </button>
