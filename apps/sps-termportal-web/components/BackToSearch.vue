9001fe7c2a00f2a656a8803abd0157ae6b07ebe5
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-09-01 10:01:53 +0200

    feat(tp): add component for back to search

---
+++ apps/sps-termportal-web/components/BackToSearch.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex h-9">
+    <AppLink class="group flex items-center space-x-2 text-lg" to="/search">
+      <Icon
+        name="ion:return-up-back-sharp"
+        size="1.7em"
+        aria-hidden="true"
+        class="h-7 w-12 rounded bg-tpblue-400 text-white group-hover:bg-blue-700"
+      ></Icon>
+      <div class="text-gray-500 group-hover:text-black group-hover:underline">
+        {{ $t("id.tilbake") }}
+      </div></AppLink
+    >
+  </div>
+</template>
