b0bdbf2e73b000a6879c2d79233c7db963f02ed6
Henrik Askjer <Henrik.Askjer@uib.no>
2023-09-01 13:26:48 +0200

    WIP restyle advanced search

--- apps/sps-ordbokene-web/components/AdvancedSearchForm.vue
+++ apps/sps-ordbokene-web/components/AdvancedSearchForm.vue
@@ -1,23 +1,24 @@
 <template>
   <div class="my-1 md:mt-0 pb-6">
     <form  @submit.prevent="submitForm" ref="form" class="flex flex-col gap-4">
-      <div class="grid gap-3 gap-y-6 md:grid-cols-4 lg:grid-cols-5">
-      <fieldset class="md:col-span-2 grid xl:grid-cols-3 gap-8 sm:gap-3 border border-1 px-6 pb-4 pt-2 rounded max-md:basis-full">
-      <legend>{{$t('options.dict')}}</legend>
-        <FormRadio v-for="(item, idx) in dicts" :key="store.dict + idx" :value="item" name="dict" :labelId="'dict-radio-'+idx" :current="store.dict" @submit="update_dict">
-          {{$t(`dicts_short.${item}`)}}
-        </FormRadio>
-      </fieldset>
-      
-      <fieldset class="md:col-span-2 grid xl:grid-cols-3 gap-8 sm:gap-3 border border-1 px-6 pb-4 pt-2 rounded max-md:basis-full">
-      <legend>{{$t('options.scope.title')}}</legend>
-        <FormRadio v-for="(item, idx) in ['e', 'ei', 'eif']" :key="store.scope + idx" :value="item" name="scope" :labelId="'scope-radio-'+idx" :current="store.scope" @submit="update_scope">
-          {{$t(`options.scope.value.${item}`)}}
-        </FormRadio>
-      </fieldset>  
 
-      <div class="md:mx-6 md:col-span-1 flex lg:flex-col xl:flex-row justify-between">
-        <fieldset class="rounded min-w-max">
+      <div class="grid gap-3 md:grid-cols-2 lg:grid-cols-5">
+        <fieldset class="gap-4 lg:col-span-2 grid xl:grid-cols-3 sm:gap-3 border border-1 px-6 pb-4 pt-2 rounded">
+          <legend>{{$t('options.dict')}}</legend>
+          <FormRadio v-for="(item, idx) in dicts" :key="store.dict + idx" :value="item" name="dict" :labelId="'dict-radio-'+idx" :current="store.dict" @submit="update_dict">
+            {{$t(`dicts_short.${item}`)}}
+          </FormRadio>
+        </fieldset>
+        
+        <fieldset class="gap-4 lg:col-span-2 grid xl:grid-cols-3 sm:gap-3 border border-1 px-6 pb-4 pt-2 rounded">
+          <legend>{{$t('options.scope.title')}}</legend>
+          <FormRadio v-for="(item, idx) in ['e', 'ei', 'eif']" :key="store.scope + idx" :value="item" name="scope" :labelId="'scope-radio-'+idx" :current="store.scope" @submit="update_scope">
+            {{$t(`options.scope.value.${item}`)}}
+          </FormRadio>
+        </fieldset>  
+
+      <div class="grid sm:grid-cols-2 xl:grid-cols-1 xl:grid-rows-3 lg:grid-cols-1 md:col-span-2 lg:col-span-1 gap-2 sm:gap-4 lg:gap-0">
+        <fieldset class="rounded xl:row-span-2">
           <legend id="pos-legend" class="bg-tertiary ">{{$t('pos')}}:</legend>
           <div class="!py-3 duration-200 ">
           <select aria-labelledby="pos-legend" name="pos" class="bg-tertiary w-full border border-1 py-1 px-2 pr-2 mr-2" @change="update_pos" v-bind:class="{not_null: store.pos}">
@@ -25,20 +26,22 @@
           </select>
           </div>
         </fieldset>
-        <button class="btn btn-primary ml-auto" v-if="!(store.pos == null &&  store.scope == 'ei' && store.dict == 'bm,nn')" type="reset" @click="reset"> <Icon name="bi:trash" size="1.25em" class="mr-3" />{{$t('reset')}}</button>
+
+          <button class="btn xl:row-span-1 sm:mt-6 lg:mt-0 sm:mb-2 md:mb-0 py-2 md:py-0" v-if="!(store.pos == null &&  store.scope == 'ei' && store.dict == 'bm,nn')" type="reset" @click="reset"> <Icon name="bi:trash" size="1.25em" class="mr-3" />{{$t('reset')}}</button>
+          
       </div>
+      
     </div>
       
-        <div class="grid grid-cols-8 gap-4 xl:gap-4">
-              <div class="col-span-8 lg:col-span-4 xl:col-span-5" :class="{activeAutocomplete: store.autocomplete && store.autocomplete.length}">
+        <div class="grid grid-cols-10 gap-4 xl:gap-4">
+ 
+          <div class="col-span-10 lg:col-span-6 xl:col-span-7" :class="{activeAutocomplete: store.autocomplete && store.autocomplete.length}">
             <Autocomplete  v-on:dropdown-submit="submitForm"/>
           </div>
-          <div class="col-span-8 lg:col-span-4 xl:col-span-3 grid grid-cols-2 gap-4">
 
-            <button v-if="store.q" class="btn p-2 px-4 col-span-2 sm:col-span-1" type="button" @click="(settings.listView = !settings.listView)" ><Icon :name='settings.listView ? "bi:list" : "bi:file-text"' class="mb-1 mr-2"/>{{settings.listView ? $t('show_articles') : $t('show_list',store.dict==='bm,nn'? 0 : 1)}}</button>
-
-
-            <button class="btn p-2 px-4 col-span-2 sm:col-span-1" :aria-expanded="mini_help" aria-controls="advanced-info" type="button" @click="mini_help = !mini_help"><Icon :name="mini_help ? 'bi:x-lg' : 'bi:question-lg'" class="mb-1 mr-2"/>{{$t('advanced_help')}}</button>
+          <div class="col-span-10 lg:col-span-4 xl:col-span-3 grid grid-cols-2 gap-4">
+            <button v-if="store.q" class="btn p-2 px-4 col-span-4 sm:col-span-1" type="button" @click="(settings.listView = !settings.listView)" ><Icon :name='settings.listView ? "bi:list" : "bi:file-text"' class="mb-1 mr-2"/>{{settings.listView ? $t('show_articles') : $t('show_list',store.dict==='bm,nn'? 0 : 1)}}</button>
+            <button class="btn p-2 px-4 col-span-4 sm:col-span-1" :aria-expanded="mini_help" aria-controls="advanced-info" type="button" @click="mini_help = !mini_help"><Icon :name="mini_help ? 'bi:x-lg' : 'bi:question-lg'" class="mb-1 mr-2"/>{{$t('advanced_help')}}</button>
           </div>
         </div>
       
