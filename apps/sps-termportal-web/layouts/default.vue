165a5c53a125916e3e13defad93746193fd4f6e4
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-09-05 10:40:53 +0200

    fix(tp): breakpoints for sidebar and landing page

--- apps/sps-termportal-web/layouts/default.vue
+++ apps/sps-termportal-web/layouts/default.vue
@@ -8,9 +8,9 @@
       <div
         class="flex-1"
         :class="{
-          'max-w-screen-2xl px-2 pb-3 pt-2 xs:pb-4 sm:px-3 md:px-4 lg:px-5 lg:pl-0':
+          'max-w-screen-2xl px-2 pb-3 pt-2 xs:pb-4 sm:px-3 md:px-4 lg:px-5 xl:pl-0':
             route.path !== '/',
-          'container mb-[10vh] max-w-5xl p-5': route.path === '/',
+          'container mb-[10vh] max-w-[73em] p-5': route.path === '/',
         }"
       >
         <header>
