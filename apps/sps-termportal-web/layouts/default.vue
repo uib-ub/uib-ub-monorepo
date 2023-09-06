b35d69e763ed171ae336073565be85787e4ec75f
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-09-06 10:21:00 +0200

    fix(tp): adapt landing page layout to localize text widths

--- apps/sps-termportal-web/layouts/default.vue
+++ apps/sps-termportal-web/layouts/default.vue
@@ -10,7 +10,9 @@
         :class="{
           'max-w-screen-2xl px-2 pb-3 pt-2 xs:pb-4 sm:px-3 md:px-4 lg:px-5 xl:pl-0':
             route.path !== '/',
-          'container mb-[10vh] max-w-[73em] p-5': route.path === '/',
+          'container mb-[10vh] max-w-[70em] p-5': route.path === '/',
+          'max-w-[69em]': locale === 'nn',
+          'max-w-[78em]': locale === 'en'
         }"
       >
         <header>
@@ -31,4 +33,5 @@
 
 <script setup>
 const route = useRoute();
+const locale = useLocale();
 </script>
