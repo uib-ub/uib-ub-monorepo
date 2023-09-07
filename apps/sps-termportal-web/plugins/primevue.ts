a79b42afc290d773625c54783c6fd69438bb3743
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-09-07 15:57:46 +0200

    feat(tp): import accordion tab

--- apps/sps-termportal-web/plugins/primevue.ts
+++ apps/sps-termportal-web/plugins/primevue.ts
@@ -1,5 +1,6 @@
 import PrimeVue from "primevue/config";
 import Accordion from "primevue/accordion";
+import AccordionTab from "primevue/accordiontab";
 import Button from "primevue/button";
 import Dropdown from "primevue/dropdown";
 import Menu from "primevue/menu";
@@ -9,6 +10,7 @@ import RadioButton from "primevue/radiobutton";
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(PrimeVue, { ripple: true });
   nuxtApp.vueApp.component("Accordion", Accordion);
+  nuxtApp.vueApp.component("AccordionTab", AccordionTab);
   nuxtApp.vueApp.component("Button", Button);
   nuxtApp.vueApp.component("Dropdown", Dropdown);
   nuxtApp.vueApp.component("Menu", Menu);
