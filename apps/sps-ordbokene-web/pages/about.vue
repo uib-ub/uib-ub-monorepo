cb7c4b272b768ca8e92cc8679224dbbae1a14436
Henrik Askjer <Henrik.Askjer@uib.no>
2023-09-11 13:44:54 +0200

    WIP about page makdown

--- apps/sps-ordbokene-web/pages/about.vue
+++ apps/sps-ordbokene-web/pages/about.vue
@@ -1,26 +1,6 @@
 <template>
 <main id="main" tabindex="-1" class="secondary-page">
-        <ContentRenderer :value="data.intro">
-          <ContentRendererMarkdown :value="data.intro" :components="{h1: 'h2'}" />
-          <template #empty>
-            <p>No content found.</p>
-          </template>
-        </ContentRenderer>
-
-        <ContentRenderer v-for="(section, index) in data.sections" :key="index" :value="section" :components="{h2: 'h3'}" >
-          <Collapsible :id="section._path.slice(section._path.lastIndexOf('/') + 1)" is="h3" :header="section.body.children[0].children[0].value">
-          <ContentRendererMarkdown :value="{...section, body: {...section.body, children: section.body.children.slice(1)}}" />
-          </Collapsible>
-          
-          <template #empty>
-            <p>No content found.</p>
-          </template>
-          
-        </ContentRenderer>
-
-        {{data.sections}}
-
-
+        <NuxtPage/>
 
 </main>
 </template>
@@ -36,10 +16,6 @@ const i18n = useI18n()
 const path = `${i18n.locale.value}/about/`
 
 const { data } = await useAsyncData(`content-${path}`, async () => {
-  //let sections = await queryContent(i18n.locale.value, "about").find()
-  //const titles = sections.map(item => item.body.children[0])
-
-  
   return {
     intro: await queryContent(i18n.locale.value, "about").findOne(),
     sections: await queryContent(i18n.locale.value, "about").skip(1).find()
@@ -51,13 +27,6 @@ useHead({
     title: i18n.t('about')
 })
 
-
-
-const components = {
-    h1: "h2",
-    h2: "h3",
-    h3: "h4",
-}
 </script>
 
 
