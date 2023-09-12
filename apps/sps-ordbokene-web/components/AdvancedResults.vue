bc369b3f4dd2862123b640b94f6bcc13510bfc86
Henrik Askjer <Henrik.Askjer@uib.no>
2023-09-12 18:34:48 +0200

    refactor advanced search

--- apps/sps-ordbokene-web/components/AdvancedResults.vue
+++ apps/sps-ordbokene-web/components/AdvancedResults.vue
@@ -1,43 +1,22 @@
 <template>
     <div v-bind:class="{'list': settings.listView}">     
     <Spinner v-if="pending"/>
-
     <div v-if="!pending && !error && articles && articles.meta" >
-    <!-- todo: accessibility info -->
-
-    <div class="gap-3 lg:gap-8 grid lg:grid-cols-2" v-if="route.query.dict == 'bm,nn' ">
-      <section class="lg:grid-cols-6" aria-labelledby="bm_heading">
+    <div  v-bind:class="{'gap-2 lg:gap-8 grid lg:grid-cols-2': dicts.length == 2}">
+      <section class="lg:grid-cols-6" v-for="dict in dicts" :key="dict" :aria-labeledby="dict+'_heading'">
         <div class="py-2 px-2">
-          <h2 id="bm_heading" class="">{{$t('dicts.bm')}} 
-            <span class="result-count-text">{{articles.meta.bm.total}}</span>
+          <h2 :id="dict+'_heading'" class="">{{$t('dicts.'+dict)}} 
+            <span class="result-count-text">{{articles.meta[dict].total}}</span>
             <span class="sr-only">{{$t('notifications.keywords')}}</span>
           </h2>
         </div>
         <client-only>
-          <MinimalSuggest v-if="articles.meta.bm.total == 0" dict="bm"/>
+          <MinimalSuggest v-if="articles.meta[dict].total == 0" :dict="dict"/>
         </client-only>
-        <component :is="settings.listView ? 'ol' : 'div'" class="article-column">
-          <component v-for="(article_id, idx) in sliced_articles.bm" :key="article_id" :is="settings.listView ? 'li' : 'div'">
-            <NuxtErrorBoundary v-on:error="article_error($event, article_id, 'bm')">
-              <Article :article_id="article_id" dict="bm" :idx="idx" :list="settings.listView"/>
-            </NuxtErrorBoundary>
-          </component>
-        </component>
-      </section>
-      <section class="lg:grid-cols-6" aria-labelledby="nn_heading">
-        <div class="py-2 px-2">
-          <h2 id="nn_heading" class="">{{$t('dicts.nn')}} 
-            <span class="result-count-text">{{articles.meta.nn.total}}</span>
-            <span class="sr-only">{{$t('notifications.keywords')}}</span>
-          </h2>
-        </div>
-        <client-only>
-          <MinimalSuggest v-if="articles.meta.nn.total == 0" dict="nn"/>
-        </client-only>
-        <component class="article-column" :is="settings.listView ? 'ol' : 'div'">
-          <component v-for="(article_id, idx) in sliced_articles.nn" :key="article_id" :is="settings.listView ? 'li' : 'div'">
-            <NuxtErrorBoundary v-on:error="article_error($event, article_id, 'nn')">
-              <Article :article_id="article_id" dict="nn" :idx="idx" :list="settings.listView"/>
+        <component v-if="articles.meta[dict].total > 0" :is="settings.listView ? 'ol' : 'div'" class="article-column">
+          <component v-for="(article_id, idx) in articles.articles[dict].slice(offset, offset + perPage)" :key="article_id" :is="settings.listView ? 'li' : 'div'">
+            <NuxtErrorBoundary v-on:error="article_error($event, article_id, dict)">
+              <Article :article_id="article_id" :dict="dict" :idx="idx" :list="settings.listView"/>
             </NuxtErrorBoundary>
           </component>
         </component>
@@ -45,45 +24,6 @@
   </div>
 
     
-    <div v-if="route.query.dict != 'bm,nn' ">
-      <section v-if="route.query.dict == 'bm' && articles.meta.bm" aria-labelledby="bm_heading">
-        <div class="py-2 px-2">
-          <h2 id="bm_heading" class="">{{$t('dicts.bm')}} 
-            <span class="result-count-text">{{articles.meta.bm.total}}</span>
-            <span class="sr-only">{{$t('notifications.keywords')}}</span>
-          </h2>
-        </div>
-        <client-only>
-          <MinimalSuggest v-if="articles.meta.bm.total == 0" dict="bm"/>
-        </client-only>
-        <component class="article-column" :is="settings.listView ? 'ol' : 'div'">
-          <component v-for="(article_id, idx) in sliced_articles.bm" :key="article_id" :is="settings.listView ? 'li' : 'div'">
-            <NuxtErrorBoundary v-on:error="article_error($event, article_id, 'bm')">
-              <Article :article_id="article_id" dict="bm" :idx="idx" :list="settings.listView"/>
-            </NuxtErrorBoundary>
-          </component>
-        </component>
-      </section>
-      <section v-if="(route.query.dict == 'nn' )  && articles.meta.nn" aria-labelledby="nn_heading">
-        <div class="py-2 px-2">
-          <h2 id="nn_heading" class="">{{$t('dicts.nn')}} 
-            <span class="result-count-text">{{articles.meta.nn.total}}</span>
-            <span class="sr-only">{{$t('notifications.keywords')}}</span>
-          </h2>
-        </div>
-        <client-only>
-          <MinimalSuggest v-if="articles.meta.nn.total == 0" dict="nn"/>
-        </client-only>
-        <component class="article-column" :is="settings.listView ? 'ol' : 'div'">
-          <component v-for="(article_id, idx) in sliced_articles.nn" :key="article_id" :is="settings.listView ? 'li' : 'div'">
-            <NuxtErrorBoundary v-on:error="article_error($event, article_id, 'nn')">
-              <Article :article_id="article_id" dict="nn" :idx="idx" :list="settings.listView"/>
-            </NuxtErrorBoundary>
-          </component>
-        </component>
-      </section>
-      
-    </div>
     <div class ="flex flex-col">
     <div v-if="pages > 1" class="p-2 py-6 md:p-8 flex md:flex-wrap justify-center flex md:gap-4">
       <NuxtLink :to="{query: {...route.query, ...{page: page -1 }}}">
@@ -142,6 +82,12 @@ const { pending, error, refresh, data: articles } = await useFetch(() => `api/ar
           }
         })
 
+const dicts = computed(()=> {
+  let currentDict = route.query.dict
+  return currentDict == 'bm,nn' ? ["bm", "nn"] : [currentDict]
+})
+
+
 if (error.value && store.endpoint == "https://oda.uib.no/opal/prod/`") {
   store.endpoint = `https://odd.uib.no/opal/prod/`
   console.log("ERROR", error.value)
@@ -157,18 +103,9 @@ const pages = computed(() => {
 
 
 const offset = computed(() => {
-  return perPage.value * page.value
+  return perPage.value * (page.value - 1)
   
 })
-
-
-const sliced_articles = computed(() => {
-  return {
-    bm: articles.value.articles.bm ? articles.value.articles.bm.slice(offset.value, offset.value + perPage.value) : [],
-    nn: articles.value.articles.nn ? articles.value.articles.nn.slice(offset.value, offset.value + perPage.value) : []
-  }
-})
-
 
 
 const update_perPage = (event) => {
