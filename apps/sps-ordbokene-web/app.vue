9f9a0aa9ddf8e3f6307d6b8d78659e9591b56279
Henrik Askjer <Henrik.Askjer@uib.no>
2023-09-18 19:51:59 +0200

    fix subpage return link

--- apps/sps-ordbokene-web/app.vue
+++ apps/sps-ordbokene-web/app.vue
@@ -1,10 +1,11 @@
 <template>
   <a class="bg-tertiary-darken1 text-center z-1000 text-anchor sr-only text-xl font-semibold underline w-full  !focus-within:p-10 focus:not-sr-only focus:absolute focus:min-w-screen" href="#main">Til innhold</a>
   <Header/>
+  
 <nav :aria-label="$t('breadcrumbs')" class="ord-container justify-start mt-3 mb-2 pl-3 flex gap-4" v-if="!['welcome', 'search', 'article', 'word'].includes($route.name)">
   <NuxtLink v-if="store.searchUrl" :to="store.searchUrl"> <Icon name="bi:arrow-left" size="1.25em" class="mb-1 mr-1 text-primary"/>{{$t('notifications.back')}}</NuxtLink>
   <NuxtLink v-else to="/"><Icon name="bi:arrow-left" size="1.25em" class="mb-1 mr-1 text-primary"/>{{$t('home')}}</NuxtLink>
-  <NuxtLink v-if="$route.params.slug"  :to="$route.matched[0].path"><Icon name="bi:arrow-left" size="1.25em" class="mb-1 mr-1 text-primary"/>{{$t($route.matched[0].children[0].name)}}</NuxtLink>
+  <NuxtLink v-if="$route.params.slug"  :to="$route.fullPath.slice(0, route.fullPath.lastIndexOf('/'))"><Icon name="bi:arrow-left" size="1.25em" class="mb-1 mr-1 text-primary"/>{{$t($route.matched[0].children[1].name)}}</NuxtLink>
 </nav>
     <NuxtPage @click="menu_expanded=false"
               v-bind:class="{'welcome': route.name == 'welcome'}"/>
