74b12e1128df2a1512cbbc16f2eefeefea589005
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-09-19 10:21:01 +0200

    feat(tp): improve sizes, spacing etc for logos

--- apps/sps-termportal-web/components/NavBar.vue
+++ apps/sps-termportal-web/components/NavBar.vue
@@ -7,17 +7,21 @@
     <div class="mx-auto flex h-full grow items-center justify-between">
       <div class="grow">
         <div v-if="context !== 'minimal'" class="flex grow items-center">
-          <SideBar class="w-12">
+          <SideBar class="w-16">
             <AppLink
               id="anchor"
               to="/"
               aria-label="Termportalen.no"
-              class="tp-hover-focus h-10 py-1 mx-1 flex justify-center border-transparent"
+              class="tp-hover-focus mx-1.5 flex h-10 justify-center border-transparent py-1"
             >
-              <div class="-mt-1 hidden w-[10.5em] xl:block">
-                <img class="" src="/logo-wordmark-termportalen.svg" aria-hidden="true" />
+              <div class="-mt-[2px] hidden w-[9.5em] xl:block">
+                <img
+                  class=""
+                  src="/logo-wordmark-termportalen.svg"
+                  aria-hidden="true"
+                />
               </div>
-              <div class="block w-full xl:hidden">
+              <div class="block w-[2.35em] xl:hidden">
                 <img src="/logo-symbol-termportalen.svg" aria-hidden="true" />
               </div>
             </AppLink>
