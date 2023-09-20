ec4b7dbbd08b671b7d5ee49ef8a97f8678019938
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-09-20 11:33:00 +0200

    fix(tp): update news

--- apps/sps-termportal-web/components/NewsTP.vue
+++ apps/sps-termportal-web/components/NewsTP.vue
@@ -7,58 +7,49 @@
       <div class="border border-gray-300"></div>
       <div v-if="locale === 'nb'">
         <div class="flex flex-wrap">
-          <dt class="font-semibold">8. september:</dt>
-          <dd>
-            <span>
-              Seminar om norsk fagspråk, handlingsplan og terminologiarbeid i
-              universitets- og høgskolesektoren
-              <AppLink
-                class="underline hover:decoration-2"
-                to="https://www.uib.no/aktuelt/164397/fr%C3%A5-ord-til-handling-tid-terminologiarbeid-i-akademia"
-              >
-                Frå ord til handling: Tid for terminologiarbeid i akademia
-              </AppLink>
-              Seminaret blir strømmet og tegnspråktolket.
-            </span>
-          </dd>
+          <span>
+            Den 8. september arrangerte Termportalen seminar om norsk fagspråk,
+            handlingsplan og terminologiarbeid i universitets- og
+            høgskolesektoren
+            <AppLink
+              class="underline hover:decoration-2"
+              to="https://www.uib.no/aktuelt/164397/fr%C3%A5-ord-til-handling-tid-terminologiarbeid-i-akademia"
+            >
+              Frå ord til handling: Tid for terminologiarbeid i akademia </AppLink
+            >. Her kan du se opptaket av seminaret.
+          </span>
         </div>
       </div>
       <div v-else-if="locale === 'nn'">
-        <dl class="flex flex-wrap">
-          <dt class="font-semibold">8. september:</dt>
-          <dd>
-            Seminar om norsk fagspråk, handlingsplan og terminologiarbeid i
-            universitets- og høgskulesektoren
-            <AppLink
-              class="underline hover:decoration-2"
-              to="https://www.uib.no/aktuelt/164397/fr%C3%A5-ord-til-handling-tid-terminologiarbeid-i-akademia"
-            >
-              Frå ord til handling: Tid for terminologiarbeid i akademia
-            </AppLink>
-            Seminaret blir strøyma og teiknspråktolka.
-          </dd>
-        </dl>
+        <span>
+          Den 8. september arrangerte Termportalen seminar om norsk fagspråk,
+          handlingsplan og terminologiarbeid i universitets- og høgskulesektoren
+          <AppLink
+            class="underline hover:decoration-2"
+            to="https://www.uib.no/aktuelt/164397/fr%C3%A5-ord-til-handling-tid-terminologiarbeid-i-akademia"
+          >
+            Frå ord til handling: Tid for terminologiarbeid i akademia </AppLink
+          >. Her kan du sjå opptaket av seminaret.
+        </span>
       </div>
       <div v-else>
-        <dl class="flex flex-wrap">
-          <dt class="font-semibold">8 September:</dt>
-          <dd>
-            Seminar on Norwegian language for specific purposes, action plans
-            and terminology work in Norwegian higher education
-            <AppLink
-              class="underline hover:decoration-2"
-              to="https://www.uib.no/aktuelt/164397/fr%C3%A5-ord-til-handling-tid-terminologiarbeid-i-akademia"
-            >
-              Frå ord til handling: Tid for terminologiarbeid i akademia
-            </AppLink>
-            The seminar will be streamed and sign interpreted [in Norwegian].
-          </dd>
-        </dl>
+        <span>
+          On 8 September, Termportalen hosted a seminar on Norwegian language
+          for specific purposes, action plans and terminology work in Norwegian
+          higher education
+          <AppLink
+            class="underline hover:decoration-2"
+            to="https://www.uib.no/aktuelt/164397/fr%C3%A5-ord-til-handling-tid-terminologiarbeid-i-akademia"
+          >
+            Frå ord til handling: Tid for terminologiarbeid i akademia </AppLink
+          >. The recording of the seminar may be viewed here [in Norwegian].
+        </span>
       </div>
       <div class="border border-gray-300"></div>
     </div>
   </div>
 </template>
+
 <script setup lang="ts">
 const locale = useLocale();
 </script>
