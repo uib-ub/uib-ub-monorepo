71225926ad958195d5b313c622218d46b19c4262
Henrik Askjer <Henrik.Askjer@uib.no>
2023-09-25 10:19:07 +0200

    cleanup

--- apps/sps-ordbokene-web/nuxt.config.ts
+++ apps/sps-ordbokene-web/nuxt.config.ts
@@ -1,7 +1,6 @@
 import { resolve, dirname } from 'node:path'
 import { fileURLToPath } from 'url'
 import VueI18nVitePlugin from '@intlify/unplugin-vue-i18n/vite'
-import { ProseH2 } from '.nuxt/components'
 
 export default defineNuxtConfig({
   css: ['~/assets/fonts/fonts.css'],
