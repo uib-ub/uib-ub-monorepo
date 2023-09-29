c84de5166a60b07cda36d4e20dea01229f9b173d
Henrik Askjer <Henrik.Askjer@uib.no>
2023-09-29 13:27:38 +0200

    debug middleware error

--- apps/sps-ordbokene-web/middleware/middleware.global.ts
+++ apps/sps-ordbokene-web/middleware/middleware.global.ts
@@ -5,21 +5,28 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
   const session = useSessionStore()
 
   // Redirect if cookie or browser language don't match route locale
-  const locale_cookie = useCookie('currentLocale')
-  if (locale_cookie.value) {
-    if (to.params.locale && locale_cookie.value != to.params.locale) {
-      return navigateTo(localizeUrl(to.fullPath, locale_cookie.value))
+  try {
+    const locale_cookie = useCookie('currentLocale')
+    if (locale_cookie.value) {
+      if (to.params.locale && locale_cookie.value != to.params.locale) {
+        return navigateTo(localizeUrl(to.fullPath, locale_cookie.value))
+      }
     }
+    else if (navigator.language == 'nb' && to.params.locale != 'nob') {
+      return navigateTo(localizeUrl(to.fullPath, 'nob'))
+    }
+    else if (navigator.language == 'nn' && to.params.locale != 'nno') {
+      return navigateTo(localizeUrl(to.fullPath, 'nno'))
+    }
+    else if (navigator.language == 'uk' && to.params.locale != 'ukr') {
+      return navigateTo(localizeUrl(to.fullPath, 'ukr'))
+    }
+
   }
-  else if (navigator.language == 'nb' && to.params.locale != 'nob') {
-    return navigateTo(localizeUrl(to.fullPath, 'nob'))
+   catch (exception) {
+    console.log("MIDDLEWARE ERROR", exception)
   }
-  else if (navigator.language == 'nn' && to.params.locale != 'nno') {
-    return navigateTo(localizeUrl(to.fullPath, 'nno'))
-  }
-  else if (navigator.language == 'uk' && to.params.locale != 'ukr') {
-    return navigateTo(localizeUrl(to.fullPath, 'ukr'))
-  }
+
 
   
   // Load concepts for definition expansion
