4b35aec5bcb3dee57b29e64d82c3ba9b4a275cd6
Henrik Askjer <Henrik.Askjer@uib.no>
2023-10-09 13:40:21 +0200

    cleanup

--- apps/sps-ordbokene-web/plugins/primevue.ts
+++ apps/sps-ordbokene-web/plugins/primevue.ts
@@ -1,10 +1,8 @@
 import PrimeVue from "primevue/config";
-import Button from "primevue/button";
 import Menu from "primevue/menu";
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(PrimeVue, { ripple: false });
-  nuxtApp.vueApp.component("Button", Button);
   nuxtApp.vueApp.component("Menu", Menu);
 
 });
