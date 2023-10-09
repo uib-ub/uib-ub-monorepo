2ef7922e08d0e795e3982502826271e9fb97bec7
Henrik Askjer <Henrik.Askjer@uib.no>
2023-10-09 18:43:21 +0200

    remove collapsible

--- apps/sps-ordbokene-web/components/custom-pages/content-accordions.vue
+++ apps/sps-ordbokene-web/components/custom-pages/content-accordions.vue
@@ -7,30 +7,17 @@
           </template>
         </ContentRenderer>
 
-    <input/>
     <ContentNavigation v-if="$route.name != 'contact'" v-slot="{ navigation }" :query="data.sections" >
         <template v-for="loc in navigation" :key="loc._path" >
-          <template v-if="loc.children[0].children">
-          <div v-for="subpage in loc.children[0].children.slice(1, loc.children[0].children.length) " :key="subpage._path">
-          <Collapsible :id="subpage._path.slice(subpage._path.lastIndexOf('/') + 1)" is="h3" :header="subpage.title">
-           <CollapsibleDocument :path="subpage._path"/>
-          </Collapsible>
-          </div>
-          </template>
+          <nav v-if="loc.children[0].children" class="mt-8">
+          <ol  class="w-full">
+          <li class=" text-left w-full content-linkt-item" v-for="subpage in loc.children[0].children.slice(1, loc.children[0].children.length) " :key="subpage._path">
+            <NuxtLink class="w-full link-header !border-none flex justify-between hover:bg-gray-100 px-5 pt-3 pb-4" :to="subpage._path">{{subpage.title}} <Icon class="self-end text-gray-700" name="bi:chevron-right"/></NuxtLink>
+          </li>
+          </ol>
+          </nav>
         </template>
     </ContentNavigation>
-      
-        <!--
-    <ContentRenderer v-for="(section, index) in data.sections" :key="index" :value="section">
-          <Collapsible :id="section._path.slice(section._path.lastIndexOf('/') + 1)" is="h3" :header="section.body.children[0].children[0].value">
-          
-          </Collapsible>
-          
-          <template #empty>
-            <p>No content found.</p>
-          </template>
-          
-      </ContentRenderer>-->
 </div>
 </template>
 
@@ -54,3 +41,12 @@ useHead({
 })
 
 </script>
+
+
+<style scoped>
+
+.content-linkt-item:not(:last-child) {
+          @apply !border-b border-solid border-gray-100;
+          
+      }
+</style>
