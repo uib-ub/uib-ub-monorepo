92a4f45624c502cb81a0b0890d8c7ad7b787d37e
Henrik Askjer <Henrik.Askjer@uib.no>
2023-10-10 17:58:28 +0200

    refactor contact-us

--- apps/sps-ordbokene-web/components/custom-pages/contact-view.vue
+++ apps/sps-ordbokene-web/components/custom-pages/contact-view.vue
@@ -1,7 +1,7 @@
 <template>
 <div>
-    <ContentRenderer :value="data">
-          <ContentRendererMarkdown :value="data" :components="{h1: 'h2'}" />
+    <ContentRenderer :value="contact">
+          <ContentRendererMarkdown :value="contact" :components="{h1: 'h2'}" />
           <template #empty>
             <p>No content found.</p>
           </template>
@@ -14,11 +14,10 @@ import { useI18n } from 'vue-i18n'
 const i18n = useI18n()
 import { useRoute } from 'vue-router'
 const route = useRoute()
-
-const { data } = await useAsyncData(route.fullPath, async () => await queryContent(i18n.locale.value, route.name).findOne(), {watch: i18n.locale});
+const contact = await queryContent(i18n.locale.value, "contact").findOne()
 
 useHead({
-    title: i18n.t(route.name)
+    title: i18n.t("contact")
 })
 
 </script>
