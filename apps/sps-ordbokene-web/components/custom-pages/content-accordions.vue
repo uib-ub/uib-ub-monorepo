64f75fccccbe4a974be71c61eeb38322de1ad5d4
Henrik Askjer <Henrik.Askjer@uib.no>
2023-10-12 10:49:05 +0200

    fix stringify error

--- apps/sps-ordbokene-web/components/custom-pages/content-accordions.vue
+++ apps/sps-ordbokene-web/components/custom-pages/content-accordions.vue
@@ -28,7 +28,7 @@ import { useRoute } from 'vue-router'
 const route = useRoute()
 
 const { data: intro } = await useAsyncData('intro_' + i18n.locale.value, () => queryContent(i18n.locale.value, route.name).findOne())
-const { data: sections } = await useAsyncData('sections_' + i18n.locale.value, () => queryContent(i18n.locale.value, route.name))
+const { data: sections } = await useAsyncData('sections_' + i18n.locale.value, () => queryContent(i18n.locale.value, route.name).find())
 
 useHead({
     title: i18n.t(route.name)
