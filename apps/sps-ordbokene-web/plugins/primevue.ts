3f636c93b29930a8227ee6bc67bd5389c4468c24
sawravchy <43489196+sawravchy@users.noreply.github.com>
2023-10-16 13:41:43 +0200

    Revert header update

--- apps/sps-ordbokene-web/plugins/primevue.ts
+++ apps/sps-ordbokene-web/plugins/primevue.ts
@@ -1,11 +1,9 @@
 import PrimeVue from "primevue/config";
 import Menu from "primevue/menu";
-import Tooltip from 'primevue/tooltip';
+// import Tooltip from 'primevue/tooltip';
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(PrimeVue, { ripple: false });
   nuxtApp.vueApp.component("Menu", Menu);
-
-
-  nuxtApp.vueApp.directive('tooltip', Tooltip);
+  // nuxtApp.vueApp.directive('tooltip', Tooltip);
 });
