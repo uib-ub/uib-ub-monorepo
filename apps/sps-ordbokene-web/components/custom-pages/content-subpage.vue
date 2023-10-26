c7f8f48bb70913bafbb72310c01583e953c30f48
Henrik Askjer <Henrik.Askjer@uib.no>
2023-10-26 09:51:39 +0200

    tweak content query

--- apps/sps-ordbokene-web/components/custom-pages/content-subpage.vue
+++ apps/sps-ordbokene-web/components/custom-pages/content-subpage.vue
@@ -19,12 +19,12 @@ const route = useRoute()
 console.log("FULLPATH", route.fullPath)
 console.log(i18n.locale)
 
-const { data } = await useAsyncData('subpage-' + route.fullPath, () => {
+const { data } = await useAsyncData('subpage-data', () => {
   console.log("FETCHING", route.fullPath)
   return queryContent(route.fullPath).findOne()
 
 } ,
-        {watch: i18n.locale})
+        {watch: [i18n.locale]})
 
 console.log("DATA", data.value)
 
