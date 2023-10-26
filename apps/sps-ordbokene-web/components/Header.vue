1c596acf86764a8615586d399c33a10826b0f7c2
Henrik Askjer <Henrik.Askjer@uib.no>
2023-10-26 16:42:03 +0200

    tweak header style

--- apps/sps-ordbokene-web/components/Header.vue
+++ apps/sps-ordbokene-web/components/Header.vue
@@ -1,27 +1,42 @@
 <template>
-<header class="bg-primary pl-3 pr-0 lg:px-5 py-1 flex flex-col lg:flex-row content-center text-white shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
-      <div class="flex flex-row content-center items-center w-full lg:w-auto">
+<header class="bg-primary pl-6 lg:pr-6 flex flex-col lg:flex-row content-center text-white shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
+  <div class="flex">
+  <div class="flex flex-grow py-1 items-center md:py-3 flex-auto !box-content">
   <NuxtLink :to="'/'+ i18n.locale.value" :aria-current="($route.name == 'welcome' || $route.name == 'index') && 'page'" @click="store.$reset()">
-      <div class="mx-1 my-1 md:my-2 mt-0 xl:my-4">
-      <div><h1 class="text-md xl:text-2xl">ordbøkene.no<span class="sr-only">, {{$t('home')}}</span></h1>
+      <div><h1 class="text-xl xl:text-2xl">ordbøkene.no<span class="sr-only">, {{$t('home')}}</span></h1>
       <div class="text-xs xl:text-sm sr-only sm:not-sr-only">{{$t("sub_title")}}</div>
-      </div>
     </div>
-      </NuxtLink>
-      <div class="lg:hidden text-lg ml-auto flex align-center">
+  </NuxtLink>
+  </div>
 
-      <button type="button"
-              class="text-lg pb-2 pt-1 px-3 sm:px-4 rounded-4xl active:bg-primary-darken focus:bg-primary-darken"
-              :aria-expanded="menu_expanded"
-              :aria-controls="menu_expanded? 'main_menu' : null"
-              @click="menu_expanded = !menu_expanded">
-        <span class="sr-only sm:inline sm:not-sr-only">{{$t('menu')}}</span><Icon :name="menu_expanded ? 'bi:x-lg' : 'bi:list'" class="sm:ml-2"/>
+
+    <div class="lg:hidden flex items-center flex-auto flex-grow-0">
+      <button  type="button"
+                class="text-center text-lg border-x-2 border-primary-lighten p-3"
+                @click="locale_menu.toggle" aria-haspopup="true" :aria-controls="locale_menu && locale_menu.overlayVisible ? 'locale_menu' : null">
+        <span class="relative">
+        <span aria-hidden="true" class="absolute text-xs right-2 top-3 bg-primary rounded px-1 select-none">{{locale2lang[$i18n.locale].toUpperCase()}}</span><Icon name="bi:globe" size="1.5em"/>
+        <span id="locale-label" class="sr-only">
+          <span lang="no">Nettsidespråk</span>
+          <span v-for="({button, lang}) in localeConfig.filter(item => item.button)" :key="lang" :lang="lang" class="">{{button}}</span>
+        </span>
+        </span>
       </button>
-</div>
+   
+
+        <button type="button"
+                class="flex text-center items-center text-lg p-3"
+                :aria-expanded="menu_expanded"
+                :aria-controls="menu_expanded? 'main_menu' : null"
+                @click="menu_expanded = !menu_expanded">
+          <span class="sr-only sm:inline sm:not-sr-only">{{$t('menu')}}</span><Icon :name="menu_expanded && false ? 'bi:x-lg' : 'bi:list'" size="2rem" class="sm:ml-2"/>
+        </button>
       </div>
-    <div id="main_menu" class="lg:flex lg:ml-auto nav-buttons flex-wrap lg:flex-row content-center text-center mr-1 mt-6 lg:mt-0"  v-bind:class="{hidden: !menu_expanded}">
+
+      </div>
+    <div id="main_menu" class="lg:flex lg:ml-auto nav-buttons flex-wrap lg:flex-row content-center text-center"  v-bind:class="{hidden: !menu_expanded}">
       <nav class="lg:mr-4 self-center" :aria-label="$t('label.nav')">
-      <ul class="flex flex-col lg:flex-row gap-4 lg:space-x-3 xl:space-x-8 content-center mb-4 lg:mb-0" >
+      <ul class="flex flex-col lg:flex-row gap-6 lg:space-x-3 xl:space-x-8 content-center my-6 lg:my-0" >
         <!-- <li class="nav-item invisible lg:visible">
           <span class="nav-link" v-tooltip.right="{ value: `<h4 class='bg-black text-canvas-darken p-4 text-3xl'>${$t('font-size.description')}</h4>`, escape: true}" >{{$t('font-size.title')}}</span>
         </li> -->
