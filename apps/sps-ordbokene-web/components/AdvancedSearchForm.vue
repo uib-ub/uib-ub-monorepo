47fbade06311e7ea9fab05c926ab74198a28475e
Henrik Askjer <Henrik.Askjer@uib.no>
2023-10-30 11:27:27 +0100

    tweak advanced search styling

--- apps/sps-ordbokene-web/components/AdvancedSearchForm.vue
+++ apps/sps-ordbokene-web/components/AdvancedSearchForm.vue
@@ -2,34 +2,34 @@
   <div class="pb-2">
     
     <form  @submit.prevent="submitForm" ref="form" class="flex flex-col gap-4">
-        <div class="flex flex-col sm:flex-row sm:flex-wrap w-full gap-3 xl:items-end mt-2 lg:mt-0">
+        <div class="flex flex-col lg:flex-row sm:flex-wrap w-full gap-3 mt-2 lg:mt-0">
 
-        <div class="whitespace-nowrap">
+        <div class="whitespace-nowrap mr-4">
             <NuxtLink class="" :to="`/${$i18n.locale}/${store.dict}/${advancedSpecialSymbols(store.q) ? '' : store.q}`"><Icon name="bi:arrow-left-short" size="1.5rem" class="mb-1 text-primary"/>{{$t('notifications.simple')}} </NuxtLink>
         </div>
 
 
-        <div class="whitespace-nowrap p-1 xl:bg-tertiary xl:shadow-none flex sm:flex-col xl:flex-row flex-grow-0 items-baseline"> 
+        <div class="whitespace-nowrap p-1 xl:bg-tertiary xl:shadow-none flex flex-grow-0 items-baseline"> 
           
             <label for="dict-select">{{ $t('options.dict') }} </label>
-            <select id="dict-select" name="dict" @change="update_dict" class="bg-tertiary flex-grow">
+            <select id="dict-select" name="dict" @change="update_dict" class="bg-tertiary flex-grow md:grow-0">
                 <option v-for="(dict, idx) in  dicts" :key="idx" :value="dict" :selected="store.dict == dict" v-bind:class="{selected: store.dict == dict}">{{$t("dicts." + dict)}}</option>
             </select>
 
         </div>
 
 
-        <div class="whitespace-nowrap p-1 xl:bg-tertiary xl:shadow-none  flex sm:flex-col xl:flex-row flex-grow-0 items-baseline"> 
+        <div class="whitespace-nowrap p-1 xl:bg-tertiary xl:shadow-none  flex flex-grow-0 items-baseline"> 
             <label for="scope-select">{{ $t('options.scope.title') }}</label>
-            <select id="scope-select" name="scope" @change="update_scope"  class="bg-tertiary flex-grow">
+            <select id="scope-select" name="scope" @change="update_scope"  class="bg-tertiary flex-grow md:grow-0">
                 <option v-for="(scope, idx) in  ['e', 'ei', 'eif']" :key="idx" :value="scope" :selected="store.scope == scope" v-bind:class="{selected: store.scope == scope}">{{$t("options.scope.value." + scope)}}</option>
             </select>
         </div>
 
 
-        <div class="whitespace-nowrap p-1 xl:bg-tertiary xl:shadow-none  flex sm:flex-col xl:flex-row xl:flex-grow-0 items-baseline"> 
+        <div class="whitespace-nowrap p-1 xl:bg-tertiary xl:shadow-none flex xl:flex-grow-0 items-baseline"> 
             <label for="pos-select">{{ $t('pos') }}</label>
-            <select id="pos-select" name="pos" @change="update_pos" class="bg-tertiary flex-grow">
+            <select id="pos-select" name="pos" @change="update_pos" class="bg-tertiary flex-grow md:grow-0">
                 <option v-for="(tag, idx) in  pos_tags" :key="idx" :value="tag" :selected="store.pos == tag" v-bind:class="{selected: store.pos == tag}">{{tag ? $t("tags." + tag) : $t("all_pos")}}</option>
             </select>
         </div>
