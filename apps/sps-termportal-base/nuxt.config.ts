6a2f18be0b4ec2bdb0aa4514f4655138432de827
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-10-31 18:44:00 +0100

    feat(tp): move modules and build settings to base config

--- apps/sps-termportal-base/nuxt.config.ts
+++ apps/sps-termportal-base/nuxt.config.ts
@@ -1,3 +1,13 @@
 export default defineNuxtConfig({
+  build: {
+    transpile: ["primevue"],
+  },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "nuxt-icon",
+    "@vueuse/nuxt",
+    // "@nuxtjs/html-validator",
+    // "@unlighthouse/nuxt",
+  ],
   css: ["/../sps-termportal-base/assets/tp-theme/theme.scss"],
 });
--- apps/sps-termportal-web/nuxt.config.ts
+++ apps/sps-termportal-web/nuxt.config.ts
@@ -12,13 +12,7 @@ export default defineNuxtConfig({
       link: [{ rel: "icon", type: "image/svg", href: "/favicon.svg" }],
     },
   },
-  build: {
-    transpile: ["primevue"],
-  },
   modules: [
-    "@nuxtjs/tailwindcss",
-    "nuxt-icon",
-    "@vueuse/nuxt",
     // "@nuxtjs/html-validator",
     // "@unlighthouse/nuxt",
   ],
