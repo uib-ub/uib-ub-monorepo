67a1ceb241c1fae28b9392fc350b8e00ac8892a2
Henrik Askjer <Henrik.Askjer@uib.no>
2023-11-01 13:58:46 +0100

    add clear cookies button

--- apps/sps-ordbokene-web/components/custom-pages/settings.vue
+++ apps/sps-ordbokene-web/components/custom-pages/settings.vue
@@ -4,37 +4,46 @@ import { useI18n } from 'vue-i18n'
 
 const { t } = useI18n()
 const settings = useSettingsStore()
+const settings_cookie = useCookie('settings')
+const locale_cookie = useCookie('currentLocale')
 
-const resetSettings = (settings) => {
-    settings.$patch({
+const default_settings = {
       simpleListView:false,
       autoSelect: true, 
       inflectionExpanded: false, 
       inflectionTableContext: false, 
       inflectionNo: false,
+    }
 
-    })
+const not_default = computed(() => {
+  for (const item in default_settings) {
+    if (default_settings[item] != settings.$state[item]) {
+      return true
+    }
+  }
+})
+
+const resetSettings = (settings) => {
+    settings.$patch(default_settings)
+};
+
+const clearCookies = () => {
+    settings_cookie.value = null
+    locale_cookie.value = null
 };
 
 useHead({
-title: t('settings.title')
+  title: t('settings.title')
 })
 
-const resetAnimation = ref(false);
-
-const startAnimation = () => {
-  resetAnimation.value = true;
-  setTimeout(() => {
-    resetAnimation.value = false;
-  }, 1500); 
-};
 
 
 </script>
 
 <template>
-<main id="main" tabindex="-1" class="secondary-page flex flex-col gap-2">
+<main id="main" tabindex="-1" class="secondary-page flex flex-col gap-4">
   <h1>{{$t('settings.title')}}</h1>
+  <client-only>
   <FormCheckbox v-model="settings.$state.simpleListView" :checked="settings.simpleListView">
       {{$t('settings.simple_search_list')}}
     </FormCheckbox>
@@ -53,11 +62,16 @@ const startAnimation = () => {
 
 
 
-  <div class="mt-4">
-    <button class="btn btn-primary" @click="resetSettings(settings); startAnimation()">
-      <Icon :name="resetAnimation ? 'bi:trash' : 'bi:trash-fill'" class="mr-3 mb-1 text-primary" />
-      <span>{{$t(resetAnimation ? 'settings.reset_done' : 'settings.reset')}}</span>
+  <div class="mt-4 flex flex-col md:flex-row gap-3">
+    <button v-if="not_default" class="btn btn-primary" @click="resetSettings(settings)">
+      <Icon name="bi:arrow-clockwise" class="mr-3 mb-1 text-primary" />
+      <span>{{$t('reset')}}</span>
+    </button>
+        <button v-if="locale_cookie || settings_cookie" class="btn btn-primary" @click="clearCookies(settings)">
+      <Icon name="bi:trash-fill" class="mr-3 mb-1 text-primary"/>
+      <span>{{$t('settings.clear_cookies')}}</span>
     </button>
   </div>
+  </client-only>
 </main>
 </template>
