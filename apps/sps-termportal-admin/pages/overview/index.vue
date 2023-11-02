797b6d7608891732411a4515d3e63e5db6d60b6c
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-11-02 15:28:23 +0100

    feat(tp): add overview page + api routes

---
+++ apps/sps-termportal-admin/pages/overview/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex">
+    <SideBar></SideBar>
+    <main class="pt-8">
+      <h1 class="mb-6 text-2xl">Termbase Overview</h1>
+      <DataTable :value="products" removableSort tableStyle="min-width: 1rem">
+        <Column
+          v-for="col of columns"
+          :key="col.field"
+          :field="col.field"
+          :header="col.header"
+          sortable
+        ></Column>
+      </DataTable>
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { data } = await useLazyFetch("/api/overview/fuseki");
+
+const products = computed(() => {
+  return data.value?.results?.bindings.map((e) => {
+    return {
+      label: e.label.value,
+      id: e.id.value,
+      conceptCount: e.concepts.value,
+    };
+  });
+});
+const columns = [
+  { field: "label", header: "Label" },
+  { field: "id", header: "ID" },
+  { field: "conceptCount", header: "Concepts" },
+];
+</script>
