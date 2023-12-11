2d1049c5a8cdec10d3390a5d0d317c6c27bf4a81
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-12-11 20:43:23 +0100

    refactor(tpa): overview querying etc

--- apps/sps-termportal-admin/pages/overview/index.vue
+++ apps/sps-termportal-admin/pages/overview/index.vue
@@ -3,34 +3,27 @@
     <SideBar></SideBar>
     <main class="pt-8">
       <h1 class="mb-6 text-2xl">Termbase Overview</h1>
-      <DataTable :value="products" removable-sort table-style="min-width: 1rem">
-        <Column
-          v-for="col of columns"
-          :key="col.field"
-          :field="col.field"
-          :header="col.header"
-          sortable
-        ></Column>
+      <DataTable :value="merged" removable-sort table-style="min-width: 1rem">
+        <Column field="label" header="Label" />
+        <Column field="id" header="ID" />
+        <Column field="conceptCount" header="Concepts" />
       </DataTable>
     </main>
   </div>
 </template>
 
 <script setup lang="ts">
-const { data } = await useLazyFetch("/api/overview/fuseki");
+const { data: dbdata } = await useLazyFetch("/api/overview/fuseki");
+const { data: cmsdata } = ""
 
-const products = computed(() => {
-  return data.value?.results?.bindings.map((e) => {
+const merged = computed(() => {
+  const dbdatap = dbdata.value?.results?.bindings.map((e) => {
     return {
       label: e.label.value,
       id: e.id.value,
       conceptCount: e.concepts.value,
     };
   });
+  return dbdatap;
 });
-const columns = [
-  { field: "label", header: "Label" },
-  { field: "id", header: "ID" },
-  { field: "conceptCount", header: "Concepts" },
-];
 </script>
