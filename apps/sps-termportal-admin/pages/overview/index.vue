37a949f87dccc250e69ac350134d411abb75aed5
Jan Ole Bangen <Jan.Bangen@uib.no>
2023-12-12 12:02:54 +0100

    feat(tpa): extend overview data display

--- apps/sps-termportal-admin/pages/overview/index.vue
+++ apps/sps-termportal-admin/pages/overview/index.vue
@@ -2,30 +2,50 @@
   <div class="flex">
     <SideBar></SideBar>
     <main class="pt-8">
-      <h1 class="mb-6 text-2xl">Termbase Overview</h1>
-      <DataTable
-        v-model:filters="filters"
-        :value="merged"
-        removable-sort
-        table-style="min-width: 1rem"
-        :global-filter-fields="['label', 'id', 'conceptCount', 'status']"
-      >
-        <template #header>
-          <div class="flex">
-            <InputText
-              v-model="filters['global'].value"
-              placeholder="Keyword Search"
-            />
-          </div>
-        </template>
-        <Column sortable field="label" header="Label" />
-        <Column sortable field="id" header="ID" />
-        <Column sortable field="conceptCount" header="Concepts" />
-        <Column sortable field="status" header="Status" />
-        <Column sortable field="labels" header="Labels" />
-        <Column sortable field="descriptions" header="Descriptions" />
-        <Column sortable field="agreement" header="Agreement" />
-      </DataTable>
+      <section>
+        <h1 class="mb-6 text-2xl">Termbase Overview</h1>
+        <DataTable
+          v-model:filters="filters"
+          v-model:selection="selectedTermbase"
+          selection-mode="single"
+          :value="merged"
+          removable-sort
+          table-style="min-width: 1rem"
+          :global-filter-fields="['label', 'id', 'conceptCount', 'status']"
+        >
+          <template #header>
+            <div class="flex">
+              <InputText
+                v-model="filters['global'].value"
+                placeholder="Keyword Search"
+              />
+            </div>
+          </template>
+          <Column sortable field="label" header="Label" />
+          <Column sortable field="id" header="ID" />
+          <Column sortable field="conceptCount" header="Concepts" />
+          <Column sortable field="status" header="Status" />
+          <Column sortable field="labels" header="Labels" />
+          <Column sortable field="descriptions" header="Descriptions" />
+          <Column sortable field="agreement" header="Agreement" />
+          <Column header="">
+            <template #body="slotProps">
+              <NuxtLink
+                v-if="slotProps.data._id"
+                :to="`/studio/desk/termbase;${slotProps.data._id}`"
+                target="_blank"
+              >
+                Rediger
+              </NuxtLink>
+            </template>
+          </Column>
+        </DataTable>
+      </section>
+      <TermgroupMembers
+        v-if="selectedTermbase?.id"
+        :key="selectedTermbase?.id"
+        :termbase="selectedTermbase?.id"
+      />
     </main>
   </div>
 </template>
@@ -51,11 +71,13 @@ const merged = computed(() => {
     labels: matchid(cmsdata, e, "labelsOk"),
     descriptions: matchid(cmsdata, e, "descriptionsOk"),
     agreement: matchid(cmsdata, e, "hasLicenseAgreement"),
+    _id: matchid(cmsdata, e, "_id"),
   }));
 
   return enriched;
 });
 
+const selectedTermbase = ref();
 const filters = ref({
   global: { value: null, matchMode: FilterMatchMode.CONTAINS },
 });
