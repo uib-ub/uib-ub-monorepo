c34655ab94ac8b256daab12d0c864fa4d16d9194
Jan Ole Bangen <Jan.Bangen@uib.no>
2024-01-15 10:07:40 +0100

    feat(tp): allow org as contact

--- apps/sps-termportal-admin/sanity/schemas/termbase.js
+++ apps/sps-termportal-admin/sanity/schemas/termbase.js
@@ -66,7 +66,7 @@ export default {
         {
           name: "person",
           type: "reference",
-          to: [{ type: "person" }],
+          to: [{ type: "person" }, { type: "organization" }],
         },
       ],
     },
