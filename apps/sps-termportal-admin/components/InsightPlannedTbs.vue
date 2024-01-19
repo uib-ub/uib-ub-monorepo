070a3aeeb4f0d0de5d2606836de5e56d69c41aba
Jan Ole Bangen <Jan.Bangen@uib.no>
2024-01-19 13:53:55 +0100

    fix(tpa): global filter fields for planned termbases

--- apps/sps-termportal-admin/components/InsightPlannedTbs.vue
+++ apps/sps-termportal-admin/components/InsightPlannedTbs.vue
@@ -9,7 +9,7 @@
       paginator
       :rows="15"
       table-style="min-width: 1rem"
-      :global-filter-fields="['label', 'termgroup', 'organization']"
+      :global-filter-fields="['label', 'status', 'domain']"
     >
       <template #header>
         <div class="flex justify-between">
@@ -36,13 +36,6 @@ const query = `
 `;
 
 const { data } = useLazySanityQuery(query);
-
-const statusopt = {
-  kjent: "1. kjent",
-  planlagt: "2. planlagt",
-  initialisert: "3. initialisert",
-  opprettet: "4. opprettet",
-};
 
 const procdata = computed(() => {
   const mapped = data.value?.map((tb) => {
