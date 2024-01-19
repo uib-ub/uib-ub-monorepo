d6dcd84027f6a7f38149be165ed3bd40ed38758f
Jan Ole Bangen <Jan.Bangen@uib.no>
2024-01-19 15:27:45 +0100

    feat(tpa): add published termbases per year to insights

---
+++ apps/sps-termportal-admin/components/InsightPublishedTbsYear.vue
@@ -0,0 +1,60 @@
+<template>
+  <section>
+    <h2 class="mb-3 text-xl">Published Termbase by Year</h2>
+    <DataTable
+      ref="datatable"
+      v-model:filters="filters"
+      :value="procdata"
+      removable-sort
+      sort-field="year"
+      :sort-order="-1"
+      paginator
+      :rows="15"
+      table-style="min-width: 1rem"
+      :global-filter-fields="['year']"
+    >
+      <template #header>
+        <div class="flex justify-between">
+          <InputText v-model="filters['global'].value" placeholder="Søk" />
+          <Button class="h-10" label="Eksport" @click="exportData($event)" />
+        </div>
+      </template>
+      <Column field="year" header="År" sortable></Column>
+      <Column field="count" header="Antall" sortable></Column>
+    </DataTable>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { FilterMatchMode } from "primevue/api";
+
+const query = `
+*[_type == "activity" && type == 'termbasePublisering']{
+  "date": timespan.beginOfTheBegin
+}
+`;
+
+const { data } = useLazySanityQuery(query);
+
+const procdata = computed(() => {
+  const mapped = data.value?.map((entry) => entry.date.substring(0, 4));
+  if (mapped) {
+    const counts = Object.entries(
+      mapped?.reduce((acc, year) => {
+        acc[year] = (acc[year] || 0) + 1;
+        return acc;
+      }, {})
+    ).map(([year, count]) => ({ year: parseInt(year), count }));
+    return counts;
+  }
+  return [];
+});
+
+const datatable = ref();
+const exportData = () => {
+  datatable.value.exportCSV();
+};
+const filters = ref({
+  global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+});
+</script>
