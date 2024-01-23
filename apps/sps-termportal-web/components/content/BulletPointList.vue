ab00d5f367c9c70f3529e2c3d0d830e537ad2514
Jan Ole Bangen <Jan.Bangen@uib.no>
2024-01-23 18:02:07 +0100

    feat(tpw): add bulletpointlist for content

---
+++ apps/sps-termportal-web/components/content/BulletPointList.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bullet-point">
+    <slot></slot>
+  </div>
+</template>
+
+<style>
+.bullet-point ul {
+  text-indent: -1em;
+  padding-left: 1.5em;
+  @apply list-disc;
+}
+</style>
