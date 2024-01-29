4eba0d104d8794e4afea71691d803e167828cd2d
Jan Ole Bangen <Jan.Bangen@uib.no>
2024-01-29 14:00:33 +0100

    fix(tpw): empty h1 on landing page

--- apps/sps-termportal-web/pages/index.vue
+++ apps/sps-termportal-web/pages/index.vue
@@ -1,23 +1,31 @@
 <template>
-  <main class="container">
+  <div>
     <Head>
       <Title>{{ $t("index.title") }}</Title>
     </Head>
-    <div class="border-grey my-2 border-b-2 px-2">
-      <SearchField />
-      <HeaderSearchOptions class="pt-2" />
-      <HeaderSearchScope />
-    </div>
-    <div class="flex flex-col gap-x-8 gap-y-6 pt-6 lg:flex-row">
-      <ContentDoc
-        :key="`welcome${locale}`"
-        :path="`/${locale}/welcome`"
-        class="content-wrapper basis-7/12"
+    <h1 class="text-5xl xs:text-6xl sm:text-7xl md:text-8xl">
+      <LogoTermportalen
+        class="mb-6 ml-2 mr-14 max-w-[25rem] sm:mb-8 md:mb-11 md:ml-7"
       />
-      <NewsTP class="basis-5/12" />
-    </div>
-    <CollaboratorsTP class="mt-6 xl:mt-8" />
-  </main>
+      <span class="sr-only">Termportalen</span>
+    </h1>
+    <main class="container">
+      <div class="border-grey my-2 border-b-2 px-2">
+        <SearchField />
+        <HeaderSearchOptions class="pt-2" />
+        <HeaderSearchScope />
+      </div>
+      <div class="flex flex-col gap-x-8 gap-y-6 pt-6 lg:flex-row">
+        <ContentDoc
+          :key="`welcome${locale}`"
+          :path="`/${locale}/welcome`"
+          class="content-wrapper basis-7/12"
+        />
+        <NewsTP class="basis-5/12" />
+      </div>
+      <CollaboratorsTP class="mt-6 xl:mt-8" />
+    </main>
+  </div>
 </template>
 <script setup lang="ts">
 const locale = useLocale();
