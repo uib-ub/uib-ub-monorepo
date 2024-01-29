ae7166942b5649dc6d5d3d3a3442ec651ccce45e
Jan Ole Bangen <Jan.Bangen@uib.no>
2024-01-29 10:22:12 +0100

    fix(tp): unwrap el and add styles for plain list

--- apps/sps-termportal-web/components/content/PlainList.vue
+++ apps/sps-termportal-web/components/content/PlainList.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="plain-list">
-    <slot></slot>
-  </div>
+  <ul class="plain-list">
+    <ContentSlot :use="$slots.default" unwrap="p ul" />
+  </ul>
 </template>
+
+<style>
+.plain-list ul {
+  @apply space-y-1;
+}
+
+.plain-list-indent ul {
+  margin-left: 2em;
+  @apply space-y-2;
+}
+</style>
