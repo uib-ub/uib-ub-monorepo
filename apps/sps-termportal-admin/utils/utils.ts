dfb106ed602a80236c48ee1001a05db7f2d87a19
Jan Ole Bangen <Jan.Bangen@uib.no>
2024-01-31 15:45:18 +0100

    feat(tpa): add number status util

--- apps/sps-termportal-admin/utils/utils.ts
+++ apps/sps-termportal-admin/utils/utils.ts
@@ -4,3 +4,8 @@ export function formatTimespan(start: string, end: string) {
   }
   return `${start ? start + "/" : "-/"}${end || "-"}`;
 }
+
+export function numberStatus(status: string) {
+  const index = statusOrder.indexOf(status);
+  return `${index > 0 ? index + 1 + "." : ""} ${status || ""}`;
+}
