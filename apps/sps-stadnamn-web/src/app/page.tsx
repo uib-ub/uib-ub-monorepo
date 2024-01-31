4e0eae278938a61d3ce3c2b23dac8280ee43f230
Henrik Askjer <Henrik.Askjer@uib.no>
2024-01-31 17:35:48 +0100

    add dummy dataset cards

--- apps/sps-stadnamn-web/src/app/page.tsx
+++ apps/sps-stadnamn-web/src/app/page.tsx
@@ -1,7 +1,16 @@
 import UiBLogo from "@/components/svg/UiBLogo"
 import { PiMapTrifold, PiDatabase } from 'react-icons/pi';
 import Link from 'next/link';
+import Image from 'next/image';
+import marker from "/public/marker.svg";
+
 export default function Home() {
+  const cards = [
+    { img: "https://upload.wikimedia.org/wikipedia/commons/4/4b/Prof_oluf_rygh.jpg", title: "Oluf Rygh: Norske Gaardnavne", description: "Description" },
+    { img: "https://via.placeholder.com/150", title: "Matrikkelen 1886", description: "Description" },
+    { img: "https://via.placeholder.com/150", title: "Bustadnamn", description: "Description" },
+  ];
+
   return (
     <>
 <main className="flex flex-col grow-1 gap-24 items-center justify-center  pt-4 pb-8 px-4 my-auto">
@@ -16,11 +25,10 @@ export default function Home() {
   </div>
   <div className="flex flex-col md:flex-row gap-12 justify-center">
   <button className="btn aspect-square flex flex-col text-xl"><PiMapTrifold className="text-9xl md:text-8xl"/>Utforsk kartet</button>
-  <button className="btn aspect-square flex flex-col text-xl"><PiDatabase className="text-9xl md:text-8xl"/>Velg datasett</button>
   </div>
   </div>
 
-  <div className="flex flex-col items-center gap-6">
+  <div className="flex items-center gap-12">
   <div className="flex flex-row items-center gap-6 text-slate-950 "><UiBLogo/><div className="flex flex-col gap-1"><h2 className="tracking-widest font-serif">UNIVERSITETET I BERGEN</h2><em className="font-serif">Universitetsbiblioteket</em></div>
   </div>
   <div className="flex gap-6 jusitfy-between">
@@ -30,11 +38,28 @@ export default function Home() {
   <Link href="https://uib.no/digitalutvikling" className="text-sm">uib.no/digitalutvikling</Link></div>
 
   </div>
+  
+
+
   </div>
+  <section className="flex flex-col items-center gap-6">
+    <h2 className="font-serif text-3xl">Datasett</h2>
+    <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      {cards.map((card, index) => (
+        <li key={index} className="card flex">
+          <img src={card.img}  alt="Image description" className="w-64 h-64 object-cover m-2"/>
+          <div className="content p-4 w-128">
+            <h3 className="text-lg font-semibold">{card.title}</h3>
+            <p>{card.description}</p>
+          </div>
+        </li>
+      ))}
+    </ul>
+    </section>
   
 
 </main>
-        <footer className="bg-slate-200  p-3 px-6 text-center">
+        <footer className="bg-slate-200  p-6 text-center">
         
         <nav className="flex flex-col md:flex-row gap-3">
           <Link href="/">Personvern</Link>
