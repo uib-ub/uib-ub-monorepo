19e0f13d6d0054a9885da5803b579e1b7a0cb07e
Henrik Askjer <Henrik.Askjer@uib.no>
2024-01-31 14:00:19 +0100

    bigger icons in search bar

--- apps/sps-stadnamn-web/src/app/SearchBar.tsx
+++ apps/sps-stadnamn-web/src/app/SearchBar.tsx
@@ -9,10 +9,10 @@ export default function SearchBar() {
     if (pathname.slice(0, 7) != '/search') return (<></>)
     return (
         <div className="flex flex-wrap md:flex-nowrap gap-1">
-        <button className="btn flex-grow">Hordanamn <PiCaretDown/></button>
+        <button className="btn flex-grow">Hordanamn <PiCaretDown className="text-lg"/></button>
         <div className="flex gap-1 flex-grow min-h-[2rem]">
         <input type="text" form="search_form" name="q" className='border border-slate-500 w-full rounded-sm px-2'/>
-        <button type="submit" form="search_form" className='btn btn-primary p-1 px-3'><PiMagnifyingGlass/></button>
+        <button type="submit" form="search_form" className='btn btn-primary p-1 px-3 text-lg'><PiMagnifyingGlass className="text-lg"/></button>
         </div>
         </div>
     )
