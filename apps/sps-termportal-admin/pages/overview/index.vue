d52c402ab8db2538b715d61a034df39f861f2f74
Jan Ole Bangen <Jan.Bangen@uib.no>
2024-02-05 09:19:36 +0100

    fix(tpa): keys for members, activities to string

--- apps/sps-termportal-admin/pages/overview/index.vue
+++ apps/sps-termportal-admin/pages/overview/index.vue
@@ -141,12 +141,12 @@
       </section>
       <TermgroupMembers
         v-if="selectedTermbase.length > 0"
-        :key="'members' + selectedTermbase"
+        :key="'members' + selectedTermbase.map((tb) => tb._id).join('')"
         :termbases="selectedTermbase.map((tb) => tb._id)"
       />
       <TermbaseActivities
         v-if="selectedTermbase.length > 0"
-        :key="'activities' + selectedTermbase"
+        :key="'activities' + selectedTermbase.map((tb) => tb._id).join('')"
         :termbases="
           Object.fromEntries(selectedTermbase.map((d) => [d._id, d.label]))
         "
