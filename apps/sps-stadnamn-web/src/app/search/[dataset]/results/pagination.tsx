8723b2f8476ef75fd33a943133a2eb28f1b2e933
Henrik Askjer <Henrik.Askjer@uib.no>
2024-02-09 16:01:55 +0100

    #sn fix but with 3 pages

--- apps/sps-stadnamn-web/src/app/search/[dataset]/results/pagination.tsx
+++ apps/sps-stadnamn-web/src/app/search/[dataset]/results/pagination.tsx
@@ -23,15 +23,14 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   return (
     <>
     <div className='flex grow gap-2 mb-2'>
-    
     {currentPage > 1 && <><button className='btn btn-primary btn-compact' onClick={() => { router.push(paginationUrl(currentPage - 1))}}><PiCaretLeft/></button>
-                          <button className='btn btn-compact' onClick={() => { router.push(paginationUrl(1))}}>1</button></>}
+                        {totalPages != 3 || currentPage == 3 &&  <button className='btn btn-compact' onClick={() => { router.push(paginationUrl(1))}}>1</button> } </>}
     {currentPage == totalPages && totalPages > 3 && <button className='btn btn-compact' onClick={() => { router.push(paginationUrl(totalPages-2))}}>{totalPages-2}</button>}
     {currentPage > totalPages-2 && totalPages > 2 && <button className='btn btn-compact' onClick={() => { router.push(paginationUrl(currentPage-1))}}>{currentPage-1}</button>}
     <span className='px-3 py-1 border-2 rounded-sm border-neutral-400'>{currentPage}</span>
     {currentPage == 1 && <button className='btn btn-compact' onClick={() => { router.push(paginationUrl(2))}}>2</button>}
-    {currentPage == 1 && totalPages > 2 && <button className='btn btn-compact' onClick={() => { router.push(paginationUrl(3))}}>3</button>}
-    {currentPage < totalPages-1 && <button className='btn btn-compact' onClick={() => { router.push(paginationUrl(totalPages))}}>{totalPages}</button>}
+    {currentPage == 1 && totalPages > 3 && <button className='btn btn-compact' onClick={() => { router.push(paginationUrl(3))}}>3</button>}
+    {((currentPage < totalPages-1) || (totalPages == 3 && currentPage < 3)) && <button className='btn btn-compact' onClick={() => { router.push(paginationUrl(totalPages))}}>{totalPages}</button>}
     {currentPage < totalPages && <button className='btn btn-primary btn-compact' onClick={() => { router.push(paginationUrl(currentPage + 1))}}><PiCaretRight/></button>}
     </div>
     <div className='mb-2'>
