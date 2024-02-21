be3f18738dcab03b38034df2633e1c42e357b2e0
Henrik Askjer <Henrik.Askjer@uib.no>
2024-02-21 10:54:47 +0100

    bigger brand text in small viewport

--- apps/sps-stadnamn-web/src/app/layout.tsx
+++ apps/sps-stadnamn-web/src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
         <header className="flex flex-col lg:flex-row lg:gap-6 py-2 px-1 lg:p-3 lg:px-4 text-neutral-900 align-middle">
           <div className="flex flex-wrap pl-3 justify-between align-middle h-full">
           
-          <Link href="/" className="sm:text-xl lg:text-2xl font-serif small-caps h-full pt-1 lg:pt-0">Norske stadnamn</Link>
+          <Link href="/" className="text-lg sm:text-xl lg:text-2xl font-serif small-caps h-full pt-1 lg:pt-0">Norske stadnamn</Link>
           <Menu/>
           
           </div>
