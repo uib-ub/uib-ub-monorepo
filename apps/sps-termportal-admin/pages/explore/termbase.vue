62108538540656445f2d86bd04193f440bcc8f9e
Jan Ole Bangen <Jan.Bangen@uib.no>
2024-02-27 13:54:23 +0100

    refactor(tpa): overview in explore view

--- apps/sps-termportal-admin/pages/explore/termbase.vue
+++ apps/sps-termportal-admin/pages/explore/termbase.vue
@@ -2,26 +2,9 @@
   <div class="flex">
     <SideBar />
     <main class="space-y-4">
-      <section>
-        <h1 class="mb-6 text-2xl">Explore Termbases</h1>
-        <DataTable
-          v-model:selection="selectedTermbase"
-          selection-mode="single"
-          :value="termbases"
-          removable-sort
-          table-style="min-width: 1rem"
-        >
-          <Column
-            v-for="col of columns"
-            :key="col.field"
-            :field="col.field"
-            :header="col.header"
-            sortable
-          ></Column>
-        </DataTable>
-      </section>
+      <SimpleOverview v-model:modelValue="selectedTermbase" />
       <ExploreDefinitions
-        v-if="selectedTermbase"
+        v-if="selectedTermbase?.id"
         :key="`exploredef${selectedTermbase?.id}`"
         :termbase="selectedTermbase"
       />
@@ -30,22 +13,5 @@
 </template>
 
 <script setup lang="ts">
-const { data } = await useLazyFetch("/api/overview/fuseki");
-
-const termbases = computed(() => {
-  return data.value?.results?.bindings.map((e) => {
-    return {
-      label: e.label.value,
-      id: e.id.value,
-      conceptCount: e.concepts.value,
-    };
-  });
-});
-const columns = [
-  { field: "label", header: "Label" },
-  { field: "id", header: "ID" },
-  { field: "conceptCount", header: "Concepts" },
-];
-
-const selectedTermbase = ref();
+const selectedTermbase = ref<Record<string, string | null>>({});
 </script>
