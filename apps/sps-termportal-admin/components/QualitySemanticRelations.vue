03bdb6bbc1fc05ff8c9dbd8f9b6236b00d843f99
Jan Ole Bangen <Jan.Bangen@uib.no>
2024-02-27 15:30:07 +0100

    feat(tpa): display semantic relations and export them

--- apps/sps-termportal-admin/components/QualitySemanticRelations.vue
+++ apps/sps-termportal-admin/components/QualitySemanticRelations.vue
@@ -1,6 +1,33 @@
 <template>
-  <section>
-    {{ relations }}
+  <section class="space-y-2">
+    <h2 class="mb-3 text-xl">Imported semantic relations</h2>
+    <p>Termbase MUST NOT have unspecified semantic relations.</p>
+    <p>
+      Only applies to imports from Språkrådets termwiki where we moved
+      unspecified links to the temporary semantic relation field.
+    </p>
+    <DataTable
+      ref="datatable"
+      :value="relations"
+      paginator
+      :rows="15"
+      removable-sort
+      table-style="min-width: 1rem"
+    >
+      <Column field="link" header="Link" sortable>
+        <template #body="slotProps">
+          <AppLink :to="slotProps.data.link">{{
+            slotProps.data.concept
+          }}</AppLink>
+        </template>
+      </Column>
+      <Column field="relations" header="Semantic Relations" sortable></Column>
+      <template #footer>
+        <div style="text-align: right">
+          <Button label="Eksport" @click="exportCSV($event)" />
+        </div>
+      </template>
+    </DataTable>
   </section>
 </template>
 
@@ -21,10 +48,13 @@ const relations = computed(() => {
         .slice(-1)[0]
         .split("-3A")
         .slice(-1)[0],
-      link: e.concept.value.split("/").slice(-1)[0].split("-3A").join("/"),
+      link:
+        "https://wiki.terminologi.no/index.php?title=" +
+        e.concept.value.split("/").slice(-1)[0].split("-3A").join(":"),
       relations: e.relations.value
         .split(";")
-        .map((rel) => rel.split("/").slice(-1)[0].split("-3A").slice(-1)[0]).join(", "),
+        .map((rel) => rel.split("/").slice(-1)[0].split("-3A").slice(-1)[0])
+        .join(", "),
     };
   });
 });
