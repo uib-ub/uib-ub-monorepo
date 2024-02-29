c95abef4cc3bb07648f45107f86a7f56ec8b02c5
Jan Ole Bangen <Jan.Bangen@uib.no>
2024-02-29 11:29:49 +0100

    feat(tpa): add option to overwrite target for AppLink

--- packages/termportal-ui/components/AppLink.vue
+++ packages/termportal-ui/components/AppLink.vue
@@ -1,18 +1,26 @@
 <template>
-  <NuxtLink v-if="linkType === 'internal'" :to="to"><slot /></NuxtLink>
-  <a v-else-if="linkType === 'pageinternal'" :href="to"><slot /></a>
-  <NuxtLink v-else-if="linkType === 'external'" :to="to" target="_blank"
+  <NuxtLink v-if="linkType === 'internal'" :to="to" :target="target"
+    ><slot
+  /></NuxtLink>
+  <a v-else-if="linkType === 'pageinternal'" :href="to" :target="target"
+    ><slot
+  /></a>
+  <NuxtLink
+    v-else-if="linkType === 'external'"
+    :to="to"
+    :target="target || '_blank'"
     ><span class="icon-pad">
       <slot />
     </span>
     <Icon name="mdi:external-link" aria-hidden="true"
   /></NuxtLink>
-  <NuxtLink v-else :to="to"><slot /></NuxtLink>
+  <NuxtLink v-else :to="to" :target="target"><slot /></NuxtLink>
 </template>
 
 <script setup lang="ts">
 const props = defineProps({
   to: { type: String, required: true },
+  target: { type: String, required: false },
 });
 const linkType = computed(() => {
   if (props.to.startsWith("/")) {
