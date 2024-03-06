2c408b91b063f7d50f75d609de7bc4ceb86e9949
Henrik Askjer <Henrik.Askjer@uib.no>
2024-03-06 12:20:14 +0100

    fix missing loading placeholder

--- apps/sps-stadnamn-web/src/components/ui/icon-button.tsx
+++ apps/sps-stadnamn-web/src/components/ui/icon-button.tsx
@@ -1,25 +1,20 @@
-"use-client"
-
 import dynamic from 'next/dynamic'
-import { Suspense } from 'react'
-
-const TooltipButton = dynamic(() => import('./tooltip-button'), { ssr: false })
 
 export default function IconButton({ children, className, textClass, type='button', label, ...rest }: { children: React.ReactNode, className?: string, textClass?: string, label: string, [x: string]: any }) {
+  const TooltipButton = dynamic(() => import('./tooltip-button'), { ssr: false ,
+    loading: () => (
+        <button type={type} className={className} aria-label={textClass ? undefined : label}>
+          {textClass ? <span className={textClass}>{label}</span> : null}
+            <i  aria-hidden='true'>
+                      {children}
+                </i> 
+        </button>
+      )
+  })
+  
 
   return (
-  
-    <Suspense fallback={
-      <button type={type} className={className} aria-label={textClass ? undefined : label}>
-        {textClass ? <span className={textClass}>{label}</span> : null}
-          <i  aria-hidden='true'>
-                    {children}
-              </i> 
-      </button>
-
-    }>
-  <TooltipButton className={className} type={type} textClass={textClass} label={label}  {...rest}>{children}</TooltipButton>
-  </Suspense>
+    <TooltipButton className={className} type={type} textClass={textClass} label={label}  {...rest}>{children}</TooltipButton>
     
   );
 }
