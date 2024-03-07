19a416a1c262da7f4ddbde2feda5f1c86f275763
Henrik Askjer <Henrik.Askjer@uib.no>
2024-03-07 10:22:05 +0100

    remove unnecessary dynamic import in icon button

--- apps/sps-stadnamn-web/src/components/ui/icon-button.tsx
+++ apps/sps-stadnamn-web/src/components/ui/icon-button.tsx
@@ -1,20 +1,27 @@
-import dynamic from 'next/dynamic'
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+  } from "@/components/ui/tooltip"
 
-export default function IconButton({ children, className, textClass, type='button', label, ...rest }: { children: React.ReactNode, className?: string, textClass?: string, label: string, [x: string]: any }) {
-  const TooltipButton = dynamic(() => import('./tooltip-button'), { ssr: false ,
-    loading: () => (
-        <button type={type} className={className} aria-label={textClass ? undefined : label}>
-          {textClass ? <span className={textClass}>{label}</span> : null}
-            <i  aria-hidden='true'>
-                      {children}
-                </i> 
-        </button>
-      )
-  })
-  
 
-  return (
-    <TooltipButton className={className} type={type} textClass={textClass} label={label}  {...rest}>{children}</TooltipButton>
-    
-  );
+export default function TooltipButton({ children, className, textClass, label, ...rest }: { children: React.ReactNode, className?: string, textClass?: string, label: string, [x: string]: any }) {
+    return (
+
+        <TooltipProvider>
+            <Tooltip>
+                <TooltipTrigger className={className} aria-label={label} {...rest}>
+                    {textClass ? <span className={textClass}>{label}</span> : null}
+                    <i  aria-hidden='true'>
+                        {children}
+                    </i>
+                </TooltipTrigger>
+                <TooltipContent>
+                {label}
+                </TooltipContent>
+            </Tooltip>
+        </TooltipProvider>
+
+    );
 }
