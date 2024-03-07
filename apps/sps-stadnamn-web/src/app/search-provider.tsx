324b5c7a44b6d1c4c90387708098b5d6de30f358
Henrik Askjer <Henrik.Askjer@uib.no>
2024-03-07 18:45:06 +0100

    fix false alarm no results

--- apps/sps-stadnamn-web/src/app/search-provider.tsx
+++ apps/sps-stadnamn-web/src/app/search-provider.tsx
@@ -14,35 +14,33 @@ interface SearchContextData {
  
   export const SearchContext = createContext<SearchContextData>({
     resultData: null,
-    isLoading: false,
-    mapBounds: [],
-  });
+    isLoading: true,
+    mapBounds: []
+    });
 
  
 export default function SearchProvider({ children }: {  children: React.ReactNode }) {
     const [resultData, setResultData] = useState<ResultData | null>(null);
-    const [isLoading, setIsLoading] = useState(false)
+    const [isLoading, setIsLoading] = useState(true)
     const [mapBounds, setMapBounds] = useState<[number, number][]>([]);
     const params = useParams()
     const searchParams = useQueryStringWithout(['docs'])
 
     useEffect(() => {
-        if (searchParams?.length) {
-            console.log("PARAMS", searchParams)
-            setIsLoading(true)
-            console.log("FETCHING ", `/api/search?dataset=${params.dataset}&${searchParams}`)
-            fetch(`/api/search?dataset=${params.dataset}&${searchParams}`).then(response => response.json()).then(es_data => {
-            if (es_data.aggregations?.viewport?.bounds) {
-                setMapBounds([[es_data.aggregations.viewport.bounds.top_left.lat, es_data.aggregations.viewport.bounds.top_left.lon],
-                    [es_data.aggregations.viewport.bounds.bottom_right.lat, es_data.aggregations.viewport.bounds.bottom_right.lon]])
-            }
-            else {
-                setMapBounds([])
-            }
-            setResultData(es_data)
-    
-            }).then(() => setIsLoading(false))
+        setIsLoading(true)
+        console.log("FETCHING ", `/api/search?dataset=${params.dataset}&${searchParams}`)
+        fetch(`/api/search?dataset=${params.dataset}&${searchParams}`).then(response => response.json()).then(es_data => {
+        if (es_data.aggregations?.viewport?.bounds) {
+            setMapBounds([[es_data.aggregations.viewport.bounds.top_left.lat, es_data.aggregations.viewport.bounds.top_left.lon],
+                [es_data.aggregations.viewport.bounds.bottom_right.lat, es_data.aggregations.viewport.bounds.bottom_right.lon]])
         }
+        else {
+            setMapBounds([])
+        }
+        setResultData(es_data)
+
+        }).then(() => setIsLoading(false))
+        
       }, [searchParams, params.dataset])
 
   return <SearchContext.Provider value={{resultData, isLoading, mapBounds}}>{children}</SearchContext.Provider>
