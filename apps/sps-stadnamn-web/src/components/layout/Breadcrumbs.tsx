5656676cae8ab73163c02fd7f36bdc8b593bc098
Henrik Askjer <Henrik.Askjer@uib.no>
2024-03-11 16:59:52 +0100

    blue breadcrumb links

--- apps/sps-stadnamn-web/src/components/layout/Breadcrumbs.tsx
+++ apps/sps-stadnamn-web/src/components/layout/Breadcrumbs.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Breadcrumbs({ parentUrl, parentName, currentName}: { parentUrl: string, parentName: string, currentName: string }) {
     return (
         <div className="flex flex-row items-center mb-6">
-            <Link className="no-underline text-primary-600 font-semibold hover:underline" href={parentUrl}>
+            <Link className="no-underline text-accent-700 hover:underline decoration-accent-700" href={parentUrl}>
                 {parentName}
             </Link>
             <span className="mx-2">/</span>
