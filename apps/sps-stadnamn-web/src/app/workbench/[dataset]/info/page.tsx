98e7ee3fe52fc3c55c430815e79bf1d57e96c1b9
Henrik Askjer <Henrik.Askjer@uib.no>
2024-03-11 16:59:43 +0100

    add go to search button

--- apps/sps-stadnamn-web/src/app/workbench/[dataset]/info/page.tsx
+++ apps/sps-stadnamn-web/src/app/workbench/[dataset]/info/page.tsx
@@ -1,10 +1,11 @@
 import { datasetPresentation, datasetTitles, subpages } from '@/config/client-config'
-import Image from 'next/image'
 import Link from 'next/link'
-import { PiArrowRight, PiArrowRightBold, PiCaretRightBold } from 'react-icons/pi'
+import { PiCaretRightBold } from 'react-icons/pi'
+import GoToSearchButtons from './GoToSearchButtons'
 
 export default function Subpage( { params }: { params: { dataset: string, subpage: string } }) {
     let [mainIndex, subindex] = params.dataset.split("_")
+    
 
     let info = datasetPresentation[mainIndex]
 
@@ -18,15 +19,17 @@ export default function Subpage( { params }: { params: { dataset: string, subpag
     return (
         <>
             <h2><span className='sr-only'>Om </span>{datasetTitles[mainIndex]}</h2>
+            
             <div className='flex flex-col md:flex-row'>
                 <div className='md:w-1/2'>
                     <p>{info['description']}</p>
+                    <GoToSearchButtons/>
                     { subpages[mainIndex]?.length &&
                     <>
                     <h3>Artikler</h3>
                     <ul className="flex flex-col gap-3 mt-1 !list-none !pl-0 mb-4">
                     { subpages[mainIndex]?.map((subpage, index) => {
-                        return <li key={index}><Link className="no-underline hover:underline text-pretty" href={`/workbench/${params.dataset}/info/${index + 1}`}>{subpage}<PiCaretRightBold aria-hidden="true" className='text-primary-600 inline align-middle ml-1'/></Link></li>
+                        return <li key={index}><Link className="no-underline text-pretty" href={`/workbench/${params.dataset}/info/${index + 1}`}>{subpage}<PiCaretRightBold aria-hidden="true" className='text-primary-600 inline align-middle ml-1'/></Link></li>
                     })
                     }
                     </ul>
