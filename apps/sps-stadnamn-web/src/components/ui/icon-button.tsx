5ff8362e4f8a8b1c63a6e21765470c74f2fb8a7f
Henrik Askjer <Henrik.Askjer@uib.no>
2024-03-13 07:51:32 +0100

    prevent sort button from submitting form

--- apps/sps-stadnamn-web/src/components/ui/icon-button.tsx
+++ apps/sps-stadnamn-web/src/components/ui/icon-button.tsx
@@ -7,12 +7,12 @@ import {
   } from "@/components/ui/tooltip"
 
 
-export default function TooltipButton({ children, className, textClass, label, ...rest }: { children: React.ReactNode, className?: string, textClass?: string, label: string, [x: string]: any }) {
+export default function TooltipButton({ children, className, textClass, label, type, ...rest }: { children: React.ReactNode, className?: string, textClass?: string, label: string, [x: string]: any, type?: "button" | "submit" | "reset" }) {
     return (
 
         <TooltipProvider>
             <Tooltip>
-                <TooltipTrigger className={className} aria-label={label} {...rest}>
+                <TooltipTrigger className={className} aria-label={label} type={type || "button"} {...rest}>
                     {textClass ? <span className={textClass}>{label}</span> : null}
                     <i  aria-hidden='true'>
                         {children}
