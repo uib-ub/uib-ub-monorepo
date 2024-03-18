d545efce31e61349756903d9e62dc9555e4fca02
Tarje Lavik <tarje.lavik@gmail.com>
2024-03-18 11:01:04 +0100

    add manifests template

---
+++ apps/api/src/config/elasticsearch/mappings/manifests.ts
@@ -0,0 +1,14 @@
+export const manifests = {
+  "properties": {
+    "id": {
+      "type": "keyword",
+    },
+    "label": {
+      "type": "keyword",
+    },
+    "manifest": {
+      "type": "object",
+      "enabled": false
+    },
+  }
+}
