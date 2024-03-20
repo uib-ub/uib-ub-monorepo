75cfe5a7f1c32093039c168ec9e46053749e0ec3
Henrik Askjer <Henrik.Askjer@uib.no>
2024-03-20 18:30:27 +0100

    fix search bar not filling height of parent

--- apps/sps-stadnamn-web/src/app/view/[dataset]/@searchSection/SearchBar.tsx
+++ apps/sps-stadnamn-web/src/app/view/[dataset]/@searchSection/SearchBar.tsx
@@ -35,17 +35,19 @@ export default function SearchBar() {
     return (
         <>
         <div className="flex gap-2 px-2 items-stretch">
-            <div className="border border-neutral-500 w-full !h-full rounded-sm flex">
+            <div className="border border-neutral-500 relative w-full rounded-sm">
+                <div className="flex w-full h-full">
                 {fieldConfig[dataset] &&
                 <select name="field" value={searchParams.get('field') || fieldConfig[dataset][0].key} className="m-2"  onChange={changeField}>
                     {fieldConfig[dataset].map((field: any) => <option key={field.key} value={field.key}>{field.label}</option>)}
                 </select>}
-            <input type="text" 
+                <input type="text" 
                    ref={inputRef}
                    name="q" 
                    value={inputValue} 
                    onChange={(event) => setInputValue(event.target.value)}    
-                   className='!w-full relative h-full px-2 border-l border-neutral-300 focus:outline-none'/>
+                   className='!w-full !h-full px-2 border-l border-neutral-300 focus:outline-none'/>
+            </div>
             { inputValue && 
             <IconButton type="button" onClick={clearInput} label="Tøm søk" className="absolute right-2 top-1/2 transform -translate-y-1/2"><PiX className="text-lg"/></IconButton> }
             </div>
