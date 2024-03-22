e91ad2e3e632083997fa7f61028e2932ca2d52ff
Jan Ole Bangen <Jan.Bangen@uib.no>
2024-03-22 16:35:52 +0100

    feat(tpa): add domain explore page

---
+++ apps/sps-termportal-admin/pages/explore/domain.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex">
+    <SideBar />
+    <main class="space-y-4 pt-8">
+      <section>
+        <h1 class="mb-6 text-2xl">Domain Exploration</h1>
+        <SimpleDomainOverview />
+      </section>
+    </main>
+  </div>
+</template>
