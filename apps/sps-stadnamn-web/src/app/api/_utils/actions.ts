44e667d23a894f09fb3fdb8fe2dfb9b620c2bd14
Henrik Askjer <Henrik.Askjer@uib.no>
2024-03-27 13:18:20 +0100

    fix env variable

--- apps/sps-stadnamn-web/src/app/api/_utils/actions.ts
+++ apps/sps-stadnamn-web/src/app/api/_utils/actions.ts
@@ -1,7 +1,7 @@
 export async function fetchDoc(params: any, retry: boolean = true) {
     'use server'
-    const endpoint = (process.env.SN_ENV == 'prod' && retry ) || !retry ? process.env.SN_ENDPOINT : process.env.ES_ENDPOINT_TEST
-    const token = endpoint == process.env.SN_ENDPOINT ? process.env.ES_TOKEN : process.env.ES_TOKEN_TEST
+    const endpoint = (process.env.SN_ENV == 'prod' && retry ) || !retry ? process.env.ES_ENDPOINT : process.env.ES_ENDPOINT_TEST
+    const token = endpoint == process.env.ES_ENDPOINT ? process.env.ES_TOKEN : process.env.ES_TOKEN_TEST
     const res = await fetch(`${endpoint}stadnamn-${process.env.SN_ENV}-${params.dataset}/_doc/${params.uuid}`, {
         method: 'GET',
         headers: {
