6a88be01acc522584793f6499c75bb5e53029c96
Henrik Askjer <Henrik.Askjer@uib.no>
2024-03-27 12:45:46 +0100

    fix popup links

--- apps/sps-stadnamn-web/src/components/Map/PopupList.tsx
+++ apps/sps-stadnamn-web/src/components/Map/PopupList.tsx
@@ -1,6 +1,6 @@
 
 import IconButton  from '@/components/ui/icon-button'
-import { useSearchParams } from 'next/navigation'
+import { useSearchParams, useParams, useRouter } from 'next/navigation'
 import { PiArticleFill, PiInfoFill, PiLinkBold } from 'react-icons/pi'
 import AudioButton from '@/components/results/audioButton';
 import Link from 'next/link';
@@ -8,62 +8,88 @@ import { resultRenderers } from '@/config/dataset-render-config';
 
 export default function PopupList({ docs, view }: { docs: any[], view: string} ) {
     const searchParams = useSearchParams()
+    const dataset = docs[0]._index.split('-')[1];
+    const router = useRouter()
+    const params = useParams<{uuid: string; dataset: string}>()
 
-    const goToView = (uuid: string, view: string, manifest?: string) => {
-        const params = new URLSearchParams(searchParams)
-        params.set('docs', String(uuid))
-        params.set('view', String(view))
-        if (manifest) {
-          params.set('manifest', String(manifest))
-        }
+    const goToDoc = (uuid: string) => {
+      const newSearchParams = new URLSearchParams(searchParams)
+      newSearchParams.delete('docs')
+      router.push(`/view/${params.dataset}/doc/${uuid}?${newSearchParams.toString()}`)
     }
-        
-    return (
-        <ul className='flex flex-col'>
-        {docs.map(doc => {
-            const dataset = doc._index.split('-')[1];
-            return (
-            <li key={doc._id} className='text-lg inline space-x-1'>
-                
-              
-              <span className='inline space-x-1'>
-                {doc._source.image && 
-                  <IconButton 
-                    onClick={() => goToView(doc._id, 'image', doc._source.image.manifest)} 
-                    label="Vis seddel">
-                      <PiArticleFill className="text-xl text-neutral-700 inline"/>
-                  </IconButton> 
-                }
-                {doc._source.audio && 
-                  <AudioButton audioFile={`https://iiif.test.ubbe.no/iiif/audio/${dataset}/${doc._source.audio.file}`} 
-                    className="text-2xl inline text-neutral-700"/> 
-                }
-                {doc._source.link &&
-                  <Link href={doc._source.link} className="no-underline" target="_blank">
-                    <IconButton 
-                      label="Ekstern ressurs">
-                        <PiLinkBold className="text-xl text-neutral-700 inline"/>
-                    </IconButton> 
-                  </Link>
-                }
-                <IconButton label="Infoside"><PiInfoFill className='text-2xl text-primary-600 inline'/></IconButton>
-              </span>
-              { dataset == view ?
-                  <div>
-                  {resultRenderers[dataset]?.title(doc)}
-                  {resultRenderers[dataset]?.details(doc)}
-                  </div>
-                  :
-                  <>
-                  {doc._source.label}
-                  </>
-                  
+
+    const goToIIIF = (uuid: string, manifest: string) => {
+      const newSearchParams = new URLSearchParams(searchParams)
+      newSearchParams.set('docs', String(uuid))
+      router.push(`/view/${params.dataset}/iiif/${manifest}?${newSearchParams.toString()}`)
+    }
+
+    const listItemRenderer = (doc: any) => {
+        return (
+            <>
+            { dataset == view ?
+                    <div>
+                        {resultRenderers[view]?.title(doc)}
+                        {resultRenderers[view]?.details(doc)}
+                    </div>
+                    :
+                    <>
+                        {doc._source.label}
+                    </>
                 } 
-            </li>
-            )
-        }
-        )}
-        
-        </ul>
-    )
+                <span className='inline space-x-1'>
+                    {doc._source.image && 
+                        <IconButton 
+                            onClick={() => goToIIIF(doc._id, doc._source.image.manifest)} 
+                            label="Vis seddel">
+                            <PiArticleFill className="text-xl text-neutral-700 inline"/>
+                        </IconButton> 
+                    }
+                    {doc._source.audio && 
+                        <AudioButton audioFile={`https://iiif.test.ubbe.no/iiif/audio/${doc._source.audio.file}`} 
+                            className="text-2xl inline text-neutral-700"/> 
+                    }
+                    {doc._source.link &&
+                        <Link href={doc._source.link} className="no-underline" target="_blank">
+                            <IconButton 
+                                label="Ekstern ressurs">
+                                <PiLinkBold className="text-xl text-neutral-700 inline"/>
+                            </IconButton> 
+                        </Link>
+                    }
+                    <IconButton label="Infoside" onClick={() => goToDoc(doc._source.uuid)}><PiInfoFill className='text-2xl text-primary-600 inline'/></IconButton>
+                </span>
+                
+            </>
+        )
+    }
+
+
+    const listRenderer = () => {
+      return docs.map(doc => {
+          return (
+          <li key={doc._id} className='text-lg inline space-x-1'>
+              {listItemRenderer(doc)}
+          </li>
+          )
+      }
+      )
+    }
+
+    if (docs.length > 1) {
+        return (
+          <ul className='flex flex-col'>
+            {listRenderer()}
+          </ul>
+        )
+    }
+    else {
+        return (
+            <div className='text-lg inline space-x-1'>
+                {listItemRenderer(docs[0])}
+            </div>
+        )
+    }
+
+
     }
