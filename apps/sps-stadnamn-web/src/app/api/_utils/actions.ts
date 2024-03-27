894cd482564ef19e12952f5829a29d38429c0727
Henrik Askjer <Henrik.Askjer@uib.no>
2024-03-27 10:29:25 +0100

    fix endpoint logic

--- apps/sps-stadnamn-web/src/app/api/_utils/actions.ts
+++ apps/sps-stadnamn-web/src/app/api/_utils/actions.ts
@@ -1,8 +1,8 @@
 export async function fetchDoc(params: any, retry: boolean = true) {
     'use server'
-    const endpoint = (process.env.SN_ENV != 'prod' && retry ) || !retry ? process.env.SN_ENDPOINT : process.env.SN_ENDPOINT_TEST
+    const endpoint = (process.env.SN_ENV == 'prod' && retry ) || !retry ? process.env.SN_ENDPOINT : process.env.SN_ENDPOINT_TEST
     const token = endpoint == process.env.SN_ENDPOINT ? process.env.SN_TOKEN : process.env.SN_TOKEN_TEST
-    const res = await fetch(`${endpoint}/stadnamn-${process.env.SN_ENV}-${params.dataset}/_doc/${params.uuid}`, {
+    const res = await fetch(`${endpoint}stadnamn-${process.env.SN_ENV}-${params.dataset}/_doc/${params.uuid}`, {
         method: 'GET',
         headers: {
         'Content-Type': 'application/json',
