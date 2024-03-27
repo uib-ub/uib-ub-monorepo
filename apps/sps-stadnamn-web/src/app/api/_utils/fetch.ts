e3fdf20e83ba1b30cb5ac883847378d04d6325ff
Henrik Askjer <Henrik.Askjer@uib.no>
2024-03-27 13:14:44 +0100

    update env variable names

--- apps/sps-stadnamn-web/src/app/api/_utils/fetch.ts
+++ apps/sps-stadnamn-web/src/app/api/_utils/fetch.ts
@@ -1,6 +1,6 @@
 export async function postQuery(dataset: string, query: any, retry: boolean = true) {
-    const endpoint = (process.env.SN_ENV == 'prod' && retry ) || !retry ? process.env.SN_ENDPOINT : process.env.SN_ENDPOINT_TEST
-    const token = endpoint == process.env.SN_ENDPOINT ? process.env.SN_TOKEN : process.env.SN_TOKEN_TEST
+    const endpoint = (process.env.SN_ENV == 'prod' && retry ) || !retry ? process.env.ES_ENDPOINT : process.env.ES_ENDPOINT_TEST
+    const token = endpoint == process.env.ES_ENDPOINT ? process.env.ES_TOKEN : process.env.ES_TOKEN_TEST
 
         const res = await fetch(`${endpoint}stadnamn-${process.env.SN_ENV}-${dataset}/_search`, {
         method: 'POST',
