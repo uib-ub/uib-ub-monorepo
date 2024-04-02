cf0be7e23c1393edb81fe8678b28cd7bc8b45715
Henrik Askjer <Henrik.Askjer@uib.no>
2024-04-02 13:24:54 +0200

    handle aa and å

--- apps/sps-stadnamn-web/src/app/api/_utils/query-string.ts
+++ apps/sps-stadnamn-web/src/app/api/_utils/query-string.ts
@@ -1,19 +1,32 @@
+function modifyQuery(query: string) {
+  const lowerCaseQuery = query.toLowerCase();
+
+  if (lowerCaseQuery.includes('aa')) {
+    return `(${query}) OR (${query.replace(/aa/gi, 'å')})`;
+  }
+
+  if (lowerCaseQuery.includes('å')) {
+    return `(${query}) OR (${query.replace(/å/gi, 'aa')})`;
+  }
+
+  return query;
+}
+
 export function getQueryString(params: { [key: string]: string | null }) {
+
   const simple_query_string = params.q ? {
     "query_string": {
-      "query": params.q,
+      "query": modifyQuery(params.q),
       "allow_leading_wildcard": true,
-      "default_operator": "AND",
+      "default_operator": params.field ? "OR" : "AND",
       "fields": [params.field || "label"]
     }} : null
 
+  const highlight = params.q && params.field && params.field != 'label' ? {
+    fields: {
+        [params.field]: {}
+    }
+    } : null
 
-    const highlight = params.q && params.field && params.field != 'label' ? {
-        fields: {
-            [params.field]: {}
-        }
-        } : null
-    
-
-    return { highlight, simple_query_string}
+  return { highlight, simple_query_string}
 }
