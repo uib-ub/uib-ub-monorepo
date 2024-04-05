26e4ed3b38a924c2f2ff1969291b3d9d1a49d806
Henrik Askjer <Henrik.Askjer@uib.no>
2024-04-05 12:56:15 +0200

    hide items without value

--- apps/sps-stadnamn-web/src/components/ui/infobox.tsx
+++ apps/sps-stadnamn-web/src/components/ui/infobox.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { PiMagnifyingGlass } from "react-icons/pi";
 
 export default function InfoBox({ items: items, dataset }: { items: Record<string,any>[], dataset: string}) {
-    const filteredItems = items.filter(item =>  item.value?.length || item.items?.length);
+    const filteredItems = items.filter(item =>  item.value?.length || (item.items?.length && item.items[0].value?.length));
     return (
       <div className="flex flex-wrap gap-8">
         {filteredItems.map((item: Record<string,any> , index: number) => (
