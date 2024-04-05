dfda66edbc86fd0bc6470d635eaede98641aad98
Jan Ole Bangen <Jan.Bangen@uib.no>
2024-04-05 11:28:45 +0200

    refactor(tpw): termpost display with comp folder

---
+++ apps/sps-termportal-web/components/termpost/Base.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="grid gap-y-5">
+    <div v-for="lang in displayInfo?.displayLanguages" :key="'disp_' + lang">
+      <TermpostLanguageSection :concept="concept" :lang="lang" />
+    </div>
+    <TermpostSymbolSection
+      v-if="displayInfo?.symbol"
+      :display-info="displayInfo"
+    />
+    <TermpostRelationSection
+      v-if="displayInfo?.semanticRelations"
+      :display-info="displayInfo"
+    />
+    <TermpostGenerelSection
+      v-if="displayInfo"
+      :concept="concept"
+      :display-info="displayInfo"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+if (process.client) {
+  useHead({
+    script: [
+      {
+        src: "/mathjax-config.js",
+        type: "text/javascript",
+        defer: true,
+      },
+      {
+        id: "MathJax-script",
+        type: "text/javascript",
+        src: "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js",
+        defer: true,
+      },
+    ],
+  });
+}
+
+const dataDisplayLanguages = useDataDisplayLanguages();
+
+const props = defineProps({
+  data: { type: Object, required: true },
+  mainConceptId: { type: String, required: true },
+});
+
+const concept = computed(() => {
+  return props.data.concept[props.mainConceptId];
+});
+
+const displayInfo = computed(() => {
+  if (props.data?.meta) {
+    const conceptLanguages = props.data.meta?.language;
+    const displayLanguages = dataDisplayLanguages.value.filter((language) =>
+      Array.from(conceptLanguages).includes(language)
+    );
+    const info = {
+      conceptLanguages,
+      displayLanguages,
+      altLabelLength: props.data?.meta.maxLen.altLabel,
+      hiddenLabelLength: props.data?.meta.maxLen.hiddenLabel,
+    };
+    // semantic relations
+    for (const relationType of semanticRelationTypes) {
+      const relData = getRelationData(
+        props.data?.concept,
+        props.mainConceptId,
+        relationType
+      );
+      if (relData) {
+        if (info.semanticRelations) {
+          info.semanticRelations[relationType] = relData;
+        } else {
+          info.semanticRelations = {};
+          info.semanticRelations[relationType] = relData;
+        }
+      }
+    }
+    // subjects
+    if (props.data.concept?.[props.mainConceptId]?.subject) {
+      const subj = props.data.concept[props.mainConceptId].subject;
+      let subjectlist;
+      if (typeof subj[0] === "string") {
+        subjectlist = subj;
+      } else {
+        subjectlist = subj.map((subj) => {
+          return subj["@value"];
+        });
+      }
+      info.subject = subjectlist.join(", ");
+    }
+
+    // notation: symbols and images
+    if (props.data.concept?.[props.mainConceptId]?.notation) {
+      const notation = props.data.concept?.[props.mainConceptId]?.notation;
+      info.symbol = notation.filter((notation) =>
+        notation?.type.includes("skosxl:Label")
+      );
+      info.image = notation.filter((notation) =>
+        notation?.type.includes(
+          "http://wiki.terminologi.no/index.php/Special:URIResolver/Category-3ADct-3AImage"
+        )
+      );
+    }
+
+    return info;
+  } else {
+    return null;
+  }
+});
+
+onMounted(() => {
+  if (typeof window?.MathJax !== "undefined") {
+    window.MathJax.typesetPromise();
+  }
+});
+</script>
---
+++ apps/sps-termportal-web/components/termpost/RelationSection.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h3 id="relasjon" class="pb-1 text-xl">
+      <AppLink to="#relasjon"> {{ $t("id.relasjon") }}</AppLink>
+    </h3>
+    <TermSection>
+      <template v-for="relationType in semanticRelationTypes">
+        <TermProp
+          v-if="displayInfo.semanticRelations[relationType]"
+          :key="relationType"
+          :label="$t('id.' + relationType)"
+        >
+          <TermDescription
+            prop="link"
+            :data="displayInfo.semanticRelations[relationType]"
+          />
+        </TermProp>
+      </template>
+    </TermSection>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({ displayInfo: { type: Object, required: true } });
+</script>
