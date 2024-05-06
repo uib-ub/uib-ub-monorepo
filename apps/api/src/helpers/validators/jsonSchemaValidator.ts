4a50d9b3a66f108f62f2858ef66f4363c7a0c0d5
Tarje Lavik <tarje.lavik@gmail.com>
2024-05-06 13:26:46 +0200

    Update schema references and file paths

--- apps/api/src/helpers/validators/jsonSchemaValidator.ts
+++ apps/api/src/helpers/validators/jsonSchemaValidator.ts
@@ -1,9 +1,9 @@
 // TODO: move to a shared location
 import addFormats from "ajv-formats";
 import Ajv from "ajv/dist/2020";
-import * as core from 'types/src/la-schemas/core.json';
-import * as object from 'types/src/la-schemas/object.json';
-import * as person from 'types/src/la-schemas/person.json';
+import * as core from 'types/src/la/schemas/core.json';
+import * as object from 'types/src/la/schemas/object.json';
+import * as person from 'types/src/la/schemas/person.json';
 
 const jsonSchemaValidator = new Ajv()
 addFormats(jsonSchemaValidator)
