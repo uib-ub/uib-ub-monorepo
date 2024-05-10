480beeeefbab687d0094ad2339b5bdc9f83131fa
Jan Ole Bangen <Jan.Bangen@uib.no>
2024-05-10 10:35:44 +0200

    refactor(tpw): search filter options

--- apps/sps-termportal-web/components/SearchFilter.vue
+++ apps/sps-termportal-web/components/SearchFilter.vue
@@ -8,41 +8,7 @@
     <div
       class="grid grid-cols-1 gap-4 rounded border-gray-300 xs:grid-cols-2 md:grid-cols-4 xl:grid-cols-1"
     >
-      <template
-        v-for="{ title, key, data } in [
-          {
-            title: $t('global.language'),
-            key: 'lang',
-            data: intersectUnique(
-              localeLangOrder,
-              Object.keys(searchDataStats.lang || {})
-            ),
-          },
-          {
-            title: '',
-            key: 'context',
-            fkey: 'context',
-            data: Object.keys(searchDataStats.context || {}).sort(),
-          },
-          {
-            title: $t('searchFilter.termproperty'),
-            key: 'predicate',
-            data: intersectUnique(
-              predicateOrder,
-              Object.keys(searchDataStats.predicate || {})
-            ),
-          },
-          {
-            title: $t('searchFilter.matching'),
-            key: 'matching',
-            data: intersectUnique(
-              matchingOrder,
-              Object.keys(searchDataStats.matching || {})
-            ),
-          },
-        ]"
-        :key="title"
-      >
+      <template v-for="{ title, key, data } in filterSections()" :key="title">
         <SearchFilterFieldset
           v-if="displaySection(key, data)"
           :title="title"
@@ -62,16 +28,55 @@
 </template>
 
 <script setup lang="ts">
+import { useI18n } from "vue-i18n";
+
 const showSearchFilter = useShowSearchFilter();
 const searchDataStats = useSearchDataStats();
 const localeLangOrder = useLocaleLangOrder();
 const searchInterface = useSearchInterface();
+const i18n = useI18n();
 
 const props = defineProps({
   placement: { type: String, default: "default" },
 });
 
+const filterSections = () => {
+  return [
+    {
+      title: i18n.t("global.language"),
+      key: "lang",
+      data: intersectUnique(
+        localeLangOrder.value,
+        Object.keys(searchDataStats.value.lang || {})
+      ),
+    },
+    {
+      title: "",
+      key: "context",
+      fkey: "context",
+      data: Object.keys(searchDataStats.value.context || {}).sort(),
+    },
+    {
+      title: i18n.t("searchFilter.termproperty"),
+      key: "predicate",
+      data: intersectUnique(
+        predicateOrder,
+        Object.keys(searchDataStats.value.predicate || {})
+      ),
+    },
+    {
+      title: i18n.t("searchFilter.matching"),
+      key: "matching",
+      data: intersectUnique(
+        matchingOrder,
+        Object.keys(searchDataStats.value.matching || {})
+      ),
+    },
+  ];
+};
+
 const displaySection = (key, data) => {
+  // don't display language filter if language selected in search interface
   if (key === "lang") {
     return searchInterface.value.language === "all";
   } else if (key === "matching") {
