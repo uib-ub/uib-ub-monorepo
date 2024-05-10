2ec5dd4cc9dd21e91260b66fbdb3ee5d99193eef
Tarje Lavik <tarje.lavik@gmail.com>
2024-05-10 11:12:14 +0200

    Pass env to server when testing

--- apps/api/src/tests/items.test.ts
+++ apps/api/src/tests/items.test.ts
@@ -1,12 +1,13 @@
+import { env } from '../config/env'
 import server from '../main'
 
 describe('Items', () => {
   test('GET /items', async () => {
-    const res = await server.request('http://localhost/items')
+    const res = await server.request('http://localhost/items', {}, env)
     expect(res.status).toBe(200)
   })
   test('GET /items?limit=2', async () => {
-    const res = await server.request('http://localhost/items?limit=2')
+    const res = await server.request('http://localhost/items?limit=2', {}, env)
     expect(res.status).toBe(200)
     const data = await res.json()
     expect(data.length).toBe(2)
