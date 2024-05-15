0fcbbccbb62979260a25d61e67276f3c4034eb35
Tarje Lavik <tarje.lavik@gmail.com>
2024-05-15 11:41:26 +0200

    add a smoke test as example for other apps

--- apps/api/src/tests/items.test.ts
+++ apps/api/src/tests/items.test.ts
@@ -3,11 +3,11 @@ import server from '../main'
 
 describe('Items', () => {
   test('GET /items', async () => {
-    const res = await server.request('http://localhost/items', {}, env)
+    const res = await server.request('/items', {}, env)
     expect(res.status).toBe(200)
   })
   test('GET /items?limit=2', async () => {
-    const res = await server.request('http://localhost/items?limit=2', {}, env)
+    const res = await server.request('/items?limit=2', {}, env)
     expect(res.status).toBe(200)
     const data = await res.json()
     expect(data.length).toBe(2)
