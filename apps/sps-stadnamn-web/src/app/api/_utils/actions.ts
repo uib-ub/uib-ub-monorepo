2a287a9ed99f2404fb5466e36ae0f2b245ad5ad1
Henrik Askjer <Henrik.Askjer@uib.no>
2024-05-27 17:15:08 +0200

    handle document not found

--- apps/sps-stadnamn-web/src/app/api/_utils/actions.ts
+++ apps/sps-stadnamn-web/src/app/api/_utils/actions.ts
@@ -1,6 +1,7 @@
-export async function fetchDoc(params: any, firstAttempt: boolean = true) {
+
+export async function fetchDoc(params: any, retry: boolean = true) {
     'use server'
-    const endpoint = (process.env.SN_ENV == 'prod' ? firstAttempt : !firstAttempt) ? process.env.ES_ENDPOINT : process.env.ES_ENDPOINT_TEST
+    const endpoint = (process.env.SN_ENV == 'prod' ? retry : !retry) ? process.env.ES_ENDPOINT : process.env.ES_ENDPOINT_TEST
     const token = endpoint == process.env.ES_ENDPOINT ? process.env.ES_TOKEN : process.env.ES_TOKEN_TEST
     const res = await fetch(`${endpoint}stadnamn-${process.env.SN_ENV}-${params.dataset}/_doc/${params.uuid}`, {
         method: 'GET',
@@ -12,10 +13,15 @@ export async function fetchDoc(params: any, firstAttempt: boolean = true) {
 
     if (!res.ok) {
         const errorResponse = await res.json();
-        if (firstAttempt) {
-            return fetchDoc(params, firstAttempt = false);
+        if (retry) {
+            return fetchDoc(params, retry = false);
         }
-        return {error: errorResponse.error.type.toUpperCase(), status: errorResponse.status};
+        if (errorResponse.error) {
+            return {error: errorResponse.error.type.toUpperCase(), status: errorResponse.status};
+        }
+        if (errorResponse.found == false) {
+            return {error: "DOCUMENT_NOT_FOUND", status: "404"}
+        }
     }
   const data = await res.json()
   return data
