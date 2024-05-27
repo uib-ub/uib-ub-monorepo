2b4a9788c05d03385e9d57904f545ab2baa17698
Henrik Askjer <Henrik.Askjer@uib.no>
2024-05-27 11:06:03 +0200

    less confusing parameter name

--- apps/sps-stadnamn-web/src/app/api/_utils/actions.ts
+++ apps/sps-stadnamn-web/src/app/api/_utils/actions.ts
@@ -1,6 +1,6 @@
-export async function fetchDoc(params: any, retry: boolean = true) {
+export async function fetchDoc(params: any, firstAttempt: boolean = true) {
     'use server'
-    const endpoint = (process.env.SN_ENV == 'prod' ? retry : !retry) ? process.env.ES_ENDPOINT : process.env.ES_ENDPOINT_TEST
+    const endpoint = (process.env.SN_ENV == 'prod' ? firstAttempt : !firstAttempt) ? process.env.ES_ENDPOINT : process.env.ES_ENDPOINT_TEST
     const token = endpoint == process.env.ES_ENDPOINT ? process.env.ES_TOKEN : process.env.ES_TOKEN_TEST
     const res = await fetch(`${endpoint}stadnamn-${process.env.SN_ENV}-${params.dataset}/_doc/${params.uuid}`, {
         method: 'GET',
@@ -12,8 +12,8 @@ export async function fetchDoc(params: any, retry: boolean = true) {
 
     if (!res.ok) {
         const errorResponse = await res.json();
-        if (retry) {
-            return fetchDoc(params, retry = false);
+        if (firstAttempt) {
+            return fetchDoc(params, firstAttempt = false);
         }
         return {error: errorResponse.error.type.toUpperCase(), status: errorResponse.status};
     }
