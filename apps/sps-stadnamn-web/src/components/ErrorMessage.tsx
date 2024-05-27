c5def91005239f5f06f24a4b050f003272b7362e
Henrik Askjer <Henrik.Askjer@uib.no>
2024-05-27 16:31:27 +0200

    larger font in error

--- apps/sps-stadnamn-web/src/components/ErrorMessage.tsx
+++ apps/sps-stadnamn-web/src/components/ErrorMessage.tsx
@@ -2,7 +2,7 @@
 export default function ErrorMessage({ error, message }: { error: Record<string, string>, message: string}) {
     return (
         <div  role="status" aria-live="polite" className="space-y-2 m-8 md:m-16">
-                <h2 className="text-2xl font-semibold">{error.status}</h2>
+                <h2 className="text-3xl font-semibold">{error.status}</h2>
                 <p className="text-xl">{message}</p>
                 <p className="text-neutral-700 mt-4">{error.error}</p>
         </div>
