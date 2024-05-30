e35b2ec3bb1c41d5e24a675dd7189583a890e7b9
Henrik Askjer <Henrik.Askjer@uib.no>
2024-05-30 16:20:14 +0200

    fix 404 in main search

--- apps/sps-stadnamn-web/src/app/api/_utils/actions.ts
+++ apps/sps-stadnamn-web/src/app/api/_utils/actions.ts
@@ -3,13 +3,37 @@ export async function fetchDoc(params: any, retry: boolean = true) {
     'use server'
     const endpoint = (process.env.SN_ENV == 'prod' ? retry : !retry) ? process.env.ES_ENDPOINT : process.env.ES_ENDPOINT_TEST
     const token = endpoint == process.env.ES_ENDPOINT ? process.env.ES_TOKEN : process.env.ES_TOKEN_TEST
-    const res = await fetch(`${endpoint}stadnamn-${process.env.SN_ENV}-${params.dataset}/_doc/${params.uuid}`, {
-        method: 'GET',
-        headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `ApiKey ${token}`,
+
+    let res
+
+    if (params.dataset == 'search') {
+        // Post a search query for the document
+        const query = {
+            query: {
+                terms: {
+                    "_id": [params.uuid]
+                }
+            }
         }
-    })
+        res = await fetch(`${endpoint}stadnamn-${process.env.SN_ENV}-*/_search`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `ApiKey ${token}`,
+            },
+            body: JSON.stringify(query)
+        });
+    }
+    else {
+        // Get the document by uuid
+        res = await fetch(`${endpoint}stadnamn-${process.env.SN_ENV}-${params.dataset}/_doc/${params.uuid}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `ApiKey ${token}`,
+            }
+        })
+    }
 
     if (!res.ok) {
         const errorResponse = await res.json();
@@ -17,6 +41,7 @@ export async function fetchDoc(params: any, retry: boolean = true) {
             return fetchDoc(params, retry = false);
         }
         if (errorResponse.error) {
+            console.log(errorResponse.error)
             return {error: errorResponse.error.type.toUpperCase(), status: errorResponse.status};
         }
         if (errorResponse.found == false) {
@@ -24,7 +49,8 @@ export async function fetchDoc(params: any, retry: boolean = true) {
         }
     }
   const data = await res.json()
-  return data
+  
+  return params.dataset == 'search' ? data.hits.hits[0] : data
 
   }
 
