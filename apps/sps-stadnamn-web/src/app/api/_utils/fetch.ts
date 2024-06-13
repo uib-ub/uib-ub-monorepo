f5a5d6e4ff244e77078fba3a0c62a34ea649923e
Henrik Askjer <Henrik.Askjer@uib.no>
2024-06-13 10:24:39 +0200

    formatting

--- apps/sps-stadnamn-web/src/app/api/_utils/fetch.ts
+++ apps/sps-stadnamn-web/src/app/api/_utils/fetch.ts
@@ -1,8 +1,8 @@
 export async function postQuery(dataset: string, query: any, retry: boolean = true) {
     const endpoint = (process.env.SN_ENV == 'prod' ? retry : !retry) ? process.env.ES_ENDPOINT : process.env.ES_ENDPOINT_TEST
     const token = endpoint == process.env.ES_ENDPOINT ? process.env.ES_TOKEN : process.env.ES_TOKEN_TEST
-
-        const res = await fetch(`${endpoint}stadnamn-${process.env.SN_ENV}-${dataset}/_search`, {
+    
+    const res = await fetch(`${endpoint}stadnamn-${process.env.SN_ENV}-${dataset}/_search`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
