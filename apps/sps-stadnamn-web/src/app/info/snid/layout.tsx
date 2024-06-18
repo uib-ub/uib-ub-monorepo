6d128910253275d5bf9b534e5a2efe5ae0672696
Henrik Askjer <Henrik.Askjer@uib.no>
2024-06-18 09:47:56 +0200

    fix import error message in prod

--- apps/sps-stadnamn-web/src/app/info/snid/layout.tsx
+++ apps/sps-stadnamn-web/src/app/info/snid/layout.tsx
@@ -1,12 +1,11 @@
 import Breadcrumbs from '@/components/layout/Breadcrumbs';
-import { metadata as parent  } from "../page.mdx"
 export const metadata = { title: 'Stadnamnsøk' }
 
 export default function Page({ children }: { children: React.ReactNode }) {
   return (
     <>
     
-    <Breadcrumbs parentName={parent.title} parentUrl={`/info`} currentName={metadata.title} />
+    <Breadcrumbs parentName="info" parentUrl="/info" currentName={metadata.title} />
     {children}
 
     </>
