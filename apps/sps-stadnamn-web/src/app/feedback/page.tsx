0032255420438495c41c224aebe622e5dc7da134
Henrik Askjer <Henrik.Askjer@uib.no>
2024-06-21 15:25:06 +0200

    add tabindex to main

--- apps/sps-stadnamn-web/src/app/feedback/page.tsx
+++ apps/sps-stadnamn-web/src/app/feedback/page.tsx
@@ -4,7 +4,7 @@ import Footer from "../../components/layout/Footer"
 export default function Feedback() {
     return (
         <>
-        <main id="main" className="flex flex-col w-full flex-grow">
+        <main id="main" tabIndex={-1} className="flex flex-col w-full flex-grow">
         <iframe className="flex-grow" src="https://skjemaker.app.uib.no/view.php?id=16665712"/>
         </main>
         <Footer/>
