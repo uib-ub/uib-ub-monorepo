69106a67acfde86bb746586791abe7497d4c2c18
Henrik Askjer <Henrik.Askjer@uib.no>
2024-06-21 16:06:36 +0200

    add subpage metadata

--- apps/sps-stadnamn-web/src/app/view/[dataset]/info/[subpage]/page.tsx
+++ apps/sps-stadnamn-web/src/app/view/[dataset]/info/[subpage]/page.tsx
@@ -6,6 +6,22 @@ import path from 'path';
 import { datasetTitles, subpages } from '@/config/metadata-config'
 import Breadcrumbs from '@/components/layout/Breadcrumbs';
 
+export async function generateMetadata( { params }: { params: { dataset: string, subpage: number } }) {
+
+  // Fetch first two  paragraph from article
+  const filePath = path.join(process.cwd(), 'src', 'content', 'datasets', params.dataset, 'article-' + params.subpage + ".mdx");
+  const src = fs.readFileSync(filePath, 'utf8');
+
+  const description = src.split('\r\n\r')?.[0].split("\n")?.slice(1).join("\n")
+
+  return {
+    title: `${datasetTitles[params.dataset]} - ${subpages[params.dataset][params.subpage - 1]}`,
+    description
+  }
+}
+
+
+
 export default function SubPage ( { params }: { params: { dataset: string, subpage: number } }) {
     let [mainIndex, subindex] = params.dataset.split("_")
 
