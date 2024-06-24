d3235288cb1db4e8ae68cad1470fa719e5d75117
Henrik Askjer <Henrik.Askjer@uib.no>
2024-06-24 10:52:20 +0200

    generate json only on permanent landing page

--- apps/sps-stadnamn-web/src/middleware.ts
+++ apps/sps-stadnamn-web/src/middleware.ts
@@ -5,13 +5,9 @@ import { fetchDoc, fetchSNID } from './app/api/_utils/actions'
 export async function middleware(request: NextRequest) {
     // Extract uuid and extension from url
     
-
-
     const url = new URL(request.url)
     const path = url.pathname.split('/')
     
-
-
     if (path[1] == 'snid') {
         // redirect
         const snid = path[2]
@@ -20,70 +16,65 @@ export async function middleware(request: NextRequest) {
         return Response.redirect(`http:localhost:3000/view/search/doc/${data.fields.uuid}${url.searchParams ? '?' + url.searchParams : ''}`, 302);
     }
 
-    
-    const dataset = path[2]
+    if (path[1] == "uuid" && path[2].includes('.')) {
+        const dataset = path[1]
+        const filename = path[2]
+        const [uuid, extension] = filename.split('.')
+        const data = await fetchDoc({ uuid, dataset});
 
-    const suffix = path[4]
-    const [uuid, extension] = suffix.split('.')
-
-
-    const data = await fetchDoc({ uuid, dataset});
-    
-    if (extension == 'geojson') {
-        const geojson = {
-            "type": "Feature",
-            "geometry": data._source.location,
-            "properties": {
-                "id": data._source.uuid,
-                "label": data._source.label,
-                "rawData": data._source.rawData,
-                "adm1": data._source.adm1,
-                "adm2": data._source.adm2,
-                "audio": data._source.audio
-            }
-        }
-        return Response.json(geojson);
-    }
-    if (extension == 'jsonld') {
-        const jsonld = {
-            "@context": {
-                "@vocab": "http://www.cidoc-crm.org/cidoc-crm/",
-                "label": "P1_is_identified_by",
-                "location": "P168_place_is_defined_by",
-                "placeName": "P87_is_identified_by",
-                "document": "P70_documents"
-            },
-            "@id": data._source.uuid,
-            "@type": "E31_Document",
-            "document": {
-                "@id": "_:placeName",
-                "@type": "E44_Place_Appellation",
-                "label": data._source.label,
-                "placeName": {
-                    "@id": "_:place",
-                    "@type": "E53_Place",
+        if (extension == 'geojson') {
+            const geojson = {
+                "type": "Feature",
+                "geometry": data._source.location,
+                "properties": {
+                    "id": data._source.uuid,
                     "label": data._source.label,
-                    "location": {
-                        "@type": "E94_Space_Primitive",
-                        "coordinates": data._source.location.coordinates
+                    "rawData": data._source.rawData,
+                    "adm1": data._source.adm1,
+                    "adm2": data._source.adm2,
+                    "audio": data._source.audio
+                }
+            }
+            return Response.json(geojson);
+        }
+
+        if (extension == 'jsonld') {
+            const jsonld = {
+                "@context": {
+                    "@vocab": "http://www.cidoc-crm.org/cidoc-crm/",
+                    "label": "P1_is_identified_by",
+                    "location": "P168_place_is_defined_by",
+                    "placeName": "P87_is_identified_by",
+                    "document": "P70_documents"
+                },
+                "@id": data._source.uuid,
+                "@type": "E31_Document",
+                "document": {
+                    "@id": "_:placeName",
+                    "@type": "E44_Place_Appellation",
+                    "label": data._source.label,
+                    "placeName": {
+                        "@id": "_:place",
+                        "@type": "E53_Place",
+                        "label": data._source.label,
+                        "location": {
+                            "@type": "E94_Space_Primitive",
+                            "coordinates": data._source.location.coordinates
+                        }
                     }
                 }
             }
+            return Response.json(jsonld);
         }
-        return Response.json(jsonld);
-
+        if (extension == 'json') {
+            return Response.json(data);
+        }
     }
-    else {
-        return Response.json(data);
-    }
-    
 }
 
   export const config = {
     matcher: [
-        '/view/:dataset/doc/:uuid*.json',
-        '/view/:dataset/doc/:uuid*.geojson',
-        '/view/:dataset/doc/:uuid*.jsonld',
+        '/uuid/:uuid*',
         '/snid/:snid*',
     ],
     
