a76eb68522896d6c7e56e21677efd13cd80a43b7
Henrik Askjer <Henrik.Askjer@uib.no>
2024-06-24 11:01:42 +0200

    fix copy link styling

--- apps/sps-stadnamn-web/src/app/view/[dataset]/doc/[uuid]/CopyLink.tsx
+++ apps/sps-stadnamn-web/src/app/view/[dataset]/doc/[uuid]/CopyLink.tsx
@@ -13,9 +13,9 @@ export default function CopyLink({ uuid }: { uuid: string }) {
 
     return (
         <span className="flex gap-3">
-        <Link href={`https://purl.org/stadnamn/uuid/${uuid}`} className="no-underline inline"><PiLink className="inline"/>Varig infoside</Link>
+        <Link href={`https://purl.org/stadnamn/uuid/${uuid}`} className="no-underline inline flex gap-1 items-center"><PiLink aria-hidden="true" className="inline"/>Varig infoside</Link>
         <button onClick={copyLink}>
-        <span>{linkCopied ? <PiCheckBold className="inline"/> : <PiClipboardBold className="inline"/>}Kopier lenke</span>
+        <span className="flex gap-1 items-center">{linkCopied ? <PiCheckBold className="inline" aria-hidden="true"/> : <PiClipboardBold className="inline" aria-hidden="true"/>}Kopier lenke</span>
         
         </button>
         </span>
