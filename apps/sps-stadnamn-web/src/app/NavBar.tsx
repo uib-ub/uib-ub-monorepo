ee5cb2e232947d127a96d141db29881d2fc65c7c
Henrik Askjer <Henrik.Askjer@uib.no>
2024-06-24 10:00:22 +0200

    remove accessibility info from navbar

--- apps/sps-stadnamn-web/src/app/NavBar.tsx
+++ apps/sps-stadnamn-web/src/app/NavBar.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 export default function NavBar({ handleBlur, ...props }: any) {
     return (
         <nav id="top " {...props}>
-            <Link onBlur={handleBlur} className="py-3 mx-3 lg:py-1 lg:px-2 lg:mx-0" href="/a11y">Tilgjengelighet</Link>
             <Link onBlur={handleBlur} className="py-3 mx-3 lg:py-1 lg:px-2 lg:mx-0" href="/help">Søkeveiledning</Link>
             <Link onBlur={handleBlur} className="py-3 mx-3 lg:py-1 lg:px-2 lg:mx-0" href="/datasets">Søkevisninger</Link>
             <Link onBlur={handleBlur} className="py-3 mx-3 lg:py-1 lg:px-2 lg:mx-0" href="/feedback">Tilbakemelding</Link>
