3378482ceaf22ad357eb5f715f8860306921a8cb
Tarje Lavik <tarje.lavik@gmail.com>
2024-06-26 16:36:41 +0200

    Filter empty results

--- apps/api/src/helpers/indexers/resolveIds.ts
+++ apps/api/src/helpers/indexers/resolveIds.ts
@@ -2,7 +2,7 @@ import { getLaItem } from './getLaItem';
 
 export async function resolveIds(ids: any, source: string): Promise<any> {
   try {
-    const promises = ids.map((item: { id: string, identifier: string }) => getLaItem(item.identifier, source))
+    const promises = ids.map((item: { id: string, identifier: string }) => getLaItem(item.identifier, source)).filter(Boolean);
     const data = await Promise.all(promises);
     return data;
   } catch (error) {
