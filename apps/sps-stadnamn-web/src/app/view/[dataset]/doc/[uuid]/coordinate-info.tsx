defdf460a7c3c6d7d6bd98ef2453a361ce377fe7
Henrik Askjer <Henrik.Askjer@uib.no>
2024-07-23 14:49:25 +0200

    add geohack link and format coodinates

--- apps/sps-stadnamn-web/src/app/view/[dataset]/doc/[uuid]/coordinate-info.tsx
+++ apps/sps-stadnamn-web/src/app/view/[dataset]/doc/[uuid]/coordinate-info.tsx
@@ -5,6 +5,21 @@ import { useEffect } from 'react';
 import { PiMapPin, PiCaretDown, PiCaretUp  } from 'react-icons/pi';
 import { useState } from 'react';
 
+function convertDMS(lat: number, lon: number): string {
+    function toDMS(degree: number, direction: string[]): string {
+        const absolute = Math.abs(degree);
+        const degrees = Math.floor(absolute);
+        const minutesNotTruncated = (absolute - degrees) * 60;
+        const minutes = Math.floor(minutesNotTruncated);
+        const seconds = (minutesNotTruncated - minutes) * 60; // Keep the decimal part
+        return `${degrees}° ${minutes}′ ${seconds.toFixed(2)}″${degree >= 0 ? direction[0] : direction[1]}`;
+      }
+  
+    const latitude = toDMS(lat, ['N', 'S']);
+    const longitude = toDMS(lon, ['Ø', 'V']);
+    return `${latitude} ${longitude}`;
+  }
+
 
 
 export default function CoordinateInfo({source}: {source: Record<string, any>}) {
@@ -37,7 +52,7 @@ export default function CoordinateInfo({source}: {source: Record<string, any>}) 
         
             <div className="flex items-center space-x-2">
                 <PiMapPin className="w-6 h-6" />
-                <p className="font-semibold">Lengdegrad: {source.location.coordinates[0]}, Breddegrad: {source.location.coordinates[1]}</p>
+                <a className="font-semibold" href={`https://geohack.toolforge.org/geohack.php?pagename=Geohack&params=${source.location.coordinates[1]};${source.location.coordinates[0]}&language=no`}>{convertDMS(source.location.coordinates[1], source.location.coordinates[0])}</a>
             </div>
             {coordinateData._source &&
             source.coordinateType && 
