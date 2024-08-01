e012804dd13f1ce750af0d69332d00125796c4fe
Jan Ole Bangen <Jan.Bangen@uib.no>
2024-08-01 11:07:56 +0200

    fix(tpa): analytics overvoew with correct import

--- apps/sps-termportal-admin/server/api/analytics/overview.ts
+++ apps/sps-termportal-admin/server/api/analytics/overview.ts
@@ -1,12 +1,8 @@
-import genEsQuery from "~/server/utils/genEsQuery";
-
 export default defineEventHandler(async (event) => {
   const query = {
     index: "termp-a-analytics",
     query: {
-      query: {
-        match_all: {},
-      },
+      match_all: {},
     },
   };
 
