f84a154e6a3746b3a6a29ad61900e194cbe94909
Jan Ole Bangen <Jan.Bangen@uib.no>
2024-08-09 16:22:42 +0200

    feat(tpa): improve dashboard with ongoing and planned activities

--- apps/sps-termportal-admin/components/dashboard/OngoingActivities.vue
+++ apps/sps-termportal-admin/components/dashboard/OngoingActivities.vue
@@ -1,15 +1,15 @@
 <template>
-  <section>
+  <section v-if="data?.length > 0">
     <div class="flex justify-between">
       <h2 class="text-lg font-semibold pb-2 text-gray-800">
-        Forestående aktiviteter
+        Pågående aktiviteter
       </h2>
       <AddButton :to="studioLinks.newActivity" target="_blank" />
     </div>
     <ol class="space-y-0.5 text-lg ml-1">
       <li
         v-for="activity in procdata"
-        :key="activity.label + activity.end"
+        :key="activity.id"
         class="hover:bg-gray-100 p-1"
       >
         <AppLink
@@ -18,41 +18,34 @@
           target="_blank"
         >
           <div class="w-[18rem]">{{ activity.label }}</div>
-          <div>{{ activity.time }}</div>
+          <div>{{ activity.begin }}/...</div>
         </AppLink>
       </li>
     </ol>
   </section>
 </template>
 
-<script setup>
+<script setup lang="ts">
 const query = `
 *[_type == "activity"
-  && dateTime(timespan.beginOfTheBegin) > dateTime(now())]{
+&& dateTime(timespan.beginOfTheBegin) < dateTime(now())
+ && !defined(timespan.endOfTheEnd)
+]{
   _id,
   label,
-  "start": timespan.beginOfTheBegin,
-  "end": timespan.endOfTheEnd,
-} | order(start asc)[0...10]`;
-
-const { data } = useLazySanityQuery(query);
+  "begin": timespan.beginOfTheBegin,
+} | order(begin desc)[0...10]`;
 
 const procdata = computed(() =>
-  data.value?.map((a) => {
+  data.value.map((a) => {
     const tmp = {
       id: a._id,
       label: a.label,
-      get time() {
-        const start = prettyPrintDate(a.start?.substring(0, 10));
-        const end = prettyPrintDate(a.end?.substring(0, 10));
-        if (start === end) {
-          return end;
-        } else {
-          return `${start}–${end}`;
-        }
-      },
+      begin: prettyPrintDate(a.begin?.substring(0, 10)),
     };
     return tmp;
   })
 );
+
+const { data } = useLazySanityQuery(query);
 </script>
