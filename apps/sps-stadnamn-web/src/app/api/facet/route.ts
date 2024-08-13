a19fa877fd69b45c430c41cc6e7b47daddd3ed26
Henrik Askjer <Henrik.Askjer@uib.no>
2024-08-13 17:29:29 +0200

    add line that was left out when reverting

--- apps/sps-stadnamn-web/src/app/api/facet/route.ts
+++ apps/sps-stadnamn-web/src/app/api/facet/route.ts
@@ -43,6 +43,7 @@ export async function GET(request: Request) {
                 size: params.facetSearch ? 10 : 50,
                 ...params.facetSort ? { order: { _key: params.facetSort } } : {},
               },
+              ...(i < facets.length - 1 ? { aggs } : {})
             }
           };
     }
