18a9740cc10d98a8688aa957182d1e579ea3c3f1
Henrik Askjer <Henrik.Askjer@uib.no>
2024-08-26 15:08:18 +0200

    remove duplicate class

--- apps/sps-stadnamn-web/src/components/layout/ContentLayout.tsx
+++ apps/sps-stadnamn-web/src/components/layout/ContentLayout.tsx
@@ -1,7 +1,7 @@
 import Footer from "./Footer"
 export default function ContentLayout({ children }: { children: React.ReactNode }) {
     return <>
-    <main id="main" tabIndex={-1} className="container flex flex-col flex-grow card container page-info mx-auto p-4 sm:p-8 md:p-12 sm:mb-6 md:my-12">
+    <main id="main" tabIndex={-1} className="container flex flex-col flex-grow card page-info mx-auto p-4 sm:p-8 md:p-12 sm:mb-6 md:my-12">
       {children}</main>
     <Footer/>
     </>
