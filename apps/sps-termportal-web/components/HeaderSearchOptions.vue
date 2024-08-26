91ba3a17721c5b10ea9139ca2b67e8b0c86e4174
Jan Ole Bangen <Jan.Bangen@uib.no>
2024-08-26 16:12:36 +0200

    feat(tpw): move domain/tb scope to searchoptions

--- apps/sps-termportal-web/components/HeaderSearchOptions.vue
+++ apps/sps-termportal-web/components/HeaderSearchOptions.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-wrap gap-x-6 gap-y-2 py-1">
+  <div class="flex flex-wrap gap-x-6 gap-y-2 py-1 pl-1">
     <SearchDropdownWrapper target="language">
       <DropdownPV
         v-model="searchInterface.language"
@@ -16,6 +16,16 @@
         class="min-w-[6rem]"
       />
     </SearchDropdownWrapper>
+    <div
+      class="flex space-x-2 pt-1 hover:cursor-pointer group"
+      @click="searchInterface.useDomain = !searchInterface.useDomain"
+    >
+      <div>{{ $t("global.domain.domain") }}</div>
+      <div class="h-4 rotate-180 pb-6">
+        <InputSwitch v-model="searchInterface.useDomain" @click.stop="false" />
+      </div>
+      <div>{{ $t("global.termbase", 0) }}</div>
+    </div>
   </div>
 </template>
 
