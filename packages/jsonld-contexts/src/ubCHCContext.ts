0c462154ad57db83cd543ffdab3f5ad376448e72
Tarje Lavik <tarje.lavik@gmail.com>
2024-09-10 12:28:59 +0200

    Add UB extension context for linked.art

---
+++ packages/jsonld-contexts/src/ubCHCContext.ts
@@ -0,0 +1,11 @@
+const ubChcContext = {
+  "@context": {
+    "@version": 1.1,
+    "odrl": "http://www.w3.org/ns/odrl/2/",
+    "inherit_from": {
+      "@id": "odrl:inheritFrom",
+    },
+  }
+}
+
+export default ubChcContext;
