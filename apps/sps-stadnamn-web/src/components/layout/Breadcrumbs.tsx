2ce3fdf42849efc536a021ed905ab7f0ea1475f8
Henrik Askjer <Henrik.Askjer@uib.no>
2024-09-12 15:17:24 +0200

    create breadcrumb class

--- apps/sps-stadnamn-web/src/components/layout/Breadcrumbs.tsx
+++ apps/sps-stadnamn-web/src/components/layout/Breadcrumbs.tsx
@@ -1,6 +1,6 @@
 
-import Link from 'next/link'
 import { Fragment } from 'react'
+import Link from 'next/link'
 export default function Breadcrumbs({ parentUrl, parentName, currentName}: { parentUrl: string | string[], parentName: string | string[], currentName: string }) {
 
     const parents = Array.isArray(parentName) ? parentName : [parentName]
@@ -10,7 +10,7 @@ export default function Breadcrumbs({ parentUrl, parentName, currentName}: { par
             {parents.map((name, index) => (
                 <Fragment key={name}>
 
-                    <Link key={name} className="no-underline text-accent-700 hover:underline decoration-accent-700" href={Array.isArray(parentUrl) ? parentUrl[index] : parentUrl}>
+                    <Link key={name} className="breadcrumb-link" href={Array.isArray(parentUrl) ? parentUrl[index] : parentUrl}>
                         {name}
                     </Link>
                     <span className="mx-2">/</span>
