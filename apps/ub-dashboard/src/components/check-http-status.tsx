e620406fff284f49f749f6073bb17fa489b818e7
Tarje Lavik <tarje.lavik@gmail.com>
2024-09-13 11:56:09 +0200

    Make sure check-http-status does not run on build

--- apps/ub-dashboard/src/components/check-http-status.tsx
+++ apps/ub-dashboard/src/components/check-http-status.tsx
@@ -1,7 +1,11 @@
 "use client"
-import { getBaseUrl } from '@/lib/utils'
+import { getBaseUrl } from '@/lib/utils';
 
 export const CheckHttpStatus = async ({ url }: { url: string }) => {
+  if (typeof window === 'undefined') {
+    return null; // Return null if not in a browser environment
+  }
+
   const res = await fetch(`${getBaseUrl()}/api/http-status?url=${url}`)
   const status = await res.json()
 
