f5425d141e02fe41b14a7edce21272f30f6f6c18
Tarje Lavik <tarje.lavik@gmail.com>
2024-09-13 11:57:32 +0200

    Add custom xata clint with overridden apiKey

--- apps/ub-dashboard/src/utils/xataClient.ts
+++ apps/ub-dashboard/src/utils/xataClient.ts
@@ -1,9 +1,5 @@
 import { XataClient } from './xata';
 
-export const xataClient = () => {
-  const xata = new XataClient({
-    apiKey: process.env.XATA_API_KEY,
-    branch: process.env.XATA_BRANCH,
-  });
-  return xata;
-};
+export const xata = new XataClient({
+  apiKey: process.env.XATA_API_KEY
+})
