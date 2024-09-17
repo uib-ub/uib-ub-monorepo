f80c4ce9d135522548ca8c25175b3432a2c3bdf3
Henrik Askjer <Henrik.Askjer@uib.no>
2024-09-17 11:29:31 +0200

    temporarily disable sosi vocab

--- apps/sps-stadnamn-web/src/components/ui/place-type.tsx
+++ apps/sps-stadnamn-web/src/components/ui/place-type.tsx
@@ -1,19 +1,16 @@
 
-import { PiLink } from "react-icons/pi";
-import { fetchSOSI } from '@/app/api/_utils/actions'
+//import { fetchSOSI } from '@/app/api/_utils/actions'
 import Link from "next/link";
 
 export default async function PlaceType({ sosiCode }: { sosiCode: string }) {
-    const data = await fetchSOSI(sosiCode)
+    //const data = await fetchSOSI(sosiCode)
 
     return (
-        data.id ?
         <Link className="inline whitespace-nowrap"
-            href={data.id}>
-        {data.label}
+            href={"https://register.geonorge.no/sosi-kodelister/stedsnavn/navneobjekttype/" + sosiCode}>
+        {sosiCode}
         </Link>
-        :
-        sosiCode
+    
     )
 
     
