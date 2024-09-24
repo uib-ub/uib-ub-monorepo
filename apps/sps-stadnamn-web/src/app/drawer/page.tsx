a7d8a7e9c46e5f4283776d5039ab1a3d23233332
Henrik Askjer <Henrik.Askjer@uib.no>
2024-09-24 13:53:24 +0200

    working demo

--- apps/sps-stadnamn-web/src/app/drawer/page.tsx
+++ apps/sps-stadnamn-web/src/app/drawer/page.tsx
@@ -1,5 +1,6 @@
 'use client'
-import { useState } from "react"
+import { useRef, useState } from "react"
+import { PiFunnel, PiInfoFill, PiTable, PiTreeView } from "react-icons/pi";
 
 export default function Drawer() {
     const [currentPosition, setCurrentPosition] = useState(25); // Example initial value
@@ -7,10 +8,17 @@ export default function Drawer() {
     const [snapped, setSnapped] = useState(false);
     const [startTouchY, setStartTouchY] = useState(0);
     const [swipeDirection, setSwipeDirection] = useState<null | 'up' | 'down'>(null);
+    const scrollableContent = useRef<HTMLDivElement>(null);
 
-    const drawerStyle = {
-        height: `${currentPosition}svh` // Set height as a percentage of the viewport height
-    };
+        
+    const isScrolling = (target) => {
+        if (snappedPosition == 100 && scrollableContent.current?.contains(target)) {
+            console.log("YES")
+            return scrollableContent.current.scrollTop != 0
+        }
+        console.log("NO")
+
+    }
 
 
     const pos2svh = (yPos: number) => {
@@ -19,17 +27,17 @@ export default function Drawer() {
     }
 
 
-
     const handleTouchStart = (e) => {
-        e.preventDefault();
+        if (isScrolling(e.target)) {
+            return
+        }
+
         setStartTouchY(e.touches[0].clientY);
         setSnapped(false);
     };
 
     const handleTouchEnd = (e) => {
-        e.preventDefault(); 
         setSnapped(true);
-
 
         let newPosition = Math.round(currentPosition / 25) * 25
         if (newPosition < 25) {
@@ -51,38 +59,65 @@ export default function Drawer() {
     };
 
     const handleTouchMove = (e) => {
-        e.preventDefault();
-
-
+        if (isScrolling(e.target)) {
+            return
+        }
         const newHeight = snappedPosition - pos2svh(startTouchY) + pos2svh(e.touches[0].clientY)
 
         setSwipeDirection(newHeight > currentPosition ? 'up' : 'down');
         setCurrentPosition(newHeight < 100 ? newHeight : 100);
 
-  
     }
         
 
     return <div className="h-full">
-        <div className="bg-accent-200 !h-full"><h1>Hello</h1>
+        <div className="bg-accent-200 !h-full">
+            Kart
 
         
             
             </div>
         
 
-        <div className={`bg-white absolute md:!h-full md:!w-full md:bg-transparent bottom-0 w-full touch-pan-down ${snapped ? 'transition-all duration-300 ease-in-out ' : ''}`}
-             style={drawerStyle} 
+        <div className={`bg-white fixed overscroll-none touch-pan-down overflow-hidden md:!h-full md:!w-full md:bg-transparent bottom-0 w-full  ${snapped ? 'transition-all duration-300 ease-in-out ' : ''}`}
+             style={{height: `${currentPosition}dvh`, touchAction: snappedPosition == 100 ? 'pan-y' : 'pan-down'}}
              onTouchStart={handleTouchStart} 
              onTouchMove={handleTouchMove}
              onTouchEnd={handleTouchEnd}>
 
-            <div className="w-full flex justify-center md:hidden"><div className="h-1 w-16 bg-neutral-300 mt-1 rounded-full"></div>
+            <div className="w-full flex justify-center md:hidden"><div className="h-1 w-16 bg-neutral-300 mt-1 rounded-full"></div></div>
+            <div className="h-full overscroll-contain max-h-[calc(100dvh-3rem)] p-4 instance-info" ref={scrollableContent} style={{overflowY: snappedPosition == 100 ? 'auto' : 'hidden', touchAction: snappedPosition == 100 || scrollableContent.current?.scrollTop && scrollableContent.current.scrollTop > 0 ? 'pan-y' : 'pan-down'}}>
+            <h2>Berg</h2>
+            <p>Gard | Ullensvang, Hordaland</p>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p>Aenam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+            <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+            <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+            <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+            <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+            <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p>Aenam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+            <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+            <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+            <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+            <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
+            <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+
             
             </div>
-            Snapped position: {snappedPosition}<br/>
-            Direction: {swipeDirection}<br/>
-            <br/>
+            <div className="fixed bottom-0 left-0 bg-neutral-800 text-white w-full h-12 flex gap-2 py-1 px-4 items-center justify-between shadow-md">
+                <div className="flex gap-2 font-semibold">Treff<span className="bg-primary-500 rounded-full px-2 text-white whitespace-nowrap">10 000+</span></div>
+                <div className="flex">
+                <div className="px-3"><PiFunnel className="text-3xl"/></div>
+                <div className="px-3"><PiTable className="text-3xl"/></div>
+                <div className="px-3"><PiTreeView className="text-3xl"/></div>
+                <div className="px-3"><PiInfoFill className="text-3xl"/></div>
+                </div>
+              
+
+            </div>
             
         </div>
     </div>
