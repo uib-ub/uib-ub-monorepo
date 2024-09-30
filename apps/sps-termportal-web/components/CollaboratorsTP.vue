b5b577f47bac11d569f747bf1725d4a24517ef17
Jan Ole Bangen <Jan.Bangen@uib.no>
2024-09-30 16:13:25 +0200

    fix(tpw): make contributors label lighter

--- apps/sps-termportal-web/components/CollaboratorsTP.vue
+++ apps/sps-termportal-web/components/CollaboratorsTP.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="m-auto max-w-xl justify-center gap-x-6">
-    <h2 class="pb-2 text-lg font-semibold">{{ $t("global.collaborators") }}</h2>
+    <h2 class="pb-2 text-lg font-semibold text-gray-800">{{ $t("global.collaborators") }}</h2>
     <div
       class="flex flex-col items-center justify-center gap-x-12 gap-y-4 sm:flex-row sm:items-start"
     >
