dae568fcc634d1a4533fef8c2daee95c3adb04cd
Henrik Askjer <Henrik.Askjer@uib.no>
2024-10-03 10:20:51 +0200

    restyle front page

--- apps/sps-stadnamn-web/src/app/page.tsx
+++ apps/sps-stadnamn-web/src/app/page.tsx
@@ -41,13 +41,13 @@ export default async function Home({ searchParams } : { searchParams?: {q: strin
 
   return (
     <>
-<main id="main" tabIndex={-1} className="flex flex-col grow-1 gap-48 items-center justify-center pb-24 lg:pt-32 md:pt-16 sm:pt-8 px-4 w-full flex-grow carta-marina">
-  <div className="flex flex-col gap-24 w-full">
-  <div className="flex flex-col gap-12 w-full">
-  <div className="flex flex-col gap-8 ">
-  <h1 className="text-2xl sm:text-3xl self-center md:text-4xl lg:text-5xl">Stadnamnportalen</h1>
+<main id="main" tabIndex={-1} className="flex flex-col grow-1 gap-48 items-center justify-center pb-24 pt-8 px-4 w-full flex-grow carta-marina">
+  <div className="flex flex-col gap-12 aspect-square px-16 w-fit self-center md:bg-white md:rounded-full md:bg-opacity-75 self align-middle items-center justify-center">
+  <div className="flex flex-col gap-8">
+  <div className="flex flex-col gap-8">
+  <h1 className="text-2xl sm:text-3xl self-center md:text-4xl lg:text-5xl text-neutral-900">Stadnamnportalen</h1>
   
-  <form className="grid grid-cols-5 md:grid-cols-7 items-center justify-center md:max-w-2xl md:mx-auto gap-3 mt-8" action="view/search">
+  <form className="grid grid-cols-5 md:grid-cols-7 items-center justify-center md:max-w-2xl md:mx-auto gap-3 px-12" action="view/search">
     <label htmlFor="search_input" className="sr-only">Søk i alle stedsnavn</label>
     <input id="search_input" className="col-span-4 rounded-sm h-12 border border-gray-400 text-base px-2" name="q" type="text"/>
     <IconButton className="btn btn-primary col-span-1 text-base h-full" type="submit" label="Søk"><PiMagnifyingGlass className="text-xl"/></IconButton>
@@ -55,7 +55,7 @@ export default async function Home({ searchParams } : { searchParams?: {q: strin
   </form>
   
 
-  { stats && <ul className="text-neutral-900 font-serif small-caps flex items-center justify-center flex-col lg:flex-row gap-6 lg:gap-12">
+  { stats && <ul className="text-neutral-900 font-serif small-caps flex items-center justify-center flex-col sm:flex-row gap-6 lg:gap-12">
   <li className="flex flex-col items-center text-lg">
       Stadnamnoppslag
       <span className="text-4xl">{stats?.snidCount?.toLocaleString('nb-NO')}</span>
@@ -76,27 +76,32 @@ export default async function Home({ searchParams } : { searchParams?: {q: strin
 
   </div>
 
-  <div className="flex items-center justify-center flex-col lg:flex-row gap-12">
-  <div className="flex flex-col md:flex-row items-center gap-6 text-neutral-950 "><UiBLogo/><div className="flex flex-col gap-1 text-center md:text-left"><h2 className="tracking-widest font-serif">UNIVERSITETET I BERGEN</h2><em className="font-serif">Universitetsbiblioteket</em></div>
-  </div>
-  <div className="flex flex-col md:flex-row gap-6 jusitfy-between text-center">
-  <div className="flex flex-col"><span className="font-semibold">Språksamlingane</span>
-  <Link href="https://spraksamlingane.no" className="text-sm">spraksamlingane.no</Link></div>
-  <div className="flex flex-col"><span className="font-semibold">Digital utvikling</span>
-  <Link href="https://uib.no/digitalutvikling" className="text-sm">uib.no/digitalutvikling</Link></div>
+  <div className="flex flex-col gap-6 justify-center">
 
+  <div className="flex justify-center flex-col lg:flex-row gap-8">
+  <div className="flex flex-col md:flex-row items-center gap-6 text-neutral-950 text-sm"><UiBLogo/><div className="flex flex-col gap-1 text-center md:text-left"><h2 className="tracking-widest font-serif">UNIVERSITETET I BERGEN</h2><em className="font-serif">Universitetsbiblioteket</em></div>
   </div>
   </div>
+  <div className="flex gap-4 text-sm text-center justify-center">
+  <div className="flex flex-col text-center"><span className="font-semibold">Språksamlingane</span>
+  <Link href="https://spraksamlingane.no" className="text-xs text-center">spraksamlingane.no</Link></div>
+  <div className="flex flex-col text-center"><span className="font-semibold">Digital utvikling</span>
+  <Link href="https://uib.no/digitalutvikling" className="text-xs text-center">uib.no/digitalutvikling</Link></div>
+  </div>
+  </div>
+
+
+  
   
 
 
   </div>
   <div className="flex flex-col items-center container gap-24">
   <section className="flex flex-col items-center gap-12" aria-labelledby="dataset_showcase">
-    <h2 id="dataset_showcase" className="font-serif text-3xl">Sist lagt til:</h2>
+    <h2 id="dataset_showcase" className="font-serif text-3xl">Sist lagt til</h2>
     <ul className="flex flex-col gap-6 xl:grid xl:grid-cols-2">
       {newest.map((card, index) => (
-        <li key={index} className="card p-1 xl:col-span-1 items-start">
+        <li key={index} className="bg-white shadow-lg  p-1 xl:col-span-1 items-start">
           <Link className=" no-underline group flex flex-col md:flex-row xl:flex-row" href={'view/' + card.code + (card.subindices?.length || card.initPage == 'info' ? '/info' : '')}>
           <div className="">
             <div className="overflow-hidden w-full md:h-[18rem] md:w-[18rem] shrink-0 aspect-square">
@@ -119,7 +124,7 @@ export default async function Home({ searchParams } : { searchParams?: {q: strin
     <h2 id="dataset_showcase" className="font-serif text-3xl">Utvalde datasett</h2>
     <ul className="flex flex-col gap-6 xl:grid xl:grid-cols-2">
       {cards.map((card, index) => (
-        <li key={index} className="card p-1 xl:col-span-1 items-start">
+        <li key={index} className="bg-white shadow-lg p-1 xl:col-span-1 items-start">
           <Link className=" no-underline group flex flex-col md:flex-row xl:flex-row" href={'view/' + card.code + (card.subindices?.length || card.initPage == 'info' ? '/info' : '')}>
           <div className="">
             <div className="overflow-hidden w-full md:h-[18rem] md:w-[18rem] shrink-0 aspect-square">
