465d7dd6584d71a9f14125213bf80251022be895
Henrik Askjer <Henrik.Askjer@uib.no>
2024-10-07 15:24:41 +0200

    fix  android scroll issue

--- apps/sps-stadnamn-web/src/app/NavBar.tsx
+++ apps/sps-stadnamn-web/src/app/NavBar.tsx
@@ -3,9 +3,10 @@ import Link from "next/link";
 export default function NavBar({ handleBlur, ...props }: any) {
     return (
         <nav id="top " {...props}>
-            <Link onBlur={handleBlur} className="py-3 mx-3 lg:py-1 lg:px-2 lg:mx-0" href="/help">Søkeveiledning</Link>
-            <Link onBlur={handleBlur} className="py-3 mx-3 lg:py-1 lg:px-2 lg:mx-0 override-external-icon" href="https://skjemaker.app.uib.no/view.php?id=16665712">Tilbakemelding</Link>
-            <Link onBlur={handleBlur} className="py-3 mx-3 lg:py-1 lg:px-2 lg:mx-0" href="/info">Info</Link>
+            <Link onBlur={handleBlur} scroll={false} className="py-3 mx-3 lg:py-1 lg:px-2 lg:mx-0 xl:hidden" href="/">Til forsiden</Link>
+            <Link onBlur={handleBlur} scroll={false} className="py-3 mx-3 lg:py-1 lg:px-2 lg:mx-0" href="/help">Søkeveiledning</Link>
+            <Link onBlur={handleBlur} scroll={false} className="py-3 mx-3 lg:py-1 lg:px-2 lg:mx-0 override-external-icon" href="https://skjemaker.app.uib.no/view.php?id=16665712">Tilbakemelding</Link>
+            <Link onBlur={handleBlur} scroll={false} className="py-3 mx-3 lg:py-1 lg:px-2 lg:mx-0" href="/info">Info</Link>
 
         </nav>
     )
