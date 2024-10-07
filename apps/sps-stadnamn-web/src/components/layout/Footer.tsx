4f1a893105ca0b6f6196d814817107aad06b4abe
Henrik Askjer <Henrik.Askjer@uib.no>
2024-10-07 11:41:16 +0200

    remove watermark

--- apps/sps-stadnamn-web/src/components/layout/Footer.tsx
+++ apps/sps-stadnamn-web/src/components/layout/Footer.tsx
@@ -3,14 +3,13 @@ import Link from 'next/link'
 
 export default function Footer() {
     return (
-        <footer className="bg-neutral-200 p-6 text-center relative">
+        <footer className="bg-neutral-200 p-6 text-center">
         
         <nav className="flex flex-col lg:flex-row gap-6 justify-center items-center">
           <Link href="https://uustatus.no/nn/erklaringer/publisert/c3abf798-49b7-4776-b1ee-f07b46dadd38" className="text-center px-4 py-2 w-full lg:w-auto">Tilgjengelighetserklæring</Link>
           <Link href="/info/privacy" className="text-center px-4 py-2 w-full lg:w-auto">Personvern</Link>
           <Link href="/info/license" className="text-center px-4 py-2 w-full lg:w-auto">Opphavsrett</Link>
         </nav>
-        {process.env.SN_ENV != 'prod' && <div className="absolute bottom-1 right-1">{process.env.SN_ENV} {process.env.VERCEL_GIT_COMMIT_SHA}</div>}
       </footer>
     )
 }
