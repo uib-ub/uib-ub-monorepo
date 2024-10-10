f76461a1f0905dea0f63cd9036372499ff4a5445
Henrik Askjer <Henrik.Askjer@uib.no>
2024-10-10 11:50:20 +0200

    show as checked if the link copied matches the uuid

--- apps/sps-stadnamn-web/src/app/view/[dataset]/doc/[uuid]/CopyLink.tsx
+++ apps/sps-stadnamn-web/src/app/view/[dataset]/doc/[uuid]/CopyLink.tsx
@@ -5,19 +5,19 @@ import Link from 'next/link'
 import { useParams } from 'next/navigation';
 
 export default function CopyLink({ uuid }: { uuid: string }) {
-    const [linkCopied, setLinkCopied] = useState(false);
+    const [linkCopied, setLinkCopied] = useState<string|null>(null);
     const params = useParams()
 
   const copyLink = async () => {
     await navigator.clipboard.writeText(`https://purl.org/stadnamn/uuid/${uuid}`);
-    setLinkCopied(true);
+    setLinkCopied(uuid);
   };
 
     return (
         <span className="flex gap-4 flex-wrap">
-        { params.dataset && <Link href={process.env.NODE_ENV == 'development' ?  `/uuid/${uuid}` : `https://purl.org/stadnamn/uuid/${uuid}`} className="no-underline inline flex gap-1 items-center whitespace-nowrap"><PiInfinityBold aria-hidden="true" className="inline"/>Varig side</Link>}
+        { params.dataset && <Link href={process.env.NODE_ENV == 'development' ?  `/uuid/${uuid}` : `https://purl.org/stadnamn/uuid/${uuid}`} className="no-underline inline-flex gap-1 items-center whitespace-nowrap"><PiInfinityBold aria-hidden="true" className="inline"/>Varig side</Link>}
         <button onClick={copyLink}>
-        <span className="flex gap-1 items-center whitespace-nowrap">{linkCopied ? <PiCheckBold className="inline" aria-hidden="true"/> : <PiClipboardBold className="inline" aria-hidden="true"/>}Kopier varig lenke</span>
+        <span className="flex gap-1 items-center whitespace-nowrap">{linkCopied == uuid ? <PiCheckBold className="inline" aria-hidden="true"/> : <PiClipboardBold className="inline" aria-hidden="true"/>}Kopier varig lenke</span>
         
         </button>
         </span>
