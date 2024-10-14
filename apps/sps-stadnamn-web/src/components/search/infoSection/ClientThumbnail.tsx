f8a8faaadd11eb2bfca4f3f20db9174c8074c9a6
Henrik Askjer <Henrik.Askjer@uib.no>
2024-10-14 10:05:23 +0200

    calculate thumbnail width

--- apps/sps-stadnamn-web/src/components/search/infoSection/ClientThumbnail.tsx
+++ apps/sps-stadnamn-web/src/components/search/infoSection/ClientThumbnail.tsx
@@ -4,22 +4,29 @@ import { useEffect, useState } from "react"
 
 export default function ClientThumbnail({ manifestId }: { manifestId: string }) {
     const [thumbnailUrl, setThumbnaiUrl] = useState<string | null>(null)
+    const [width, setWidth] = useState<number | null>(null)
+    const height = 120
 
     useEffect(() => {
 
         fetch(`https://iiif.test.ubbe.no/iiif/manifest/${manifestId}.json`).then(res => res.json()).then(data => {
+            const [, originalWidth, originalHeight] = data?.thumbnail?.[0]?.id.match(/\/full\/(\d+),(\d+)\//) || [];
+            setWidth(originalWidth ? Math.round((parseInt(originalWidth, 10) * height) / parseInt(originalHeight, 10)) : null);
             setThumbnaiUrl(data?.thumbnail?.[0]?.id)
+            
         }).catch(() => {
             fetch(`https://iiif.test.ubbe.no/iiif/manifest/stadnamn/NBAS/${manifestId}.json`).then(res => res.json()).then(data => {
+                const [, originalWidth, originalHeight] = data?.thumbnail?.[0]?.id.match(/\/full\/(\d+),(\d+)\//) || [];
+                setWidth(originalWidth ? Math.round((parseInt(originalWidth, 10) * height) / parseInt(originalHeight, 10)) : null);
                 setThumbnaiUrl(data?.thumbnail?.[0]?.id)
+                
+                
             })
         })
 
-    }, [manifestId])
+    }, [manifestId, width])
 
 
-
-
-    return thumbnailUrl && <Image width={200} height={200} src={thumbnailUrl} alt="Seddel" />
+    return thumbnailUrl && width && height && <Image width={width} height={height} src={thumbnailUrl} alt="Seddel" />
 
     }
