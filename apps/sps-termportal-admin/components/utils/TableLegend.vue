1994f81a028e78b30e068a7b6ee518f8220bb2ea
Jan Ole Bangen <Jan.Bangen@uib.no>
2024-11-01 14:49:20 +0100

    feat(tpa): add table legend utils

---
+++ apps/sps-termportal-admin/components/utils/TableLegend.vue
@@ -0,0 +1,3 @@
+<template>
+  <dl class="space-y-1 my-2"><slot> </slot></dl>
+</template>
---
+++ apps/sps-termportal-admin/components/utils/TableLegendEntry.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex space-x-2">
+    <dt class="font-semibold w-48">{{ legendKey }}</dt>
+    <dd>{{ legendValue }}</dd>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  legendKey: { type: String, required: true },
+  legendValue: { type: String, required: true },
+});
+</script>
