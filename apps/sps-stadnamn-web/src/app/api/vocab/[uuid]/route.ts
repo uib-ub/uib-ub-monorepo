377760a068609c9aa4c99b83d60e479ae5474a93
Henrik Askjer <Henrik.Askjer@uib.no>
2024-11-06 15:20:25 +0100

    cleanup

--- apps/sps-stadnamn-web/src/app/api/vocab/[uuid]/route.ts
+++ apps/sps-stadnamn-web/src/app/api/vocab/[uuid]/route.ts
@@ -2,12 +2,9 @@ export const runtime = 'edge'
 import { fetchDoc } from '../../_utils/actions';
 
 export async function GET(request: Request) {
-  console.log("REQUEST", request)
     const url = new URL(request.url);
     const uuid = url.pathname.split('/')[3];
-    console.log("UUID", uuid)
-    console.log("PATHNAME", url.pathname)
-    //const data = await fetchDoc({ uuid, dataset: 'vocab' });
+    const data = await fetchDoc({ uuid, dataset: 'vocab' });
 
-  return Response.json({hello: "world"});
+  return Response.json(data);
 }
