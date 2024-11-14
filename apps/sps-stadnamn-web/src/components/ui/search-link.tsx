26fc11905e8dc4b742e6c3ebdc55bbe404aa1c1d
Henrik Askjer <Henrik.Askjer@uib.no>
2024-11-14 09:58:50 +0100

    add search link

---
+++ apps/sps-stadnamn-web/src/components/ui/search-link.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { createSerializer, parseAsString } from "nuqs";
+
+
+export default function SearchLink({children, dataset, params, ...props}: {dataset: string, params: Record<string, string>, children: React.ReactNode, [key: string]: any}) {
+    const serialize = createSerializer({
+        dataset: parseAsString,
+        ...Object.fromEntries(Object.keys(params).map(key => [key, parseAsString]))
+    })
+
+    
+    return (
+        <Link 
+            href={serialize({
+                ...(dataset !== 'search' && { dataset }),
+                ...params
+            })} 
+            {...props}
+        >
+            {children}
+        </Link>
+    )
+}
