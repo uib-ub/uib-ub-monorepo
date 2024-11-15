d316d41d524a9dce05faaec994e4b957824b33c1
Jan Ole Bangen <Jan.Bangen@uib.no>
2024-11-15 15:42:26 +0100

    feat(tpa): add option to provide width to legend entry

--- apps/sps-termportal-admin/components/utils/TableLegendEntry.vue
+++ apps/sps-termportal-admin/components/utils/TableLegendEntry.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="flex space-x-2">
-    <dt class="font-semibold w-48">{{ legendKey }}</dt>
+    <dt class="font-semibold" :class="`w-${legendWidth}`">
+      {{ legendKey }}
+    </dt>
     <dd>{{ legendValue }}</dd>
   </div>
 </template>
@@ -9,5 +11,6 @@
 const props = defineProps({
   legendKey: { type: String, required: true },
   legendValue: { type: String, required: true },
+  legendWidth: { type: String, default: "48" },
 });
 </script>
