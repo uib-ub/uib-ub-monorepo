647f9d6493daa83f934130b552495ecd2c0f1147
Henrik Askjer <Henrik.Askjer@uib.no>
2024-11-26 13:40:29 +0100

    delete old search view and keep files locally

--- apps/sps-stadnamn-web/src/app/view/[dataset]/info/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function InfoLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <div className="dataset-info p-8 flex flex-col gap-2 h-full !overflow-y-auto">
-        {children}
-        </div>
-    )
-}
