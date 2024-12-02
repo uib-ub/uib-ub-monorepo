5d7181791e8006cf4a1fd8a43f51bee46f203e69
Henrik Askjer <Henrik.Askjer@uib.no>
2024-12-02 09:58:33 +0100

    remove redundant component

--- apps/sps-stadnamn-web/src/app/search/page.tsx
+++ apps/sps-stadnamn-web/src/app/search/page.tsx
@@ -1,6 +1,22 @@
-import UserAgentLayout from "@/components/search/user-agent-layout";
-export default async function Page() {
-    return (
-        <UserAgentLayout/>
-    );
+import SearchProvider from "@/app/map-search-provider";
+import { headers } from "next/headers";
+import { userAgent } from "next/server";
+import MobileLayout from "@/components/search/mobile-layout";
+import DesktopLayout from "@/components/search/desktop-layout";
+import DocProvider from "@/app/doc-provider";
+
+export default async function UserAgentLayout() {
+
+  const headersList = await headers()
+  const device = userAgent({headers: headersList}).device
+  const isMobile = device.type === 'mobile'
+
+
+
+  return <SearchProvider>
+          <DocProvider>
+            {isMobile  ? <MobileLayout/> : <DesktopLayout/>}
+          </DocProvider>
+        </SearchProvider>
 }
+
