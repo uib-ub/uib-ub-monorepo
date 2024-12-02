5a80e7512821c87cbae229c17b3277f4ce10f2d7
Henrik Askjer <Henrik.Askjer@uib.no>
2024-12-02 11:18:19 +0100

    tweak dataset cards

--- apps/sps-stadnamn-web/src/app/page.tsx
+++ apps/sps-stadnamn-web/src/app/page.tsx
@@ -31,14 +31,14 @@ export default async function Home({ searchParams } : { searchParams?: Promise<{
 
   const cards = [ 'bsn', 'hord', 'rygh', 'leks'].map(code => {
     const info = datasetPresentation[code]
-    return { img: info.img, alt: info.alt, imageAttribution: info.imageAttribution, title: datasetTitles[code], code: code, description: isMobile ? datasetShortDescriptions[code] : datasetDescriptions[code], subindices: info.subindices, initPage: info.initPage }
+    return { img: info.img, alt: info.alt, imageAttribution: info.imageAttribution, title: datasetTitles[code], code: code, description: datasetShortDescriptions[code], subindices: info.subindices, initPage: info.initPage }
   }
   )
 
 
   const newest = Object.entries(publishDates).sort((a, b) => b[1].localeCompare(a[1])).slice(0, 2).map(entry => entry[0]).map(code => {
     const info = datasetPresentation[code]
-    return { img: info.img, alt: info.alt, imageAttribution: info.imageAttribution, title: datasetTitles[code], code: code, description: isMobile? datasetShortDescriptions[code] : datasetDescriptions[code], subindices: info.subindices, initPage: info.initPage }
+    return { img: info.img, alt: info.alt, imageAttribution: info.imageAttribution, title: datasetTitles[code], code: code, description: datasetShortDescriptions[code], subindices: info.subindices, initPage: info.initPage }
   }
   )
 
@@ -110,57 +110,44 @@ export default async function Home({ searchParams } : { searchParams?: Promise<{
   </div>
   </div>
   <div className="flex flex-col items-center container gap-24">
-
- 
-  
-
-  <section className="flex flex-col items-center gap-12 mt-24" aria-labelledby="dataset_showcase">
-    <h2 id="dataset_showcase" className="font-serif text-3xl">Sist lagt til</h2>
+  <section className="flex flex-col items-center gap-12" aria-labelledby="recently_added">
+    <h2 id="recently_added" className="font-serif text-3xl">Sist lagt til</h2>
     <ul className="flex flex-col gap-6 xl:grid xl:grid-cols-2">
       {newest.map((card, index) => (
-        <li key={index} className="bg-white shadow-lg  p-1 xl:col-span-1 items-start">
-          <Link className=" no-underline group flex flex-col md:flex-row xl:flex-row" href={'view/' + card.code + (card.subindices?.length || card.initPage == 'info' ? '/info' : '')}>
-          <div>
-            <div className="overflow-hidden w-full md:h-[18rem] md:w-[18rem] shrink-0 aspect-square">
-            <Image src={card.img} alt="Illustrasjon" aria-describedby={card.code + "_attribution"} height="512" width="512" className="sepia-[25%] grayscale-[50%] object-cover !h-full !w-full"/>
+        <li key={index} className="bg-white shadow-lg p-4 xl:col-span-1 items-start rounded-lg">
+          <Link className="no-underline group flex flex-col md:flex-row xl:flex-row" href={'view/' + card.code + (card.subindices?.length || card.initPage == 'info' ? '/info' : '')}>
+            <div className="overflow-hidden w-full md:h-[18rem] md:w-[18rem] shrink-0 aspect-square rounded-t-lg md:rounded-l-lg md:rounded-t-none">
+              <Image src={card.img} alt="Illustrasjon" aria-describedby={card.code + "_attribution"} height="512" width="512" className="sepia-[25%] grayscale-[50%] object-cover !h-full !w-full"/>
             </div>
-            <div id={card.code + "_attribution"} className="text-xs text-neutral-700 w-full mt-1">{card.imageAttribution}</div>
-          </div>
-
-            <div className=" py-4 px-6">
+            <div className="py-4 px-6 flex flex-col">
               <h3 className="text-2xl group-hover:underline decoration-1 decoration-primary-600 underline-offset-4">{card.title}</h3>
               <p className="pt-2 text-small">{card.description}</p>
-              
+              <div id={card.code + "_attribution"} className="text-xs text-neutral-700 mt-auto">{card.imageAttribution}</div>
             </div>
           </Link>
         </li>
       ))}
     </ul>
-  </section>
+</section>
   <section className="flex flex-col items-center gap-12" aria-labelledby="dataset_showcase">
     <h2 id="dataset_showcase" className="font-serif text-3xl">Utvalde datasett</h2>
     <ul className="flex flex-col gap-6 xl:grid xl:grid-cols-2">
       {cards.map((card, index) => (
-        <li key={index} className="bg-white shadow-lg p-1 xl:col-span-1 items-start">
-          <Link className=" no-underline group flex flex-col md:flex-row xl:flex-row" href={'view/' + card.code + (card.subindices?.length || card.initPage == 'info' ? '/info' : '')}>
-          <div className="">
-            <div className="overflow-hidden w-full md:h-[18rem] md:w-[18rem] shrink-0 aspect-square">
-            <Image src={card.img} alt="Illustrasjon" aria-describedby={card.code + "_attribution"} height="512" width="512" className="sepia-[25%] grayscale-[50%] object-cover !h-full !w-full"/>
+        <li key={index} className="bg-white shadow-lg p-4 xl:col-span-1 items-start rounded-lg">
+          <Link className="no-underline group flex flex-col md:flex-row xl:flex-row" href={'view/' + card.code + (card.subindices?.length || card.initPage == 'info' ? '/info' : '')}>
+            <div className="overflow-hidden w-full md:h-[18rem] md:w-[18rem] shrink-0 aspect-square rounded-t-lg md:rounded-l-lg md:rounded-t-none">
+              <Image src={card.img} alt="Illustrasjon" aria-describedby={card.code + "_attribution"} height="512" width="512" className="sepia-[25%] grayscale-[50%] object-cover !h-full !w-full"/>
             </div>
-            <div id={card.code + "_attribution"} className="text-xs text-neutral-700 w-full mt-1">{card.imageAttribution}</div>
-          </div>
-
-            <div className=" py-4 px-6">
+            <div className="py-4 px-6 flex flex-col">
               <h3 className="text-2xl group-hover:underline decoration-1 decoration-primary-600 underline-offset-4">{card.title}</h3>
               <p className="pt-2 text-small">{card.description}</p>
-              
+              <div id={card.code + "_attribution"} className="text-xs text-neutral-700 mt-auto">{card.imageAttribution}</div>
             </div>
           </Link>
         </li>
       ))}
     </ul>
-
-    </section>
+</section>
     </div>
   
 
