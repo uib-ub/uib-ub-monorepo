a98ae6d48b49f3373a8c5788e4943d865dccf039
Tarje Lavik <tarje.lavik@gmail.com>
2024-12-09 10:30:06 +0100

    Update Next.js and dependencies; remove unused remark plugin
    - Updated Next.js dependency to "^13.5.7" in package.json.
    - Added "@theguild/remark-mermaid" dependency.
    - Removed unused remark plugin configuration from next.config.mjs.

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,12 +1,8 @@
-import { remarkMermaid } from 'remark-mermaid-nextra';
 import nextra from 'nextra';
 
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-  mdxOptions: {
-    remarkPlugins: [remarkMermaid],
-  }
 })
 
 export default withNextra({
