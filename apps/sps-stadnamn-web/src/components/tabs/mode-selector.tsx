e73b9c8d5ba0302c7b2ab7dc43637826e126fbe3
Henrik Askjer <Henrik.Askjer@uib.no>
2024-12-10 12:58:25 +0100

    switch out of result tab when navigation to table view

--- apps/sps-stadnamn-web/src/components/tabs/mode-selector.tsx
+++ apps/sps-stadnamn-web/src/components/tabs/mode-selector.tsx
@@ -8,7 +8,7 @@ import SearchLink from "../ui/search-link";
 
 export default function ModeSelector() {
     const mode = useQueryState('mode', { defaultValue: 'map' })[0]
-    const pathname = usePathname()
+    const searchParams = useSearchParams()
 
     return <nav className="bg-white rounded-md shadow-md flex">
               <SearchLink aria-current={mode == 'map' ? 'page' : false}
@@ -19,7 +19,7 @@ export default function ModeSelector() {
                 </SearchLink>
 
 
-            <SearchLink add={{mode: 'table'}} 
+            <SearchLink add={{mode: 'table', nav: searchParams.get('nav') == 'results' ? 'filters' : searchParams.get('nav')}} 
                         aria-current={mode == 'table' ? 'page' : false}
                         className="flex  m-1 whitespace-nowrap rounded items-center basis-1 gap-1 no-underline w-full lg:w-auto p-1 px-2 aria-[current=page]:bg-neutral-100 aria-[current=page]:text-neutral-950 aria-[current=page]:shadow-inner">
                             {mode == 'table' ? <PiTableFill aria-hidden="true"/>  : <PiTable aria-hidden="true"/>}Tabell
