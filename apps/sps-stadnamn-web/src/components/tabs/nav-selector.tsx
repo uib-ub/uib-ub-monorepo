c67a8857d8332cb0fad4cfe06542456fcd9c0e61
Henrik Askjer <Henrik.Askjer@uib.no>
2024-12-12 10:48:34 +0100

    responsivity: adapt label visibility to number of links

--- apps/sps-stadnamn-web/src/components/tabs/nav-selector.tsx
+++ apps/sps-stadnamn-web/src/components/tabs/nav-selector.tsx
@@ -16,7 +16,7 @@ export default function NavSelector({leftSection}: {leftSection: string | null})
               <SearchLink aria-current={leftSection == 'datasets' ? 'page' : false}
                       add={{nav: 'datasets'}}
                       className="flex  m-1 whitespace-nowrap rounded-md items-center basis-1 gap-1 no-underline w-full p-2 px-4 lg:w-auto lg:p-1 lg:px-2 aria-[current=page]:bg-neutral-100 aria-[current=page]:text-neutral-950 aria-[current=page]:shadow-inner">
-                        {leftSection == 'datasets' ? <PiDatabaseFill aria-hidden="true"/>  : <PiDatabase aria-hidden="true"/>}<span className="sr-only">Datasett</span>
+                        {leftSection == 'datasets' ? <PiDatabaseFill aria-hidden="true"/>  : <PiDatabase aria-hidden="true"/>}<span className={treeSettings[dataset] ? "sr-only" : "sr-only xl:not-sr-only"}>Datasett</span>
                 </SearchLink>
                 
                 {  treeSettings[dataset] && <SearchLink aria-current={leftSection == 'tree' ? 'page' : false}
@@ -28,14 +28,14 @@ export default function NavSelector({leftSection}: {leftSection: string | null})
                 
                 <SearchLink aria-current={leftSection == 'filters' ? 'page' : false}
                       add={{nav: 'filters'}}
-                      className="flex  m-1 ml-auto whitespace-nowrap rounded-md items-center basis-1 gap-1 no-underline w-full p-2 px-4 lg:w-auto lg:p-1 lg:px-2 aria-[current=page]:bg-neutral-100 aria-[current=page]:text-neutral-950 aria-[current=page]:shadow-inner">
-                       {leftSection == 'filters' ? <PiFunnelFill aria-hidden="true"/>  : <PiFunnel aria-hidden="true"/>}Filtre
+                      className={`flex  m-1 ${treeSettings[dataset] ? "ml-auto" : ""} whitespace-nowrap rounded-md items-center basis-1 gap-1 no-underline w-full p-2 px-4 lg:w-auto lg:p-1 lg:px-2 aria-[current=page]:bg-neutral-100 aria-[current=page]:text-neutral-950 aria-[current=page]:shadow-inner`}>
+                       {leftSection == 'filters' ? <PiFunnelFill aria-hidden="true"/>  : <PiFunnel aria-hidden="true"/>}<span className={treeSettings[dataset] ? "sr-only" : "sr-only xl:not-sr-only"}>Filtre</span>
                 </SearchLink>
 
                 
                 {!isLoading && searchFilterParamsString && leftSection != 'tree' && mode != 'table' && <SearchLink aria-current={leftSection == 'results' ? 'page' : false}
                       add={{nav: 'results'}}
-                      className="flex m-1 whitespace-nowrap rounded-md items-center basis-1 gap-1 no-underline w-full p-2 px-4 lg:w-auto lg:p-1 lg:px-2 aria-[current=page]:bg-neutral-100 aria-[current=page]:text-neutral-950 aria-[current=page]:shadow-inner">
+                      className={`flex m-1 whitespace-nowrap rounded-md items-center basis-1 gap-1 no-underline w-full p-2 px-4 lg:w-auto lg:p-1 lg:px-2 aria-[current=page]:bg-neutral-100 aria-[current=page]:text-neutral-950 aria-[current=page]:shadow-inner ${treeSettings[dataset] ? "" : "ml-auto"}`}>
                         <PiListBullets aria-hidden="true"/>Treff
                         {leftSection == 'results' ? <span className="results-badge bg-accent-700 text-white shadow-sm left-8 rounded-full px-1 text-xs whitespace-nowrap">{totalHits?.relation == 'gte' ? '10 000+' : totalHits?.value || '0'}</span>
                         : <span className="results-badge bg-primary-600 text-white shadow-sm left-8 rounded-full px-1 text-xs whitespace-nowrap">{totalHits?.relation == 'gte' ? '10 000+' : totalHits?.value || '0'}</span>}
