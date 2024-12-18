a7ed86e3a0359e6a6653bcdcf115dcd01882d55b
Henrik Askjer <Henrik.Askjer@uib.no>
2024-12-18 16:24:05 +0100

    always show dataset selector on mobile devices

--- apps/sps-stadnamn-web/src/components/search/datasets/dataset-drawer.tsx
+++ apps/sps-stadnamn-web/src/components/search/datasets/dataset-drawer.tsx
@@ -1,33 +1,42 @@
 
 'use client'
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import DatasetInfo from '../info/dataset-info';
 import DatasetSelector from './dataset-selector';
 import { useDataset } from '@/lib/search-params';
 import { PiCaretDown, PiCaretUp } from 'react-icons/pi';
+import { GlobalContext } from '@/app/global-provider';
 
 
 export default function DatasetDrawer() {
   const [datasetSelectorOpen, setDatasetSelectorOpen] = useState(false);
   const dataset = useDataset()
+  const { isMobile } = useContext(GlobalContext)
 
   useEffect(() => {
-    setDatasetSelectorOpen(false)
+      setDatasetSelectorOpen(false)
   }
   , [dataset])
+
 
   return (    
         <section className="flex gap-2 flex-col mobile-padding">
           <DatasetInfo/>
-          <h3 className="text-lg small-caps px-2 mt-4" >
+          { isMobile ?
+          <h3 className="text-xl small-caps px-2 mt-4" >
+            Velg datasett
+          </h3>
+          :
+          <h3 className="text-lg px-2 mt-4" >
               <button aria-controls="dataset_selector" 
                       className="flex gap-2 items-center"
                       onClick={() => setDatasetSelectorOpen(!datasetSelectorOpen)}
                       aria-expanded={datasetSelectorOpen}>
                         {datasetSelectorOpen ? <PiCaretUp aria-hidden="true" className="text-primary-600"/> : <PiCaretDown aria-hidden="true" className="text-primary-600"/>}
                         Velg datasett</button></h3>
+          }
           <div id="dataset_selector">
-          {datasetSelectorOpen && <DatasetSelector/>   }
+          {(datasetSelectorOpen || isMobile) && <DatasetSelector/>   }
           </div> 
       </section>
   );
