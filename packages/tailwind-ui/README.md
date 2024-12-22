1010e8cbbf753647e087ab0f7607e1b904d18c4e
Tarje Lavik <tarje.lavik@gmail.com>
2024-12-22 12:49:55 +0100

    Remove unused tailwind-ui and react-ui. Use assets or ui in the app

--- packages/tailwind-ui/README.md
+++
@@ -1,3 +0,0 @@
-# Tailwind UI
-
-**DEPRECATED**: This package is no longer maintained. Please use the *Design system* package instead.
--- packages/tailwind-ui/src/components/Spacer.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const Spacer = () => {
-  return (
-    <div className='grow md:block' aria-hidden>&nbsp;</div>
-  )
-}
