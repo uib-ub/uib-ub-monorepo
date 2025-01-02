98f032b7dfb7386ecf57d9ea23b2a60ffd49dc0a
Henrik Askjer <Henrik.Askjer@uib.no>
2025-01-02 10:35:00 +0100

    children view button in lower right corner

--- apps/sps-stadnamn-web/src/components/search/desktop-layout.tsx
+++ apps/sps-stadnamn-web/src/components/search/desktop-layout.tsx
@@ -3,7 +3,6 @@ import SearchResults from "./results/search-results"
 import TreeResults from "./results/tree-results"
 import MapExplorer from "./map-explorer"
 import { useQueryState } from "nuqs"
-import InfoContent from "./info/info-content"
 import { useDataset, useSearchQuery } from "@/lib/search-params"
 import StatusSection from "./status-section"
 import Facets from "./facets/facet-section"
@@ -12,7 +11,7 @@ import { treeSettings } from "@/config/server-config"
 import DatasetDrawer from "./datasets/dataset-drawer"
 import TableExplorer from "./table/table-explorer"
 import NavSelector from "../tabs/nav-selector"
-import { PiXBold } from "react-icons/pi"
+import { PiArchive, PiArrowUpLeft, PiCaretDownBold, PiCaretUpBold, PiFiles, PiFileSqlThin, PiFilesThin, PiFolder, PiTable, PiTag, PiXBold } from "react-icons/pi"
 import SourceList from "./results/source-list"
 import { useContext } from "react"
 import { DocContext } from "@/app/doc-provider"
@@ -20,6 +19,8 @@ import Spinner from "../svg/Spinner"
 import { useSearchParams } from "next/navigation"
 import { ChildrenContext } from "@/app/children-provider"
 import DocSkeleton from "./info/doc-skeleton"
+import DocInfo from "./info/doc-info"
+import ParamLink from "../ui/param-link"
 
 export default function DesktopLayout() {
     const searchParams = useSearchParams()
@@ -29,10 +30,10 @@ export default function DesktopLayout() {
     const [doc, setDoc] = useQueryState('doc')
     const [mode, setMode] = useQueryState('mode', {history: 'push', defaultValue: 'map'})
     const dataset = useDataset()
-    const { parentLoading, parentData, docLoading } = useContext(DocContext)
-    const { childrenLoading } = useContext(ChildrenContext)
-    const parent = searchParams.get('parent')
+    const { parentLoading, parentData, docLoading, docData } = useContext(DocContext)
+    const { childrenLoading, childrenData } = useContext(ChildrenContext)
 
+    const [parent, setParent] = useQueryState('parent')
 
 
     // Keep filters or expanded open when switching to a different section
@@ -54,7 +55,7 @@ export default function DesktopLayout() {
         <div className={`lg:absolute left-2 top-2 flex-col gap-2 max-h-[calc(100svh-6rem)] max-w-[40svw] lg:w-[calc(25svw-1rem)] !z-[3001] bg-white shadow-md rounded-md`}>
 
         <NavSelector leftSection={nav}/>
-        <div className="overflow-y-auto stable-scrollbar ml-2 max-h-[calc(100svh-10rem)] py-3">
+        <div className="overflow-y-auto stable-scrollbar pl-2 max-h-[calc(100svh-10rem)] py-3 border-t border-neutral-200">
 
         { nav == 'tree' && 
             <TreeResults isMobile={false}/>
@@ -84,32 +85,56 @@ export default function DesktopLayout() {
        { mode != 'table' && (doc || parent) &&
         <div className="lg:absolute right-0 top-0 pb-6 flex flex-col items-end p-2 justify-between gap-2 h-full">
         <div className={`flex flex-col  w-[30svw] 2xl:w-[25svw] !z-[3001] ${parent ? 'lg:max-h-[50svh] lg:min-h-[25svh]' :  'lg:max-h-[calc(100svh - 2rem)] lg:min-h-[25svh]'}`}>
-        {doc && !docLoading && <div className={`bg-white relative lg:rounded-md lg:shadow-md break-words pr-4 pl-4 py-2 overflow-y-auto stable-scrollbar`}>
-            <InfoContent/>
+        {doc && !docLoading && docData?._source && <div className={`bg-white relative lg:rounded-md lg:shadow-md break-words pr-4 pl-4 py-2 overflow-y-auto stable-scrollbar`}>
+            <DocInfo/>
         </div>}
         { docLoading && <div className="bg-white relative lg:rounded-md lg:shadow-md break-words p-4 overflow-y-auto stable-scrollbar"><DocSkeleton/></div> }
         
         </div>
-        {parent ? 
-            (parentLoading || childrenLoading) ? 
+        { docData?._source?.children?.length > 1 || parentData?._source?.children?.length > 1 || (treeSettings[dataset] && docData?._source?.sosi == 'gard') || parent ?
+         <div className="rounded-md shadow-md !z-[3001] bg-white  flex flex-col instance-info justify-start">
+            { !treeSettings[dataset] &&<div className="flex">
+            <h2 className=""><ParamLink className="flex gap-2 no-underline p-2 px-4 justify-start items-center" add={{parent: docData?._source.uuid}}>
 
-            <div className="rounded-md shadow-md !z-[3001] bg-white w-12 h-12 flex justify-center items-center"><Spinner status={treeSettings[dataset] ? 'Laster garder' : 'Laster kilder'} className="w-full h-full m-2 self-center" /></div>
+            
 
-        
-            :
-        <div className={`flex-col gap-2 max-w-[40svw] min-w-[20svw] !z-[3001]`}>
-                <div className="rounded-md shadow-md bg-white max-h-[40svh] overflow-auto">
+             <><PiFiles className="text-neutral-800 text-2xl"/><span className="text-xl">Kilder</span><div className="!h-6 self-center text-base font-bold bg-neutral-200 text-neutral-950 rounded-full px-2 !font-sans">{childrenData?.length}</div></>
+            
 
-                   { treeSettings[dataset] ?  
-                      parentData?._id && <CadastralSubdivisions isMobile={false}/>
-                   :  dataset == 'search' && <div className="p-2"><SourceList/></div>}
-                </div>
+            
+            
+            </ParamLink>
+            </h2>
+            
+
+            {parent && <ParamLink className="flex gap-2 no-underline px-2 justify-start items-center" add={{doc: parent}}><PiTag className="text-neutral-800 text-2xl"/> {parentData?._source.label}</ParamLink>}
+            {parent && <button onClick={() => setParent(null)} className="text-neutral-800 text-2xl p-2 ml-auto"><PiXBold/></button>}
+            
             </div>
+            }
+            { treeSettings[dataset] && docData?._source?.sosi == 'gard' && (!parent || childrenLoading) &&
+            <div className="flex">
+                <h2 className="flex gap-2 no-underline justify-start items-center">
+                    <ParamLink className="flex gap-2 no-underline p-2 px-4 justify-start items-center" add={{parent: docData?._source.uuid}}>
+                    <PiTable className="text-neutral-800 text-2xl"/><span className="text-xl">Matrikkeltabell</span>
+                    </ParamLink>
+                    {childrenLoading && <Spinner status="Laster matrikkeltabell"/>}
+                </h2>
+                </div>
+            
+            }
 
-            : null
-        }
 
-        
+            {parent && <div className="max-h-[40svh] min-w-[25svw] overflow-y-auto stable-scrollbar border-t border-neutral-200">
+            
+            {treeSettings[dataset] ?  
+                      parentData?._id && <CadastralSubdivisions isMobile={false}/>
+                   :  dataset == 'search' && <SourceList/>}
+            </div>}
+        </div>
+        : null
+
+        }       
 
         </div>
         }
