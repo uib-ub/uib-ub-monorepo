6cc4457f92254600d44e722fb55f2c555982b20a
Henrik Askjer <Henrik.Askjer@uib.no>
2025-01-02 12:48:42 +0100

    use files icon instead of database icon

--- apps/sps-stadnamn-web/src/components/search/results/result-item.tsx
+++ apps/sps-stadnamn-web/src/components/search/results/result-item.tsx
@@ -44,7 +44,7 @@ export default function ResultItem({hit, isMobile}: {hit: any, isMobile: boolean
             <span className="text-neutral-950 flex items-center">{titleRenderer(hit, 'map')}</span>
             {dataset == 'search' && <div className="float-right flex flex-col gap-1 text-neutral-950 text-sm">  { hit.fields?.children?.length > 1 ? 
             <span className="self-center flex gap-1 items-center">
-                <PiDatabase aria-hidden="true"/>
+                <PiFiles aria-hidden="true"/>
                 <span>{hit.fields.children.length} kilder</span>
                 </span>
                 
