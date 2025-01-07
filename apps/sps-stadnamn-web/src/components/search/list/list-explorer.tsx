774aec5c4396fdc3db5c563839a77293f63272fc
Henrik Askjer <Henrik.Askjer@uib.no>
2025-01-07 10:18:30 +0100

    WIP list explorer

--- apps/sps-stadnamn-web/src/components/search/list/list-explorer.tsx
+++ apps/sps-stadnamn-web/src/components/search/list/list-explorer.tsx
@@ -4,26 +4,31 @@ import { SearchContext } from "@/app/search-provider"
 import Pagination from "@/components/results/pagination"
 import ClientThumbnail from "@/components/doc/client-thumbnail"
 import DocInfo from "../info/doc-info"
+import { GlobalContext } from "@/app/global-provider"
 
 export default function ListExplorer() {
     const searchParams = useSearchParams()
     const { tableData, totalHits, isLoading } = useContext(SearchContext)
+    const { isMobile } = useContext(GlobalContext)
+
+
+
     
 
 
     return <div  className='flex flex-col py-2 gap-4 h-full'>
-        
 
-                    <ul className="flex flex-col divide-y divide-neutral-200 instance-info !pt-0">
+
+                    <ul className="flex flex-col divide-y divide-neutral-200 instance-info !pt-0 gap-8">
                         {tableData?.map((item: any, index: number) => {
                             const docDataset = item._index.split('-')[2]
-                            return <li key={index} className="p-2 !py-4 flex">
+                            return <li key={index} className={`p-2 !py-4 flex ${isMobile ? 'flex-col gap-4' : ''}`}>
                                 
-                                
+                                { isMobile && item._source.image?.manifest && <div className="ml-auto"><ClientThumbnail manifestId={item._source.image?.manifest}/></div>}
                                 <div className="flex flex-col px-4">
                                 <DocInfo docParams={{docDataset, docData: item, snidParent: null, sameMarkerList: []}}/>
                                 </div>
-                                { item._source.image?.manifest && <div className="ml-auto"><ClientThumbnail manifestId={item._source.image?.manifest}/></div>}
+                                { !isMobile && item._source.image?.manifest && <div className="ml-auto"><ClientThumbnail manifestId={item._source.image?.manifest}/></div>}
                                 
                             </li>
                       
