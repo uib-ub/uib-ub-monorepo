5704853ba73446c73415d6f58c1ed623d320a28a
Henrik Askjer <Henrik.Askjer@uib.no>
2025-01-07 11:14:32 +0100

    tweak styling

--- apps/sps-stadnamn-web/src/components/tabs/mode-selector.tsx
+++ apps/sps-stadnamn-web/src/components/tabs/mode-selector.tsx
@@ -10,22 +10,22 @@ export default function ModeSelector() {
     return <nav className={`flex ${mode == 'map' ? 'rounded-md shadow-md bg-white' : ''}`}>
               <ParamLink aria-current={mode == 'map' ? 'page' : false}
                       remove={['mode']}
-                      className="flex  m-1 whitespace-nowrap rounded items-center basis-1 gap-1 no-underline w-full lg:w-auto p-1 px-2 aria-[current=page]:bg-neutral-100 aria-[current=page]:text-neutral-950 aria-[current=page]:shadow-inner">
-                        { mode == 'map' ? <PiMapTrifoldFill aria-hidden="true"/>  : <PiMapTrifold aria-hidden="true"/>}<span className="sr-only lg:not-sr-only">Kart</span>
+                      className="flex  m-1 whitespace-nowrap rounded items-center basis-1 gap-1 no-underline w-full lg:w-auto p-1 px-2 aria-[current=page]:bg-neutral-100 text-neutral-950 aria-[current=page]:shadow-inner">
+                        { mode == 'map' ? <PiMapTrifoldFill className="text-2xl xl:text-xl" aria-hidden="true"/>  : <PiMapTrifold className="text-2xl xl:text-xl" aria-hidden="true"/>}<span className="sr-only lg:not-sr-only">Kart</span>
 
                 </ParamLink>
 
 
             <ParamLink add={{mode: 'table', nav: searchParams.get('nav') == 'results' ? 'filters' : searchParams.get('nav')}} 
                         aria-current={mode == 'table' ? 'page' : false}
-                        className="flex  m-1 whitespace-nowrap rounded items-center basis-1 gap-1 no-underline w-full lg:w-auto p-1 px-2 aria-[current=page]:bg-neutral-100 aria-[current=page]:text-neutral-950 aria-[current=page]:shadow-inner">
-                            {mode == 'table' ? <PiTableFill aria-hidden="true"/>  : <PiTable aria-hidden="true"/>}<span className="sr-only lg:not-sr-only">Tabell</span>
+                        className="flex  m-1 whitespace-nowrap rounded items-center basis-1 gap-1 no-underline w-full lg:w-auto p-1 px-2 aria-[current=page]:bg-neutral-100 text-neutral-950 aria-[current=page]:shadow-inner">
+                            {mode == 'table' ? <PiTableFill className="text-2xl xl:text-xl" aria-hidden="true"/>  : <PiTable className="text-2xl xl:text-xl" aria-hidden="true"/>}<span className="sr-only lg:not-sr-only">Tabell</span>
             </ParamLink>
 
             <ParamLink add={{mode: 'list', nav: searchParams.get('nav') == 'results' ? 'filters' : searchParams.get('nav')}} 
                         aria-current={mode == 'list' ? 'page' : false}
-                        className="flex  m-1 whitespace-nowrap rounded items-center basis-1 gap-1 no-underline w-full lg:w-auto p-1 px-2 aria-[current=page]:bg-neutral-100 aria-[current=page]:text-neutral-950 aria-[current=page]:shadow-inner">
-                            {mode == 'list' ? <PiRowsFill aria-hidden="true"/>  : <PiRows aria-hidden="true"/>}<span className="sr-only lg:not-sr-only">Liste</span>
+                        className="flex  m-1 whitespace-nowrap rounded items-center basis-1 gap-1 no-underline w-full lg:w-auto p-1 px-2 aria-[current=page]:bg-neutral-100 text-neutral-950 aria-[current=page]:shadow-inner">
+                            {mode == 'list' ? <PiRowsFill className="text-2xl xl:text-xl" aria-hidden="true"/>  : <PiRows className="text-2xl xl:text-xl" aria-hidden="true"/>}<span className="sr-only lg:not-sr-only">Liste</span>
             </ParamLink>
 
 
