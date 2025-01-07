40c8dfc612c37d4048e0319a9acfbd46c37b5462
Henrik Askjer <Henrik.Askjer@uib.no>
2025-01-07 10:16:43 +0100

    smaller cadastre breadcrumb

--- apps/sps-stadnamn-web/src/components/search/info/cadastre-breadcrumb.tsx
+++ apps/sps-stadnamn-web/src/components/search/info/cadastre-breadcrumb.tsx
@@ -1,9 +1,15 @@
 import ParamLink from "@/components/ui/param-link"
 import { treeSettings } from "@/config/server-config"
 import { getValueByPath } from "@/lib/utils"
+import { useSearchParams } from "next/navigation"
 
 export default function CadastreBreadcrumb({source, docDataset, subunitName}: {source: Record<string, any>, docDataset: string, subunitName: string}) {
+    const mode = useSearchParams().get('mode')
     const parentLabel = getValueByPath(source, treeSettings[docDataset]?.subunit) + " " + getValueByPath(source, subunitName )
     const currentName = getValueByPath(source, treeSettings[docDataset]?.leaf) + " " + source.label
-    return <div><ParamLink className="breadcrumb-link text-base" add={{doc: source.within}}>{parentLabel}</ParamLink><span className="mx-2">/</span>{currentName}</div>
+    return <div>
+      <ParamLink className="breadcrumb-link text-base" 
+                 add={{doc: source.within, parent: mode != 'map' ? source.within : null}}>{parentLabel}
+      </ParamLink>
+      <span className="mx-2">/</span>{currentName}</div>
   }
