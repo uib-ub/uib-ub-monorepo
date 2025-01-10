8b675f9a0b2b526e4b7c26dd7d3127002c9c838c
Henrik Askjer <Henrik.Askjer@uib.no>
2025-01-10 16:24:41 +0100

    dedicated pinned state

--- apps/sps-stadnamn-web/src/app/global-provider.tsx
+++ apps/sps-stadnamn-web/src/app/global-provider.tsx
@@ -7,45 +7,37 @@ import { SearchContext } from './search-provider';
 
 interface FacetOption {
   sort: 'doc_count' | 'asc' | 'desc';
-  isPinned: boolean;
-  filters: Array<[string, string]>;
+  pinningActive: boolean;
 }
 
-interface DatasetOptions {
-  facets: Record<string, FacetOption>;
-  pinned: Array<[string, string]>;
-}
-
-interface FacetOptions extends Record<string, DatasetOptions> {}
-
-interface GlobalContextData {
-  currentUrl: string | null;
-  setCurrentUrl: (url: string | null) => void;
-  isMobile: boolean;
-  facetOptions: FacetOptions;
-  updateFacetOption: (facetName: string, updates: Partial<FacetOption>) => void;
-  setPinnedFilters: (filters: [string, string][]) => void;
-}
-
-export const GlobalContext = createContext<GlobalContextData>({
-  currentUrl: null,
-  setCurrentUrl: () => {},
+export const GlobalContext = createContext({
+  currentUrl: null as string | null,    
+  setCurrentUrl: (url: string | null) => {},
   isMobile: false,
-  facetOptions: {},
-  updateFacetOption: () => {},
-  setPinnedFilters: () => {}
+  facetOptions: {} as Record<string, Record<string, Partial<FacetOption>>>,
+  pinnedFilters: {} as Record<string, [string, string][]>,
+  updateFacetOption: (facetName: string, options: Partial<FacetOption>) => {},
+  updatePinnedFilters: (filters: [string, string][]) => {},
 });
 
 export default function GlobalProvider({ children, isMobile }: { children: React.ReactNode, isMobile: boolean }) {
   const [currentUrl, setCurrentUrl] = useState<string | null>(null);
-  const [facetOptions, setFacetOptions] = useState<FacetOptions>({});
+  const [facetOptions, setFacetOptions] = useState<Record<string, Record<string, Partial<FacetOption>>>>({});
+  const [pinnedFilters, setPinnedFilters] = useState<Record<string, [string, string][]>>({});
   const dataset = useDataset()
 
   // Load facet options from localStorage on mount
   useEffect(() => {
     const storedOptions = localStorage.getItem('facetOptions');
     if (storedOptions) {
+        console.log("Loading facet options from localStorage")
       setFacetOptions(JSON.parse(storedOptions));
+    }
+
+    const storedPinnedFilters = localStorage.getItem('pinnedFilters');
+    if (storedPinnedFilters) {
+        console.log("Loading pinned filters from localStorage")
+        setPinnedFilters(JSON.parse(storedPinnedFilters));
     }
   }, []);
 
@@ -54,35 +46,34 @@ export default function GlobalProvider({ children, isMobile }: { children: React
     localStorage.setItem('facetOptions', JSON.stringify(facetOptions));
   }, [facetOptions]);
 
-  const updateFacetOption = (facetName: string, updates: Partial<FacetOption>) => {
+  useEffect(() => {
+    localStorage.setItem('pinnedFilters', JSON.stringify(pinnedFilters));
+  }, [pinnedFilters]);
+
+  const updateFacetOption = (facetName: string, options: Partial<FacetOption>) => {
     setFacetOptions(prev => ({
       ...prev,
       [dataset]: {
-        ...prev[dataset] || { facets: {}, pinned: [] },
-        facets: {
-          ...(prev[dataset]?.facets || {}),
-          [facetName]: {
-            ...(prev[dataset]?.facets?.[facetName] || { 
-              sort: facetConfig[dataset].find(item => item.key == facetName)?.sort || 'doc_count', 
-              isPinned: false,
-              filters: []
-            }),
-            ...updates
-          }
+        ...prev[dataset] || {},
+        [facetName]: {
+          ...(prev[dataset]?.[facetName] || { 
+            sort: facetConfig[dataset].find(item => item.key == facetName)?.sort || 'doc_count', 
+            pinningActive: false,
+          }),
+          ...options
         }
       }
     }));
   };
 
-  const setPinnedFilters = (filters: [string, string][]) => {
-    setFacetOptions(prev => ({
+  const updatePinnedFilters = (filters: [string, string][]) => {
+    setPinnedFilters(prev => ({
       ...prev,
-      [dataset]: {
-        ...prev[dataset] || { facets: {}, pinned: [] },
-        pinned: filters
-      }
+      [dataset]: filters
     }));
   };
+
+ 
 
   return (
     <GlobalContext.Provider 
@@ -92,7 +83,8 @@ export default function GlobalProvider({ children, isMobile }: { children: React
         isMobile,
         facetOptions,
         updateFacetOption,
-        setPinnedFilters
+        pinnedFilters,
+        updatePinnedFilters,
       }}
     >
       {children}
