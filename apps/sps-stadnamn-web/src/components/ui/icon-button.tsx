3458d6b95989171773b66df121e1480c37630cea
Henrik Askjer <Henrik.Askjer@uib.no>
2025-01-13 14:30:12 +0100

    tooltip not needed on  mobile

--- apps/sps-stadnamn-web/src/components/ui/icon-button.tsx
+++ apps/sps-stadnamn-web/src/components/ui/icon-button.tsx
@@ -1,30 +1,31 @@
 'use client'
-import { useRouter } from 'next/navigation'
+import { GlobalContext } from "@/app/global-provider";
 import {
     Tooltip,
     TooltipContent,
     TooltipProvider,
     TooltipTrigger,
   } from "@/components/ui/tooltip"
+import { useContext } from "react";
 
+/*
+Tooltip button without route handling
+ */
 
-export default function IconButton({ children, label, type, side = 'bottom', href, ...rest }: 
-    { children: React.ReactNode, className?: string, label: string, href?: string, side?: "bottom" | "left" | "right" | "top", [x: string]: any, type?: "button" | "submit" | "reset" }) {
-    const router = useRouter();
+export default function IconButton({ children, label, type, side = 'bottom', onClick, ...rest }: 
+    { children: React.ReactNode, className?: string, label: string, side?: "bottom" | "left" | "right" | "top", [x: string]: any, type?: "button" | "submit" | "reset" }) {
+        const { isMobile } = useContext(GlobalContext)
 
-    const handleClick = (e: React.MouseEvent) => {
-        if (href) {
-            e.preventDefault();
-            router.push(href);
+        if (isMobile) {
+            return <button aria-label={label} type={type || "button"} onClick={onClick} {...rest}><i aria-hidden='true'>{children}</i></button>
         }
-    };
-    
-    return (
+        else {
+            return (
 
         <TooltipProvider>
             <Tooltip>
                 <TooltipTrigger asChild>
-                    <button aria-label={label} type={type || "button"} onClick={handleClick} {...rest}>
+                    <button aria-label={label} type={type || "button"} onClick={onClick} {...rest}>
                      <i  aria-hidden='true'>{children}</i>
                     </button>
                 </TooltipTrigger>
@@ -35,4 +36,5 @@ export default function IconButton({ children, label, type, side = 'bottom', hre
         </TooltipProvider>
 
     );
+}
 }
