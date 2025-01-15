5c8ca68cc5d63c92f3564b923f9fbf7846040beb
Henrik Askjer <Henrik.Askjer@uib.no>
2025-01-15 17:38:56 +0100

    same height for top toolbars

--- apps/sps-stadnamn-web/src/components/tabs/mode-selector.tsx
+++ apps/sps-stadnamn-web/src/components/tabs/mode-selector.tsx
@@ -15,7 +15,7 @@ export default function ModeSelector() {
                       role="tab"
                       label="Kart"
                       remove={['mode']}
-                      className="flex  m-1 whitespace-nowrap rounded items-center basis-1 gap-1 no-underline w-full lg:w-auto p-1 px-2 aria-selected:bg-neutral-100 text-neutral-950 aria-selected:shadow-inner">
+                      className="flex  m-1 h-8 whitespace-nowrap rounded items-center basis-1 gap-1 no-underline w-full lg:w-auto p-1 px-2 aria-selected:bg-neutral-100 text-neutral-950 aria-selected:shadow-inner">
                         { mode == 'map' ? <PiMapTrifoldFill className="text-2xl xl:text-xl" aria-hidden="true"/>  : <PiMapTrifold className="text-2xl xl:text-xl" aria-hidden="true"/>}
 
             </ClickableIcon>
@@ -24,7 +24,7 @@ export default function ModeSelector() {
                         role="tab"
                         label="Tabell"
                         aria-selected={mode == 'table' ? true : false}
-                        className="flex  m-1 whitespace-nowrap rounded items-center basis-1 gap-1 no-underline w-full lg:w-auto p-1 px-2 aria-selected:bg-neutral-100 text-neutral-950 aria-selected:shadow-inner">
+                        className="flex  m-1 h-8 whitespace-nowrap rounded items-center basis-1 gap-1 no-underline w-full lg:w-auto p-1 px-2 aria-selected:bg-neutral-100 text-neutral-950 aria-selected:shadow-inner">
                             {mode == 'table' ? <PiTableFill className="text-2xl xl:text-xl" aria-hidden="true"/>  : <PiTable className="text-2xl xl:text-xl" aria-hidden="true"/>}
             </ClickableIcon>
 
@@ -32,7 +32,7 @@ export default function ModeSelector() {
                         role="tab"
                         label="Liste"
                         aria-selected={mode == 'list' ? true : false}
-                        className="flex  m-1 whitespace-nowrap rounded items-center basis-1 gap-1 no-underline w-full lg:w-auto p-1 px-2 aria-selected:bg-neutral-100 text-neutral-950 aria-selected:shadow-inner">
+                        className="flex  m-1 h-8 whitespace-nowrap rounded items-center basis-1 gap-1 no-underline w-full lg:w-auto p-1 px-2 aria-selected:bg-neutral-100 text-neutral-950 aria-selected:shadow-inner">
                             {mode == 'list' ? <PiRowsFill className="text-2xl xl:text-xl" aria-hidden="true"/>  : <PiRows className="text-2xl xl:text-xl" aria-hidden="true"/>}
             </ClickableIcon>
         </div>
