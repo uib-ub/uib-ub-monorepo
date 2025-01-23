7106bf579a54c63a62acea3faf41c2151882c421
Henrik Askjer <Henrik.Askjer@uib.no>
2025-01-23 13:22:53 +0100

    remove padding

--- apps/sps-stadnamn-web/src/components/doc/collapsible-heading.tsx
+++ apps/sps-stadnamn-web/src/components/doc/collapsible-heading.tsx
@@ -11,7 +11,7 @@ export default function CollapsibleHeading(props: {
   const [open, setOpen] = useState(props.alwaysOpen || false);
 
   return <div className="">
-    <h3 className="!mt-0"><button className="text-left w-full text-neutral-900 flex items-center gap-1" aria-expanded={open} aria-controls={props.title + '-collapsible'} onClick={() => setOpen(!open)}>
+    <h3 className="!mt-0 !py-0"><button className="text-left w-full text-neutral-900 flex items-center gap-1" aria-expanded={open} aria-controls={props.title + '-collapsible'} onClick={() => setOpen(!open)}>
         
         {props.title} {open ? <PiCaretUp className="inline self-center text-primary-600" /> : <PiCaretDown className="inline self-center text-primary-600" />}</button></h3>
         
