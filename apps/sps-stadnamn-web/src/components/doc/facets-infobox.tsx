99fa5ba09d520913748ae9767568e800fa5dfd97
Henrik.Askjer <henrik.askjer@uib.no>
2025-01-28 11:38:06 +0100

    avoid inadvertent navigation to different search view

--- apps/sps-stadnamn-web/src/components/doc/facets-infobox.tsx
+++ apps/sps-stadnamn-web/src/components/doc/facets-infobox.tsx
@@ -1,11 +1,18 @@
 'use client'
 import Link from "next/link";
-import { PiMagnifyingGlass } from "react-icons/pi";
+import { PiFunnel, PiMagnifyingGlass } from "react-icons/pi";
 import { facetConfig } from "@/config/search-config";
 import { getValueByPath } from "@/lib/utils";
 import { createSerializer, parseAsString } from "nuqs";
+import { DocContext } from "@/app/doc-provider";
+import { useContext } from "react";
+import { useDataset } from "@/lib/search-params";
+import Clickable from "../ui/clickable/clickable";
+import { datasetTitles } from "@/config/metadata-config";
 
-export default function FacetsInfobox({ dataset, source }: { dataset: string, source: Record<string,any> }) {
+export default function FacetsInfobox({ source }: { source: Record<string,any> }) {
+  const { docDataset } = useContext(DocContext)
+  const dataset = useDataset()
   const serialize = createSerializer({
     dataset: parseAsString,
     nav: parseAsString,
@@ -42,14 +49,19 @@ export default function FacetsInfobox({ dataset, source }: { dataset: string, so
     const filteredItems = items.filter(item =>  (item.items?.length && item.items[0].value?.length));
 
     const subitemRenderer = (item: any) => {
-      return (
-        
-        <Link className="no-underline flex items-center gap-1" 
-                        href={item.hrefParams ? buildHref(item.hrefParams) : item.href || serialize({dataset,  [item.key]: item.value, nav: 'results'})}>
-                    {item.value}
-                    <PiMagnifyingGlass aria-hidden={true} className="inline text-primary-600"/>
-                  </Link>
-      )
+      if (dataset == docDataset) {
+        return (
+          <Link className="no-underline flex items-center gap-1" 
+                href={item.hrefParams ? buildHref(item.hrefParams) : item.href || serialize({dataset,  [item.key]: item.value, nav: 'results'})}>
+                      {item.value}
+                      <PiMagnifyingGlass aria-hidden={true} className="inline text-primary-600"/>
+          </Link>
+        )
+      }
+      else {
+        // No link if not same dataset
+        return item.value
+      }
     }
 
 
@@ -59,24 +71,35 @@ export default function FacetsInfobox({ dataset, source }: { dataset: string, so
       return serialize({dataset, nav: 'results', ...newParams})
     }
 
-    return filteredItems?.length > 0 ? (
-      <div className="flex flex-col sm:flex-row flex-wrap gap-8 p-4 inner-slate">
-        {filteredItems.map((item: Record<string,any> , index: number) => (
-            <div key={index} className="flex flex-col">
-                <strong className="text-neutral-900">{item.title}</strong>
-                {item.items?.length == 1 && <p>{subitemRenderer(item.items[0])}</p>}                
-                {item.items?.length > 1 && <ul className="!list-none flex flex-wrap gap-x-4 !mx-0 !px-0"> 
-                  {item.items.map((subItem: any, subIndex: number) => (
-                    <li key={subIndex}> {subitemRenderer(subItem)} </li>
-                  ))}
-                </ul> }
-                {!item.items &&  <p>{subitemRenderer(item)}</p>}
+    return filteredItems?.length > 0 && docDataset? (
+      
+        <div className="flex flex-col gap-4 p-4 inner-slate">
+          <div className="flex flex-col sm:flex-row flex-wrap gap-8">
+            {filteredItems.map((item: Record<string,any> , index: number) => (
+                <div key={index} className="flex flex-col">
+                    <strong className="text-neutral-900">{item.title}</strong>
+                    {item.items?.length == 1 && <p>{subitemRenderer(item.items[0])}</p>}                
+                    {item.items?.length > 1 && <ul className="!list-none flex flex-wrap gap-x-4 !mx-0 !px-0"> 
+                      {item.items.map((subItem: any, subIndex: number) => (
+                        <li key={subIndex}> {subitemRenderer(subItem)} </li>
+                      ))}
+                    </ul> }
+                    {!item.items &&  <p>{subitemRenderer(item)}</p>}
+                </div>
+            ))}
+          </div>
+          {dataset !== docDataset && (
+            <Clickable
+              link 
+              only={{dataset: docDataset, doc: source.uuid}}
+              className="no-underline flex items-center gap-1"
+            >
+              <PiFunnel aria-hidden={true} className="inline text-primary-600"/>
+              Filtrer i søkevisning for {datasetTitles[docDataset]}
+            </Clickable>
+          )}
+        </div>
 
-            </div>
-        ))}
-            
-     </div>
-  
     ) : null
   
   
