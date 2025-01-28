dcfa3a666e7ff4625695ef2e86e4ccb79778c456
Henrik.Askjer <henrik.askjer@uib.no>
2025-01-28 17:44:01 +0100

    add icon link with tooltip, only href needed

---
+++ apps/sps-stadnamn-web/src/components/ui/icon-link.tsx
@@ -0,0 +1,36 @@
+'use client'
+import Link from 'next/link'
+import { GlobalContext } from "@/app/global-provider"
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip"
+import { useContext } from "react"
+
+// Icon link with tooltip that only requires href
+
+export default function IconLink({ children, label, href, side = 'bottom', ...rest }: 
+    { children: React.ReactNode, label: string, href: string, side?: "bottom" | "left" | "right" | "top", [x: string]: any }) {
+    const { isMobile } = useContext(GlobalContext)
+
+    if (isMobile) {
+        return <Link href={href} aria-label={label} {...rest}><i aria-hidden='true'>{children}</i></Link>
+    }
+    
+    return (
+        <TooltipProvider>
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    <Link href={href} aria-label={label} {...rest}>
+                        <i aria-hidden='true'>{children}</i>
+                    </Link>
+                </TooltipTrigger>
+                <TooltipContent side={side}>
+                    {label}
+                </TooltipContent>
+            </Tooltip>
+        </TooltipProvider>
+    )
+}
