fd2e72b499c57727f63fe709ffee3d96ba7f7b28
Henrik Askjer <Henrik.Askjer@uib.no>
2025-01-29 15:33:05 +0100

    fill all available space

--- apps/sps-stadnamn-web/src/components/search/desktop-layout.tsx
+++ apps/sps-stadnamn-web/src/components/search/desktop-layout.tsx
@@ -52,7 +52,7 @@ export default function DesktopLayout() {
 
        { mode == 'map' && (doc || parent) &&
         <div className="lg:absolute right-0 top-0 pb-6 flex flex-col items-end p-2 justify-between gap-2 h-full">
-        <div className={`flex flex-col  w-[30svw] 2xl:w-[25svw] !z-[3001] ${parent ? 'lg:max-h-[50svh] lg:min-h-[25svh]' :  'lg:max-h-[calc(100svh - 2rem)] lg:min-h-[25svh]'}`}>
+        <div className={`flex flex-col  w-[30svw] 2xl:w-[25svw] !z-[3001] ${parent ? ' lg:min-h-[25svh]' :  'lg:max-h-[calc(100svh - 2rem)] lg:min-h-[25svh]'}`}>
         {doc && !docLoading && docData?._source && <div className={`bg-white relative lg:rounded-md lg:shadow-md break-words pr-4 pl-4 py-2 overflow-y-auto stable-scrollbar`}>
             <DocInfo/>
         </div>}
