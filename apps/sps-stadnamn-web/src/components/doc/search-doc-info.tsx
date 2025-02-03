672a57836db6162759291d185dd98528297f9450
Henrik Askjer <Henrik.Askjer@uib.no>
2025-02-03 15:08:03 +0100

    remove slate background from source filters

--- apps/sps-stadnamn-web/src/components/doc/search-doc-info.tsx
+++ apps/sps-stadnamn-web/src/components/doc/search-doc-info.tsx
@@ -37,7 +37,7 @@ export default function SearchDocInfo({docSource}: {docSource: any}) {
 
 
 
-    <div className={` flex flex-col ${hasAltLabels || hasAttestations ? 'border-2 p-2 inner-slate' : ''}`}>
+    <div className={` flex flex-col`}>
     {hasAltLabels && <ul className='flex flex-wrap !list-none !p-0 gap-1'>
     {Array.from(uniqueLabels).map((label: string, index: number) => {
       const isActive = sourceLabel === label
@@ -46,7 +46,7 @@ export default function SearchDocInfo({docSource}: {docSource: any}) {
           link 
           add={{sourceLabel: label, parent: doc}} 
           remove={["sourceLabel", "sourceDataset"]} 
-          className={`no-underline border shadow-sm rounded-full px-3 py-1
+          className={`no-underline border shadow-sm rounded-md px-3 py-1
             ${isActive ? '!bg-accent-700 text-white border-accent-700' : 'bg-white border-neutral-200'} text-neutral-950`}
           aria-current={isActive ? 'page' : undefined}
         >
@@ -66,15 +66,15 @@ export default function SearchDocInfo({docSource}: {docSource: any}) {
         
         {docSource.datasets.map((dataset: string, index: number) => {
           const isActive = parent && sourceDataset === dataset
-          return <li key={index} className='whitespace-nowrap !m-0 !p-0'>
+          return <li key={index} className='whitespace-nowrap !m-0 !p-0 grow basis-0'>
             <Clickable 
                
               className={`
                 flex items-center gap-1 
                 no-underline border rounded-md rounded-full 
-                pr-3 pl-2 py-1 
+                pr-3 pl-2 py-2 xl:py-1 
+                w-full shadow-sm
                 ${isActive ? '!bg-accent-700 text-white border-accent-700' : 'bg-white border-neutral-200'}
-                ${hasAltLabels || hasAttestations ? 'shadow-sm' : ''}
               `}
               add={docSource.datasets.length > 1 ? {sourceDataset: dataset, parent: doc} : {parent: doc}}
               remove={["sourceLabel", "sourceDataset"]}
@@ -89,15 +89,15 @@ export default function SearchDocInfo({docSource}: {docSource: any}) {
         })}
 
     {docSource.datasets.length > 1 && (
-          <li className='whitespace-nowrap !m-0 !p-0'>
+          <li className='whitespace-nowrap !m-0 !p-0 xl:grow xl:basis-0 w-full xl:w-auto'>
             <Clickable 
                
               className={`
                 flex items-center gap-1 
+                w-full
                 no-underline border rounded-md rounded-full 
-                pr-3 pl-2 py-1 
+                pr-3 pl-2 py-2 xl:py-1 text-black shadow-sm
                 ${parent && !sourceLabel && !sourceDataset ? '!bg-accent-700 text-white border-accent-700' : 'bg-white border-neutral-200'}
-                ${hasAltLabels || hasAttestations ? 'shadow-sm' : ''}
               `}
               remove={["sourceLabel", "sourceDataset"]}
               add={{parent: doc}}
@@ -105,7 +105,7 @@ export default function SearchDocInfo({docSource}: {docSource: any}) {
               aria-expanded={isMobile ? undefined : parent ? true : false}
               aria-controls={isMobile ? undefined : 'children-window'}
             >
-              <PiFilesFill className={`${parent && !sourceLabel && !sourceDataset ? 'text-white' : 'text-neutral-700'}`} />
+                <PiFilesFill className="text-xl text-primary-600" />
               Alle kjelder
             </Clickable>
           </li>
