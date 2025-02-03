71485dabc92be6651d5914f6c2acee30464c6458
Henrik Askjer <Henrik.Askjer@uib.no>
2025-02-03 10:34:40 +0100

    show list if only one column, and tweak styling

--- apps/sps-stadnamn-web/src/components/children/cadastral-subdivisions.tsx
+++ apps/sps-stadnamn-web/src/components/children/cadastral-subdivisions.tsx
@@ -41,54 +41,80 @@ export default function CadastralSubdivisions() {
             <h2 className="pb-2">{gnr} {parentData?._source?.label}</h2>
             
             : <div className="flex rounded-t-md">
-                <h2 className={`p-2 px-2 text-lg  !font-sans text`}>
+                <h2 className={`py-2 pr-8 text-lg  !font-sans text`}>
                     <Clickable link aria-current={doc == parentData?._source?.uuid ? 'page' : false} 
-                                      className="no-underline flex items-center gap-1"
-                                      add={{ doc: parent }}>{gnr} {parentData?._source?.label} { doc == parentData?._source?.uuid ? <PiInfoFill className="text-accent-800" aria-hidden="true"/> : <PiInfo className="text-primary-600" aria-hidden="true"/>}
+                                      className="no-underline flex items-center gap-2"
+                                      add={{ doc: parent }}>
+                                        { doc == parentData?._source?.uuid ? <PiInfoFill className="text-accent-800" aria-hidden="true"/> : <PiInfo className="text-primary-600" aria-hidden="true"/>}
+                                        {gnr} {parentData?._source?.label} 
                     </Clickable>
 
                 </h2>
                 {mode == 'map' && 
                 <div className="float-right text-2xl flex gap-2 p-1 items-center ml-auto">
+                    <Clickable aria-label="Lukk" remove={['parent']} add={docView?.current ? docView.current : {}}><PiX aria-hidden="true"/></Clickable>
+                </div>}
+            </div>}
 
-                <Clickable aria-label="Lukk" remove={['parent']} add={docView?.current ? docView.current : {}}><PiX aria-hidden="true"/></Clickable>
-                </div>}
-                </div>}
+            {fields.length === 0 ? (
+                <div className="instance-info">
+                    <ul className="!p-0 divide-y divide-neutral-200">
+                        {childrenData?.map((hit: any) => (
+                            <li key={hit._id}>
+                                <Clickable link aria-current={doc==hit._source?.uuid ? 'page' : false} 
+                                    className="no-underline !flex !w-full p-2 !h-full items-center gap-2"
+                                    add={{ doc: hit._source?.uuid }}>
+                                    {doc == hit._source?.uuid ? 
+                                        <PiInfoFill className="text-accent-800 text-xl" aria-hidden="true"/> : 
+                                        <PiInfo className="text-primary-600 text-xl" aria-hidden="true"/>
+                                    }
+                                    <span>
+                                        {Array.isArray(hit._source?.[leaf]) ? hit._source?.[leaf]?.join(", ") : hit._source?.[leaf]}{' '}
+                                        {Array.isArray(hit._source?.cadastre?.[0]?.bnr) ? hit._source?.cadastre?.[0]?.bnr?.join(", ") : hit._source?.cadastre?.[0]?.bnr}{' '}
+                                        {hit._source?.label}
+                                    </span>
+                                </Clickable>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            ) : (
                 <div className="overflow-x-auto border border-neutral-300 rounded-md mb-2">
-                        <table className="w-full result-table">
-                            <thead className="w-full">
-                                <tr>
-                                    <th className="">Bruk</th>
+                    <table className="w-full result-table">
+                        <thead className="w-full">
+                            <tr>
+                                <th className="">Bruk</th>
+                                {fields.map((field: Record<string, any>) => (
+                                    <th className="" key={field.key}>{field.label}</th>
+                                ))}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {childrenData?.map((hit: any) => (
+                                <tr key={hit._id}>
+                                    <th className="!p-0">
+                                    <Clickable link aria-current={doc==hit._source?.uuid ? 'page' : false} 
+                                                className={`no-underline !flex !w-full p-2 !h-full items-center gap-2`}
+                                                add={{ doc: hit._source?.uuid }}>
+                                    { doc == hit._source?.uuid ? <PiInfoFill className="text-accent-800 text-xl" aria-hidden="true"/> : <PiInfo className="text-primary-600 text-xl" aria-hidden="true"/>}
+                                        {Array.isArray(hit._source?.[leaf]) ? hit._source?.[leaf]?.join(", ") : hit._source?.[leaf]}{' '}
+                                        {Array.isArray(hit._source?.cadastre?.[0]?.bnr) ? hit._source?.cadastre?.[0]?.bnr?.join(", ") : hit._source?.cadastre?.[0]?.bnr}{' '}
+                                        {hit._source?.label}
+                                    
+                                        
+                                                                
+                                                                
+                                        </Clickable>
+                                    </th>
                                     {fields.map((field: Record<string, any>) => (
-                                        <th className="" key={field.key}>{field.label}</th>
+                                        <td className="p-2" key={field.key}>{getValueByPath(hit._source, field.key)}</td>
                                     ))}
                                 </tr>
-                            </thead>
-                            <tbody>
-                                {childrenData?.map((hit: any) => (
-                                    <tr key={hit._id}>
-                                        <th className="!p-0">
-                                        <Clickable link aria-current={doc==hit._source?.uuid ? 'page' : false} 
-                                                            className={`no-underline !flex !w-full p-2 !h-full ${doc == hit._source?.uuid ? 'border-l-4 bg-accent-800' : 'pl-2'} `}
-                                                            add={{ doc: hit._source?.uuid }}>
-                                        <span className={`${doc == hit._source?.uuid ? 'text-white' : 'text-black'}`}>
-                                            {Array.isArray(hit._source?.[leaf]) ? hit._source?.[leaf]?.join(", ") : hit._source?.[leaf]}{' '}
-                                            {Array.isArray(hit._source?.cadastre?.[0]?.bnr) ? hit._source?.cadastre?.[0]?.bnr?.join(", ") : hit._source?.cadastre?.[0]?.bnr}{' '}
-                                            {hit._source?.label}
-                                        </span>
-                                            
-                                                                
-                                                                
-                                            </Clickable>
-                                        </th>
-                                        {fields.map((field: Record<string, any>) => (
-                                            <td className="p-2" key={field.key}>{getValueByPath(hit._source, field.key)}</td>
-                                        ))}
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                        </div>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            )}
     </>
     )
 }
