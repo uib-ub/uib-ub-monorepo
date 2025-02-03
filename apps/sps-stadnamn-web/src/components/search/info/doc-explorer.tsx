bb96833b7c8a6a07298b714124bec5070a868b98
Henrik Askjer <Henrik.Askjer@uib.no>
2025-02-03 14:44:51 +0100

    adapt doc explorer to new source filters

--- apps/sps-stadnamn-web/src/components/search/info/doc-explorer.tsx
+++ apps/sps-stadnamn-web/src/components/search/info/doc-explorer.tsx
@@ -11,7 +11,7 @@ import { treeSettings } from "@/config/server-config";
 import CadastralSubdivisions from "@/components/children/cadastral-subdivisions";
 import SourceList from "../../children/source-list";
 import Clickable from "@/components/ui/clickable/clickable";
-import { PiArrowLeft, PiCaretLeft, PiFilesFill, PiRows, PiTable, PiTag, PiTagFill, PiX } from "react-icons/pi";
+import { PiArrowLeft, PiCaretLeft, PiFilesFill, PiInfoFill, PiRows, PiTable, PiTag, PiTagFill, PiX } from "react-icons/pi";
 import { GlobalContext } from "@/app/global-provider";
 
 
@@ -29,7 +29,7 @@ export default function DocExplorer({hidden}: {hidden: boolean}) {
     return <>
     { docLoading && <div className="flex px-4 mt-4 mb-8"><DocSkeleton/></div>}
     {doc && !docLoading && docData?._source &&
-    <div className={`${hidden ? 'hidden' : ''} instance-info ${doc != parent ? 'xl:!pt-4 xl:mt-2 pb-4' : ''} xl:px-4 gap-8 flex flex-col`}>
+    <div className={`${hidden ? 'hidden' : ''} instance-info ${doc != parent ? 'xl:!pt-4 xl:mt-2 pb-4' : ''} xl:px-4 gap-8 flex flex-col pb-8`}>
         {(!parent || doc != parent || mode != 'map') && <DocInfo/>}
 
         {parent ? 
@@ -50,8 +50,8 @@ export default function DocExplorer({hidden}: {hidden: boolean}) {
             { !docLoading && !childrenLoading && !parentLoading &&
             <div className="flex flex-col lg:flex-row gap-1 xl:gap-2 py-4 w-full lg:w-auto text-neutral-950">
             {!parent && false && treeSettings[dataset] && docData?._source?.sosi == 'gard' && <Clickable link className="flex p-4 lg:p-2   gap-2 w-full lg:w-auto rounded-md bg-neutral-50 border border-neutral-200 h-full items-center no-underline" add={{parent: docData?._source?.uuid}}><PiFilesFill className="text-2xl text-primary-600"/>Underordna bruk</Clickable>}
-            {!parent && docData?._source?.children?.length > 0 && <Clickable link className="flex p-4 lg:p-2 gap-2 w-full lg:w-auto rounded-md bg-neutral-50 border border-neutral-200 h-full items-center no-underline" add={{parent: docData?._source?.uuid}}><PiFilesFill className="text-2xl text-primary-600"/>Kjelder</Clickable>}
-            {parent && !treeSettings[dataset] && <Clickable link className="flex p-4 lg:p-2 gap-2 w-full lg:w-auto rounded-md bg-neutral-50 border border-neutral-200 h-full items-center no-underline" remove={['parent']} add={{doc: parent}}><PiTag className="text-2xl text-primary-600"/>Stadnamnoppslag</Clickable>}
+            {false && !parent && docData?._source?.children?.length > 0 && <Clickable link className="flex p-4 lg:p-2 gap-2 w-full lg:w-auto rounded-md bg-neutral-50 border border-neutral-200 h-full items-center no-underline" add={{parent: docData?._source?.uuid}}><PiFilesFill className="text-2xl text-primary-600"/>Kjelder</Clickable>}
+            {parent && !treeSettings[dataset] && <Clickable link className="flex p-4 lg:p-2 gap-2 w-full lg:w-auto rounded-md bg-neutral-50 border border-neutral-200 h-full items-center no-underline" remove={['parent']} add={{doc: parent}}><PiInfoFill className="text-2xl text-primary-600"/>Stadnamnoppslag</Clickable>}
             <Clickable link className="flex p-4 lg:p-2 gap-2 w-full lg:w-auto rounded-md bg-neutral-50 border border-neutral-200 h-full items-center no-underline" remove={['doc', 'parent']}>
                { mode == 'table' &&<><PiTable className="text-2xl text-primary-600"/>Vis trefftabellen</>}
                { mode == 'list' &&<><PiRows className="text-2xl text-primary-600"/>Vis trefflista</>}
