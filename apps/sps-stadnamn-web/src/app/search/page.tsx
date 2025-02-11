e41b6ee25cfcc096add6a2ff5e180caab52f5de1
Henrik Askjer <Henrik.Askjer@uib.no>
2025-02-11 11:37:54 +0100

    add metadata in search

--- apps/sps-stadnamn-web/src/app/search/page.tsx
+++ apps/sps-stadnamn-web/src/app/search/page.tsx
@@ -5,6 +5,15 @@ import MobileLayout from "@/components/search/mobile-layout";
 import DesktopLayout from "@/components/search/desktop-layout";
 import DocProvider from "@/app/doc-provider";
 import ChildrenProvider from "../children-provider";
+import { datasetTitles, datasetShortDescriptions } from "@/config/metadata-config";
+
+export async function generateMetadata({searchParams}: {searchParams: Promise<{dataset: string}>}) {
+  const { dataset } = await searchParams
+  return {
+    title: datasetTitles[dataset || 'search'],
+    description: datasetShortDescriptions[dataset || 'search']
+  }
+}
 
 export default async function SearchPage() {
   const headersList = await headers()
