62df392964ff79e299b114f28b663e32fe75469c
Henrik Askjer <Henrik.Askjer@uib.no>
2025-02-11 10:07:37 +0100

    restyle side panel at landing page

--- apps/sps-stadnamn-web/src/app/uuid/[uuid]/server-parent.tsx
+++ apps/sps-stadnamn-web/src/app/uuid/[uuid]/server-parent.tsx
@@ -8,13 +8,13 @@ export default async function ServerParent({uuid}: {uuid: string}) {
     const parent = await fetchSNIDParent(uuid)
     
 
-    return <aside className="bg-neutral-800 text-white px-4 pb-4 pt-0 rounded-md">
-        <h2 className="!text-neutral-50 !uppercase !font-semibold !tracking-wider !text-sm !font-sans !m-0">Overordna oppslag</h2>
-        <h3 className="!text-white !m-0 !p-0 font-serif !text-xl !font-normal flex items-center gap-2">
-            <Link aria-label="Opne" href={"/uuid/" + parent.fields.uuid[0]} className="flex items-center gap-2 no-underline text-white">{parent.fields.label}<PiCaretRight className="text-primary-300" aria-hidden="true" /></Link>
+    return <aside className="bg-neutral-50 shadow-md !text-neutral-950 px-4 pb-4 pt-0 rounded-md">
+        <h2 className="!text-neutral-800 !uppercase !font-semibold !tracking-wider !text-sm !font-sans !m-0">Overordna søk</h2>
+        <h3 className="!m-0 !p-0 font-serif !text-xl !font-normal flex items-center gap-2">
+            {parent.fields.label}
         </h3>
         
-        <div className="text-sm text-neutral-50">
+        <div className="text-sm text-neutral-800">
             Basert på kjelder i desse datasetta:
             <ul>
                 {parent.fields.datasets?.map((dataset: string, index: number) => {
@@ -24,8 +24,9 @@ export default async function ServerParent({uuid}: {uuid: string}) {
         </div>
 
         <div className="flex justify-stretch gap-2 mt-4 w-full">
-            <Link href={"/search?dataset=search&doc=" + parent.fields.uuid[0]} className="btn btn-dark-outline">Vis i søket</Link>
-            <Link href={"/info/search"} className="btn btn-dark-outline">Les meir</Link>
+            <Link href={"/uuid/" + parent.fields.uuid[0]} className="btn btn-outline">Opne</Link>
+            <Link href={"/search?dataset=search&doc=" + parent.fields.uuid[0]} className="btn btn-outline">Vis i søket</Link>
+            <Link href={"/info/search"} className="btn btn-outline">Les meir</Link>
         </div>
     </aside>
 
