eb13ff736389d92760961b3c4bf2b05a7d78e799
Henrik Askjer <Henrik.Askjer@uib.no>
2025-02-19 09:39:11 +0100

    move reserved params to top of script

--- apps/sps-stadnamn-web/src/app/api/_utils/facets.ts
+++ apps/sps-stadnamn-web/src/app/api/_utils/facets.ts
@@ -1,6 +1,33 @@
 import { datasetTypes } from "@/config/metadata-config";
 
-export function extractFacets(request: Request ) {
+export const RESERVED_PARAMS = [
+  'q',
+  'display',
+  'dataset',
+  'page',
+  'asc',
+  'desc',
+  'fulltext',
+  'facetSort',
+  'fields',
+  'size',
+  'from',
+  'topLeftLat',
+  'topLeftLng',
+  'bottomRightLat',
+  'bottomRightLng',
+  'doc',
+  'sourceLabel',
+  'sourceDataset',
+  'facetSearch',
+  'totalHits',
+  'facets',
+  'zoom',
+  'point',
+  'datasetTag',
+] as const;
+
+export function extractFacets(request: Request) {
   const urlParams = new URL(request.url).searchParams;
 
   const termFilters = []
@@ -10,39 +37,14 @@ export function extractFacets(request: Request ) {
   const serverFacets: { [key: string]: string[] } = {};
 
   for (const [key, value] of urlParams.entries()) {
-    switch (key) {
-      case 'q':
-      case 'display': // Remove?
-      case 'dataset':
-      case 'page':
-      case 'asc':
-      case 'desc':
-      case 'fulltext':
-      case 'facetSort':
-      case 'fields':
-      case 'size':
-      case 'from':
-      case 'topLeftLat':
-      case 'topLeftLng':
-      case 'bottomRightLat':
-      case 'bottomRightLng':
-      case 'doc': // Excludes children when searching for markers at the same coordinates
-      case 'sourceLabel': // Filter sources
-      case 'sourceDataset': // Filter sources
-      case 'facetSearch':
-      case 'totalHits':
-      case 'facets':
-      case 'zoom':
-      case 'point':
-      case 'datasetTag':
-        filteredParams[key] = urlParams.get(key)!;
-        break;
-      default:
-        const facets = key == 'adm' ? clientFacets : serverFacets;
-        if (!facets[key]) {
-          facets[key] = [];
-        }
-        facets[key].push(value);
+    if (RESERVED_PARAMS.includes(key as any)) {
+      filteredParams[key] = urlParams.get(key)!;
+    } else {
+      const facets = key == 'adm' ? clientFacets : serverFacets;
+      if (!facets[key]) {
+        facets[key] = [];
+      }
+      facets[key].push(value);
     }
   }
 
