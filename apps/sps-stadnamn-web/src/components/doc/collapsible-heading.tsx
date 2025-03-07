aa551f98eddcd8a794af7e3449b4327cf4c7d8b6
Henrik Askjer <Henrik.Askjer@uib.no>
2025-03-07 12:47:32 +0100

    collapsible always expanded if not map view

--- apps/sps-stadnamn-web/src/components/doc/collapsible-heading.tsx
+++ apps/sps-stadnamn-web/src/components/doc/collapsible-heading.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { PiCaretDown, PiCaretUp } from "react-icons/pi";
+import { usePathname, useSearchParams } from "next/navigation";
 
 export default function CollapsibleHeading(props: {
   title: string;
@@ -9,8 +10,26 @@ export default function CollapsibleHeading(props: {
   alwaysOpen?: boolean;
   headingLevel?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 }) {
-  const [open, setOpen] = useState(props.alwaysOpen || false);
+  const searchParams = useSearchParams();
+  const pathname = usePathname()
+  const mode = searchParams.get('mode') || 'map'
+  // If the route is uuid
+  const landingPage = pathname.startsWith('/uuid/')
+  const alwaysOpen = landingPage || props.alwaysOpen || false
+
+  
+  const [open, setOpen] = useState(alwaysOpen);
   const HeadingTag = props.headingLevel || 'h3';
+
+  // If not in map mode, render a simple heading with content
+  if (alwaysOpen) {
+    return (
+      <div>
+        <HeadingTag className="!mt-0 !py-0">{props.title}</HeadingTag>
+        <div className="py-2">{props.children}</div>
+      </div>
+    );
+  }
 
   return <div className="">
     <HeadingTag className="!mt-0 !py-0"><button className="text-left w-full text-neutral-900 flex items-center gap-1" aria-expanded={open} aria-controls={props.title + '-collapsible'} onClick={() => setOpen(!open)}>
