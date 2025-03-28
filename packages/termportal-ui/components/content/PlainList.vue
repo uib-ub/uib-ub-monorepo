d3d8542a36ed4a453c123daadd2018c110e7b796
Jan Ole Bangen <Jan.Bangen@uib.no>
2025-03-28 19:38:33 +0100

    fix(tpw): improve content components

--- packages/termportal-ui/components/content/PlainList.vue
+++ packages/termportal-ui/components/content/PlainList.vue
@@ -1,15 +1,21 @@
 <template>
-  <ul class="plain-list">
+  <ul class="plain-list" :class="indent ? 'plain-list-indent' : ''">
     <ContentSlot :use="$slots.default" unwrap="p ul" />
   </ul>
 </template>
 
+<script setup lang="ts">
+const props = defineProps({
+  indent: { type: Boolean, default: false },
+});
+</script>
+
 <style>
-.plain-list ul {
+.plain-list {
   @apply space-y-1;
 }
 
-.plain-list-indent ul {
+.plain-list-indent {
   margin-left: 2em;
   @apply space-y-2;
 }
