01ba21bffeb4ab1bd52bfff62c9cf4d8a1e337bd
Henrik Askjer <Henrik.Askjer@uib.no>
2025-04-03 13:05:19 +0200

    add missing param

--- apps/sps-stadnamn-web/src/app/iiif/[[...slug]]/page.tsx
+++ apps/sps-stadnamn-web/src/app/iiif/[[...slug]]/page.tsx
@@ -38,7 +38,7 @@ export default async function IIIFPage({params}: {params: Promise<{slug: string[
             
                           
                 <div className={`w-full lg:col-span-4 !min-h-[40svh] ${manifest?.type == 'Manifest' ? 'max-h-[60svh] lg:max-h-full' : 'lg:max-h-[calc(100svh-3rem)]'} bg-neutral-200`}>
-                    {isImage && <ImageViewer canvases={manifest.canvases} manifestDataset={manifestDataset} />}
+                    {isImage && <ImageViewer canvases={manifest.canvases} manifestDataset={manifestDataset} manifestId={manifest.uuid}/>}
                     {manifest?.audio && <div className="flex flex-col gap-4 items-center justify-center h-full hidden lg:flex">
                         <h2 className="text-2xl text-neutral-900 font-semibold">{resolveLanguage(manifest.audio.label)}</h2>
                         
