9f5ec0a97a5c849a473496e4a2c19b485601341c
Henrik Askjer <Henrik.Askjer@uib.no>
2025-04-09 12:07:42 +0200

    Refactor IIIFExpandSummary component to improve summary display logic by showing the first 20 words instead of a fixed slice, and remove unnecessary conditional rendering.

--- apps/sps-stadnamn-web/src/app/iiif/[[...slug]]/iiif-expand-summary.tsx
+++ apps/sps-stadnamn-web/src/app/iiif/[[...slug]]/iiif-expand-summary.tsx
@@ -6,10 +6,11 @@ import { PiCaretDown, PiCaretUp } from "react-icons/pi"
 export default function IIIFExpandSummary({summary}: {summary: string}) {
     const [expanded, setExpanded] = useState(false)
 
-    if (summary.length > 150) {
+
     return <>
+        {expanded ? " " : "..."}
          <span className={`${expanded ? '' : 'hidden'} overflow-hidden transition-all duration-300`}>
-            {summary.slice(100)}
+            {summary.split(' ').slice(20).join(' ')}
         </span>
         <br/><button className="btn btn-outline btn-compact mt-2 gap-1 items-center pl-2" onClick={() => setExpanded(!expanded)}>
             {expanded ? <><PiCaretUp aria-hidden="true" className="" /> Vis mindre</> : <><PiCaretDown aria-hidden="true" className="" /> Vis meir</>}
@@ -19,8 +20,5 @@ export default function IIIFExpandSummary({summary}: {summary: string}) {
        
        
     </>
-    }
-    else {
-        return summary.slice(100)
-    }
+
 }
