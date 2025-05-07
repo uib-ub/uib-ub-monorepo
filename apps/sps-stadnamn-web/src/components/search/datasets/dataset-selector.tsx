e4825ba8086aec6b718ce6b44ab9062a0f8b197e
Henrik Askjer <Henrik.Askjer@uib.no>
2025-05-07 16:58:21 +0200

    tweak dataset selector

--- apps/sps-stadnamn-web/src/components/search/datasets/dataset-selector.tsx
+++ apps/sps-stadnamn-web/src/components/search/datasets/dataset-selector.tsx
@@ -1,11 +1,12 @@
 import { Fragment, useContext, useEffect, useState} from 'react';
 import { datasetTitles, datasetDescriptions, datasetShortDescriptions } from '@/config/metadata-config'
-import { PiCaretRight } from 'react-icons/pi';
+import { PiCaretRight, PiFunnel, PiFunnelFill, PiMagnifyingGlass } from 'react-icons/pi';
 import { useSearchParams } from 'next/navigation';
 import Link from 'next/link';
 import Clickable from '@/components/ui/clickable/clickable';
 import { useDataset } from '@/lib/search-params';
 import { GlobalContext } from '@/app/global-provider';
+import ClickableIcon from '@/components/ui/clickable/clickable-icon';
 
 
 
@@ -25,7 +26,7 @@ export default function DatasetSelector() {
   useEffect(() => {
     const titleMatch: string[] = []
     const contentMatch: string[] = []
-    const no_subindices = Object.keys(datasetTitles).filter(d => !d.includes("_") && d != dataset)
+    const no_subindices = Object.keys(datasetTitles).filter(d => !d.includes("_"))
     // Datasets match
     if (searchTerm?.length) {
 
@@ -49,11 +50,11 @@ export default function DatasetSelector() {
     return <>
         <div className='flex flex-col justify-between w-full px-2'>
           <div className='flex flex-col'>
-          <label htmlFor="titleSearch" className="sr-only">Søk i datasett</label>
+          <label htmlFor="titleSearch" className="sr-only">Finn datasett</label>
           <input
               id='titleSearch'
               autoComplete="off"
-              className='rounded-sm border border-gray-400 text-base px-2 py-1'
+              className='w-full border rounded-md border-neutral-300 p-1 px-2'
               type="text"
               value={searchTerm}
               onChange={handleSearchChange}
@@ -67,32 +68,50 @@ export default function DatasetSelector() {
           
           <ul className="flex flex-col w-full divide-y mt-4">
             {filteredDatasets
-              .filter((dataset, index) => showAll ? true : (index < 4 || filteredDatasets.length < 6))
-              .map((dataset, index) => (
-                <Fragment key={dataset}>
-                  <li className="flex w-full ">
-                    <Clickable link only={{dataset, q: searchParams.get('q'), nav: 'datasets' }} 
-                                className="w-full h-full py-2 px-2 md:px-2 hover:bg-neutral-50 no-underline">
-                      <strong className="font-semibold">{datasetTitles[dataset]}</strong>{" | "}
-                      
-                      {datasetShortDescriptions[dataset]}      
-                    </Clickable>
-                  </li>
-                  {filteredDatasets.length > 5 && index === 3 && !showAll && (
-                    <li className="w-full flex justify-center py-4">
-                      <button onClick={() => setShowAll(true)} className="rounded-full bg-neutral-100 font-semibold px-8 py-2 no-underline">
-                        Vis alle
-                      </button>
-                    </li>
-                  )}
-                  {index == filteredDatasets.length - 1 && showAll && (
-                    <li className="w-full flex justify-center py-4">
-                      <button onClick={() => setShowAll(false)} className="rounded-full bg-neutral-100 font-semibold px-8 py-2 no-underline">
-                        Vis færre
-                      </button>
-                    </li>
-                  )}
+              .filter(d => d != 'search' || !isMobile)
+              .map((itemDataset, index) => (
+                <Fragment key={itemDataset}>
+                  <li className="flex w-full">
+                    { dataset == 'search' ?
+                      <div className={`flex flex-col gap-6 p-4 w-full ${itemDataset == dataset ? 'bg-accent-50 border-l-4 border-accent-600' : ''}`}>
+                        <div className="flex-1 gap-2">
+                          {isMobile ? <h4 className="text-lg font-semibold text-neutral-900 mb-1">{datasetTitles[itemDataset]}</h4> : <h3 className="text-lg font-semibold text-neutral-900 mb-1">{datasetTitles[itemDataset]}</h3>}
+                          <p className="text-neutral-700">{datasetShortDescriptions[itemDataset]}</p>      
+                        </div>
+                        {itemDataset == dataset ? 
+                        <em className='text-neutral-700 ml-auto'>Gjeldande søkevisning</em>
+                        : <div className="flex flex-col xl:flex-row gap-2">
+                          <Clickable 
+                            className="btn btn-outline xl:btn-compact flex items-center gap-2 text-base !pl-2" 
+                            link 
+                            only={{dataset: itemDataset, q: searchParams.get('q'), nav: 'datasets'}} 
+                            label="Eiga søkevisning"
+                          >
+                            <PiMagnifyingGlass className="text-neutral-600 text-lg" aria-hidden="true"/>Søkevisning
+                          </Clickable>
 
+                          {itemDataset != 'all' && <Clickable 
+                            className="btn btn-outline xl:btn-compact flex items-center gap-2 text-base !pl-2" 
+                            link 
+                            add={{datasets: itemDataset, nav: 'filters', facet: 'datasets'}} 
+                          >
+                            <PiFunnel className="text-neutral-600 text-lg" aria-hidden="true"/>
+                            Bruk som filter
+
+                          </Clickable>}
+                        </div>}
+                      </div>
+                      :
+                      <Clickable 
+                        link 
+                        only={{dataset: itemDataset, q: searchParams.get('q'), nav: 'datasets' }} 
+                        className={`w-full h-full p-4  no-underline transition-colors duration-200 ${itemDataset == dataset ? 'bg-accent-50 border-l-4 border-accent-600' : 'hover:bg-neutral-50'}`}
+                      >
+                        {isMobile ? <h4 className="text-lg font-semibold text-neutral-900 mb-1">{datasetTitles[itemDataset]}</h4> : <h3 className="text-lg font-semibold text-neutral-900 mb-1">{datasetTitles[itemDataset]}</h3>}
+                        <p className="text-neutral-700">{datasetShortDescriptions[itemDataset]}</p>      
+                      </Clickable>
+                    }
+                  </li>
                 </Fragment>
               ))}
           </ul>
