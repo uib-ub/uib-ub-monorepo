498773d7f21fe678f1aecf08d2090916850ab712
Henrik Askjer <Henrik.Askjer@uib.no>
2025-05-09 14:47:37 +0200

    add margin below breadcrumbs

--- apps/sps-stadnamn-web/src/components/layout/breadcrumbs.tsx
+++ apps/sps-stadnamn-web/src/components/layout/breadcrumbs.tsx
@@ -8,7 +8,7 @@ export default function Breadcrumbs({ parentUrl, parentName, currentName, homeUr
     const parents = typeof parentName === 'string' ? [parentName] : parentName
     
     return (
-        <nav className="flex flex-wrap lg:flex-row gap-2 items-center  text-lg">
+        <nav className="flex flex-wrap lg:flex-row gap-2 items-center  text-lg mb-4">
             {homeUrl && <Fragment key="home"><IconLink label={homeLabel || "Hjem"} className="breadcrumb-link" href={homeUrl}><PiHouse className="w-4 h-4 self-center" /></IconLink><PiCaretRight className="w-4 h-4 self-center" /></Fragment>}
             { parents?.map((name, index) => (
                 <Fragment key={name}>
