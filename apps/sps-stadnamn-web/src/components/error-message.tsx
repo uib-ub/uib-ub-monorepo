a81064a98a710c10682de0f152182c9fd99c5052
Henrik Askjer <Henrik.Askjer@uib.no>
2025-05-27 13:01:42 +0200

    WIP: error message reusable in article

--- apps/sps-stadnamn-web/src/components/error-message.tsx
+++ apps/sps-stadnamn-web/src/components/error-message.tsx
@@ -1,7 +1,7 @@
 
-export default function ErrorMessage({ error, message }: { error: Record<string, string>, message: string}) {
+export default function ErrorMessage({ error, message, className }: { error: Record<string, string>, message: string, className?: string}) {
     return (
-        <div  role="status" aria-live="polite" className="space-y-2 m-8 md:m-16">
+        <div  role="status" aria-live="polite" className={className || "space-y-2 m-8 md:m-16"}>
                 <h2 className="text-3xl font-semibold">{error ? error.status : '500'}</h2>
                 <p className="text-xl">{message}</p>
                 { error && <p className="text-neutral-700 mt-4">{error.error}</p> }
