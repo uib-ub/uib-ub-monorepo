89fb9dc639b8974cf62347e66a020adef330c441
Tarje Lavik <tarje.lavik@gmail.com>
2025-06-11 15:01:43 +0200

    Enhance UBB extensions by adding HumanMadeObject and Person schemas
    - Updated linked_art.ts to include UBBHumanMadeObjectExtension and UBBPersonExtension in the import statements and schema definitions.
    - Merged new extensions into existing schemas for human-made objects and persons to improve data validation and structure.
    - Modified extension.ts to define new UBBHumanMadeObjectExtension and UBBPersonExtension schemas, enhancing the overall schema capabilities.

--- packages/types/src/ubb/zod/extension.ts
+++ packages/types/src/ubb/zod/extension.ts
@@ -1,4 +1,4 @@
-import { crmE55TypeSchema, crmE7ActivitySchema, crmP104IIsSubjectToSchema, crmP7TookPlaceAtSchema } from 'src/la/zod/linked_art';
+import { crmE55TypeSchema, crmE74GroupSchema, crmE7ActivitySchema, crmP104IIsSubjectToSchema, crmP7TookPlaceAtSchema, e21PersonSchema } from 'src/la/zod/linked_art';
 import { z } from "@hono/zod-openapi";
 
 export const langLabelSchema = z.record(z.string(), z.array(z.string()));
@@ -6,10 +6,19 @@ export const langLabelSchema = z.record(z.string(), z.array(z.string()));
 export const UBBClassExtension = z.object({
   _available: z.string().optional(),
   _modified: z.string(),
+  // _links: HalSchema,
+});
+
+export const UBBHumanMadeObjectExtension = z.object({
   subject_to: z.lazy(() => crmP104IIsSubjectToSchema),
   current_permanent_location: z.lazy(() => crmP7TookPlaceAtSchema).optional(),
   was_used_for: z.lazy(() => z.array(crmE7ActivitySchema)).optional(),
-  // _links: HalSchema,
+});
+
+export const UBBPersonExtension = z.object({
+  partner_in: z.lazy(() => z.array(UBBSocialRelationshipExtension)).optional(),
+  participated_in: z.lazy(() => z.array(crmE7ActivitySchema)).optional(),
+  current_or_former_member_of: z.lazy(() => z.array(crmE74GroupSchema)).optional(),
 });
 
 export const UBBTimeSpanExtension = z.object({
@@ -22,4 +31,11 @@ export const UBBRightExtension = z.object({
 
 export const UBBPlaceExtension = z.object({
   defined_by_geojson: z.string().optional(),
+})
+
+export const UBBSocialRelationshipExtension = z.object({
+  type: z.string(),
+  _label: z.string(),
+  classified_as: z.lazy(() => z.array(crmE55TypeSchema)),
+  involves_partner: z.lazy(() => z.array(e21PersonSchema)).optional(),
 })
