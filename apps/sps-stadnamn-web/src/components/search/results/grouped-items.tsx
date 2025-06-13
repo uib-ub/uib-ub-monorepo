22c8bb984270c3a69834604562a0dc7076f4a765
Henrik Askjer <Henrik.Askjer@uib.no>
2025-06-13 11:33:58 +0200

    group by h3 and gnidu

--- apps/sps-stadnamn-web/src/components/search/results/grouped-items.tsx
+++ apps/sps-stadnamn-web/src/components/search/results/grouped-items.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react"
 import { PiCaretDown, PiCaretUp } from "react-icons/pi"
 import ResultItem from "./result-item"
-import { getSkeletonLength } from "@/lib/utils"
+import { gridDisk, gridDistance } from "h3-js"
 
 
 
@@ -20,10 +20,77 @@ export default function GroupedItems({ resultData }: {resultData: any[]}) {
       return newSet
     })
   }
+  
+  
+  
+
+
+  // First, build canonical h3 mapping
+  const canonicalMap: Record<string, Record<string, string>> = {};
+  const gnidu2h3: Record<string, Record<string, string>> = {};
+  
+  resultData?.forEach(hit => {
+    if (!hit || !hit.fields.h3) return;
+    
+    const label = hit.fields.label?.[0] ?? '';
+
+    const h3 = hit.fields.h3[0];
+    const gniduArray = hit.fields.gnidu || []
+    if (!canonicalMap[h3]) {
+      canonicalMap[h3] = {};
+    }
+    gniduArray.forEach((g: string) => {
+      if (!gnidu2h3[g]) {
+        gnidu2h3[g] = {};
+      }
+    })
+
+
+    
+
+    const neighbours = gridDisk(h3, 1);
+
+    const existingH3 = neighbours.find(neighbour => 
+      canonicalMap[neighbour]?.[label]
+    );
+
+    const existingGnidu = gniduArray.find((g: string) => gnidu2h3[g]?.[label])
+
+
+    if (existingGnidu) {
+        console.log("existingGnidu", existingGnidu)
+      canonicalMap[h3][label] = existingGnidu;
+    } else if (existingH3) {
+      canonicalMap[h3][label] = existingH3;
+    } else {
+      canonicalMap[h3][label] = h3;
+      gniduArray.forEach((g: string) => {
+        gnidu2h3[g][label] = h3
+      })
+      neighbours.forEach(neighbour => {
+        if (!canonicalMap[neighbour]) {
+          canonicalMap[neighbour] = {};
+        }
+        canonicalMap[neighbour][label] = h3;
+      });
+    }
+    
+  });
 
   const groupedResults: Record<string, any[]> = resultData?.reduce((acc, hit) => {
     if (!hit) return acc;
-    const groupKey = `${hit.fields.label ?? ''}-${hit.fields.gnidu ? `GNIDU-${hit.fields.gnidu}` : hit.fields.uuid}`;
+    
+    const label = hit.fields.label ?? '';
+    let groupKey;
+    
+    if (hit.fields.h3) {
+
+      const h3 = canonicalMap[hit.fields.h3]?.[label] || hit.fields.h3;
+      groupKey = `${label}-H3-${h3}`;
+    } else {
+      groupKey = `${label}-${hit.fields.uuid}`;
+    }
+    
     if (!acc[groupKey]) {
       acc[groupKey] = [];
     }
