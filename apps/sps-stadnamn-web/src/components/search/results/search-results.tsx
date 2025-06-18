8f4261a18150d6717e09770b0bbcd3c716e412af
Henrik Askjer <Henrik.Askjer@uib.no>
2025-06-18 09:02:03 +0200

    use collapse

--- apps/sps-stadnamn-web/src/components/search/results/search-results.tsx
+++ apps/sps-stadnamn-web/src/components/search/results/search-results.tsx
@@ -8,127 +8,87 @@ import ResultItem from "./result-item";
 import { getSkeletonLength, trimResultData } from "@/lib/utils";
 import GroupedItems from "./grouped-items"
 import { useSearchQuery } from "@/lib/search-params";
+import { useRouter } from "next/navigation";
 
 
-const PER_PAGE = 100
+const PER_PAGE = 20
 
 export default function SearchResults() {
     const { resultData, totalHits, isLoading, searchError } = useContext(SearchContext)
 
-    const [resultGroups, setResultGroups] = useState<any[]>([])
-    const [loadingAdditionalResults, setLoadingAdditionalResults] = useState(false)
-    const [offset, setOffset] = useState(0)
+    const [collapsedResults, setCollapsedResults] = useState<any[]>([])
+    const [isLoadingResults, setIsLoadingResults] = useState(false)
+    const searchParams = useSearchParams()
+    const page = searchParams.get('page')
+    const router = useRouter()
     const {searchQueryString } = useSearchQuery()
 
 
     // Reset when search query changes
     useEffect(() => {
-      setResultGroups([])
-      setOffset(0)
+      setCollapsedResults([])
+      const newUrl = new URLSearchParams(searchParams)
+      newUrl.set('page', '0')
+      router.push(`?${	newUrl.toString()}`);
+
     }, [searchQueryString])
-
-    const loadMore = () => {
-      setOffset(resultData.length + resultGroups.reduce((acc, curr) => acc + curr.length, 0))
-    }
-
-
-
 
 
 
     useEffect(() => {
       //if (offset === 0) return;
-      setLoadingAdditionalResults(true)
-      const url = `/api/search/map?${searchQueryString}&size=${PER_PAGE}&from=${offset}`
+      setIsLoadingResults(true)
+      const url = `/api/search/collapsed?${searchQueryString}&size=${collapsedResults.length == 0 && page ? PER_PAGE * (parseInt(page) + 1): PER_PAGE}&from=${collapsedResults.length || 0}`
       fetch(url)
         .then(response => {
           if (!response.ok) {
-            setLoadingAdditionalResults(false)
+            setIsLoadingResults(false)
             throw response
           }
           return response.json()
         })
         .then(es_data => {
-          setResultGroups(prev => [...prev, trimResultData(es_data.hits.hits, es_data.hits.total.value)])
+          // Filter out any duplicates based on _id before updating state
+          const newHits = es_data.hits.hits.filter(
+            (newHit: any) => !collapsedResults.some((existingHit: any) => existingHit._id === newHit._id)
+          )
+          setCollapsedResults(prev => [...prev, ...newHits])
         })
-        .finally(() => setLoadingAdditionalResults(false))
-    }, [searchQueryString, offset, totalHits])
+        .finally(() => setIsLoadingResults(false))
+    }, [searchQueryString, page])
 
     return (
       <>
         <ul id="result_list" className='flex flex-col mb-2 divide-y divide-neutral-200'>
-          {/* Loading states */}
-          {isLoading ? Array.from({length: 10}).map((_, i) => (
+
+          {Array.from({length: 1 + Math.min(PER_PAGE + (parseInt(page || '0') * PER_PAGE), totalHits?.value)}).map((_, i) => {
+            if (isLoadingResults) {
+              return (
             <li className="h-14 flex flex-col mx-2 flex-grow justify-center gap-1" key={i}>
               <div className="bg-neutral-200 rounded-full h-4 animate-pulse" style={{width: `${getSkeletonLength(i, 4, 10)}rem`}}></div>
               <div className="bg-neutral-200 rounded-full h-4 animate-pulse" style={{width: `${getSkeletonLength(i, 10, 16)}rem`}}></div>
             </li>
-          ))
-          : totalHits?.value ? (
-            <>
-              {/* Only use GroupedItems if total results > 20, otherwise use ResultItem directly */}
-              {totalHits.value > 10 ? (
-
-
-                  <>
-
-                  { resultGroups.length > 0 && Array.from({length: resultGroups.length + (resultData.length + resultGroups.reduce((acc, curr) => acc + curr.length, 0) < totalHits?.value ? 1 : 0)}).map((_, i: number) => {
-                    if (i < resultGroups.length) {
-                      return (
-                    <GroupedItems 
-                    resultData={resultGroups[i]}
-
-                    key={i}
-                    />
-                    )
-                  } else if (loadingAdditionalResults) {
-                    return (
-                    <div className="" key={i}
-                          aria-label="Laster resultater"
-                          tabIndex={0}
-                    
-                    >
-                      {Array.from({length: 50}).map((_, j) => (
-                        <div
-                          
-                          key={j}
-                          className="h-14 flex flex-col mx-2 flex-grow justify-center gap-1"
-                        >
-                          <div className="bg-neutral-200 rounded-full h-4 animate-pulse" style={{width: `${getSkeletonLength(j, 4, 10)}rem`}}></div>
-                          <div className="bg-neutral-200 rounded-full h-4 animate-pulse" style={{width: `${getSkeletonLength(j, 10, 16)}rem`}}></div>
-                        </div>
-                      ))}
-                    </div>
-                  )
-                  } else {
-                    return (
-                      <li className="p-4" key={i}>
-                        <button
-                          onClick={loadMore}
-                          className="bg-neutral-100 p-4 rounded-full w-full block"
-                          key={i}
-                        >
-                          Last fleire resultat
-                        </button>
-                      </li>
-                    )
-                  }
-                })}
-
-                  
-
-                </>
-        
-
-                ) : (
-                <>
-                  {resultGroups[0]?.map((hit: any) => (
-                    <ResultItem key={hit._id} hit={hit} />
-                  ))}
-                </>
-              )}
-            </>
-          ) : null}
+            )
+            } else if (i < collapsedResults.length) {
+              return <ResultItem key={collapsedResults[i]._id} hit={collapsedResults[i]} />
+            } else if (i === collapsedResults.length && collapsedResults.reduce((acc, curr) => acc + (curr.inner_hits?.gnidu?.hits?.total?.value || 0), 0) < totalHits?.value) {
+              return <button 
+                type="button" 
+                onClick={(e) => {
+                  e.preventDefault();
+                  const newUrl = new URLSearchParams(searchParams)
+                  newUrl.set('page', (parseInt(page || '0') + 1).toString())
+                  router.push(`?${	newUrl.toString()}`);
+                }} 
+                key={i} 
+                className="bg-neutral-100 p-4 rounded-full w-full block"
+              >
+                Last fleire resultat
+              </button>
+            }
+          })}
+          
+            
         </ul>
         
         {searchError ? (
