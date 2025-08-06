00a6741d32647f0effdf1102a34b91feb9f2c0d0
Tarje Lavik <tarje.lavik@gmail.com>
2025-08-06 19:44:18 +0200

    Refactor ESLint configuration for sps-termportal-admin and sps-termportal-web; replace .eslintrc.js with eslint.config.mjs, update linting rules, and add @nuxt/eslint dependency. Upgrade ESLint to version 9.0.0 and streamline linting commands in package.json.

--- packages/eslint-config-custom/nuxt.js
+++ packages/eslint-config-custom/nuxt.js
@@ -1,22 +1,26 @@
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
 import { config as baseConfig } from "./index.js";
 
-/** @type {import("eslint").Linter.FlatConfig[]} */
-export default tseslint.config(
-  ...baseConfig,
-  {
-    files: ["**/*.{js,ts,vue}"],
-    languageOptions: {
-      parser: (await import("@nuxtjs/eslint-config-typescript")).default,
-    },
-    rules: {
-      "@typescript-eslint/no-unused-vars": ["off"],
-      "tsdoc/syntax": "warn",
-      "import/default": "warn",
-    },
-    plugins: {
-      "simple-import-sort": (await import("eslint-plugin-simple-import-sort")).default,
-    },
-  },
-);
+export default createConfigForNuxt({
+  features: {
+    stylistic: {
+      semi: false,
+      indent: 2,
+      quotes: 'single',
+    }
+  }
+})
+  .append(
+    ...baseConfig,
+    {
+      files: ["**/*.{js,ts,vue}"],
+      rules: {
+        "@typescript-eslint/no-unused-vars": ["off"],
+        "tsdoc/syntax": "warn",
+        "import/default": "warn",
+      },
+      plugins: {
+        "simple-import-sort": (await import("eslint-plugin-simple-import-sort")).default,
+      },
+    }
+  )
