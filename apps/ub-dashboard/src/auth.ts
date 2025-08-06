5858c6716d9b72f21be1a168c236d2fb5e4eaa44
Tarje Lavik <tarje.lavik@gmail.com>
2025-08-06 12:39:12 +0200

    Update next-auth

---
+++ apps/ub-dashboard/src/auth.ts
@@ -0,0 +1,23 @@
+import NextAuth from "next-auth"
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    {
+      id: 'dataporten',
+      name: 'DATAPORTEN',
+      type: 'oauth',
+      wellKnown: 'https://auth.dataporten.no/.well-known/openid-configuration',
+      token: true,
+      clientId: process.env.DATAPORTEN_ID,
+      clientSecret: process.env.DATAPORTEN_SECRET,
+      profile: (profile: any) => {
+        return {
+          ...profile,
+          id: profile.sub,
+          image: profile.picture,
+          email: profile.email,
+        };
+      },
+    },
+  ]
+})
