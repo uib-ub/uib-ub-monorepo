c2c2ff13f0878c51dc606909c4c74560dd6ffac3
Henrik Askjer <Henrik.Askjer@uib.no>
2025-08-07 17:30:04 +0200

    tweak status section

--- apps/sps-stadnamn-web/src/components/search/status-section.tsx
+++ apps/sps-stadnamn-web/src/components/search/status-section.tsx
@@ -2,27 +2,33 @@
 import { useContext } from 'react';
 import { SearchContext } from '@/app/search-provider';
 import ActiveFilters from './form/active-filters';
-import { PiInfoFill, PiWarningFill, PiXBold } from 'react-icons/pi';
+import { PiInfoFill, PiMagnifyingGlass, PiWarningFill, PiX, PiXBold } from 'react-icons/pi';
 import ModeSelector from '../tabs/mode-selector';
 import { GlobalContext } from '@/app/global-provider';
 import SortSelector from './sort/sort-selector'
-import { useMode } from '@/lib/search-params';
+import { useMode, useSearchQuery } from '@/lib/search-params';
+import { useRouter, useSearchParams } from 'next/navigation';
+import Clickable from '../ui/clickable/clickable';
 
 export default function StatusSection() {
     const { resultBounds, isLoading, coordinatesError, searchError, setSearchError, totalHits } = useContext(SearchContext)
     const mode = useMode()
     const { isMobile } = useContext(GlobalContext)
+    const searchParams = useSearchParams()
+    const nav = searchParams.get('nav')
+    const { facetFilters, datasetFilters } = useSearchQuery()
 
     return <div className="flex flex-col gap-2"> 
     <div className={`flex gap-1 flex-wrap w-fit ${mode != 'map' ? 'items-center' : ''} ${(mode == 'map' && !isMobile) ? 'lg:mt-2' : ''}`}>
     <ModeSelector/>
     
-
-    { (mode != 'doc' && !isMobile) && <div className="flex flex-wrap xl:flex-row h-full p-2 xl:p-1 gap-1"><ActiveFilters/> </div> }
+    { (mode != 'doc' && !isMobile) && <div className="flex flex-wrap xl:flex-row h-full p-1 xl:py-0 gap-2"><ActiveFilters showQuery={true} showFacets={true} showDatasets={true}/> </div> }
     </div>
     {mode == 'list' && <div className="flex flex-wrap xl:flex-row h-full p-2 px-6 gap-1"><SortSelector/></div>}
-    { (mode == 'map' && !isLoading && !resultBounds?.length && !searchError && totalHits?.value > 0) ? <div role="status" aria-live="polite" className="bg-neutral-900 rounded-md p-4 text-white opacity-90 flex gap-2 items-center w-fit"><PiInfoFill className="inline text-xl"/> Ingen treff med koordinatar</div> : null}
-    { (mode == 'map' && !isLoading && !searchError && totalHits?.value == 0) ? <div role="status" aria-live="polite" className="bg-neutral-900 rounded-md p-4 text-white opacity-90 flex gap-2 items-center w-fit"><PiInfoFill className="inline text-xl"/> Ingen treff</div> : null}
+    {mode == 'map' && <div className="flex flex-wrap gap-2 mx-1.5 xl:mx-0">
+    { (!isLoading && !resultBounds?.length && !searchError && totalHits?.value > 0) ? <div role="status" aria-live="polite" className="bg-neutral-900 rounded-md p-4 text-white opacity-90 flex gap-2 items-center w-fit"><PiInfoFill className="inline text-xl"/> Ingen treff med koordinatar</div> : null}
+    { ( !isLoading && !searchError && totalHits?.value == 0) ? <div role="status" aria-live="polite" className="bg-neutral-900 rounded-md p-4 text-white opacity-90 flex gap-2 items-center w-fit"><PiInfoFill className="inline text-xl"/> Ingen treff</div> : null}
+    </div>}
     { searchError && <div role="status" aria-live="polite" className="bg-primary-700 rounded-md p-4 text-white opacity-90 flex gap-4 items-center w-fit">
         <PiWarningFill className="inline text-xl"/> 
         <span>Kunne ikkje hente søkeresultat</span>
