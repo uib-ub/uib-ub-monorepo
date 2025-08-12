1c065a248979a11c4f1a8a9c6bb027d43939c7f6
Jan Ole Bangen <Jan.Bangen@uib.no>
2025-08-12 14:32:16 +0200

    fix(tpw): use await in plugin

--- apps/sps-termportal-web/plugins/bootstrapData.ts
+++ apps/sps-termportal-web/plugins/bootstrapData.ts
@@ -1,12 +1,12 @@
 import { useBootstrapData } from "~/composables/states";
 
-export default defineNuxtPlugin((_) => {
+export default defineNuxtPlugin(async (_) => {
   /*
   Get labels, data and relations that are maintained in the CMS.
   */
   const bootstrapData = useBootstrapData();
 
-  useAsyncData("fetchBoostrapData", () =>
+  await useAsyncData("fetchBoostrapData", () =>
     $fetch<BootstrapData>("/api/bootstrap", {
       headers: import.meta.server
         ? { cookie: "session=" + useRuntimeConfig().apiKey }
