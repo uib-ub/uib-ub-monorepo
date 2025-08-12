220d651f405ed6bb4817b6bd381e5c7973cf9e39
henrikaskj <henrik.askjer@uib.no>
2025-08-12 12:52:59 +0200

    Update robots.ts

--- apps/sps-stadnamn-web/src/app/robots.ts
+++ apps/sps-stadnamn-web/src/app/robots.ts
@@ -4,8 +4,8 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: '/', '/view/*/info',
       disallow: ['/api', '/view/*'],
+      allow: ['/', '/view/*/info'],
     }
   }
 }
