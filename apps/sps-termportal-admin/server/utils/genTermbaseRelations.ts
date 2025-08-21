96ba065f047f83043d8c4a739390e0250bc9d386
Jan Ole Bangen <Jan.Bangen@uib.no>
2025-08-21 14:46:15 +0200

    feat(tpa): improve relations query

--- apps/sps-termportal-admin/server/utils/genTermbaseRelations.ts
+++ apps/sps-termportal-admin/server/utils/genTermbaseRelations.ts
@@ -7,12 +7,25 @@ export default function (termbase: string) {
   SELECT ?concept1 ?published1 ?p ?concept2 ?published2
   WHERE {
     GRAPH ns:${termbase} {
-      ?concept1 skosp:memberOf ?tb .
+      ?concept1 a skos:Concept .
+      ?concept2 a skos:Concept .
+      ?concept1 ?p ?concept2 .
+      FILTER(?p IN (
+        skos:semanticRelation,
+        skos:broader,
+        skos:narrower,
+        skos:related,
+        xkos:specializes,
+        xkos:generalizes,
+        xkos:isPartOf,
+        xkos:hasPart,
+        rdfs:seeAlso
+        )
+    )
+
       OPTIONAL {
         ?concept1 skosp:publisere ?published1 .
       }
-      ?concept1 ?p ?concept2 .
-      ?concept2 skosp:memberOf ?tb .
       OPTIONAL {
         ?concept2 skosp:publisere ?published2 .
       }
