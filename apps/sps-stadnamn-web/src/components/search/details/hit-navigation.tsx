ca5e5c8ea0f85b6ccda777d0fd4b9f9942d7d130
Henrik Askjer <Henrik.Askjer@uib.no>
2025-08-26 11:06:04 +0200

    show correct doc index

--- apps/sps-stadnamn-web/src/components/search/details/hit-navigation.tsx
+++ apps/sps-stadnamn-web/src/components/search/details/hit-navigation.tsx
@@ -22,7 +22,7 @@ export default function HitNavigation({docIndex, prevDocUuid, nextDocUuid}: {doc
         {isMobile ? <PiCaretDoubleLeft className="text-xl" aria-hidden="true"/> : <PiCaretLeftBold className="xl:text-xl text-primary-600" aria-hidden="true"/>}
       </ClickableIcon>
       <span className="text-neutral-800 border-y border-neutral-200 shadow-sm h-8 xl:h-10 flex items-center self-center xl:min-w-12 text-center px-4">       
-        {docIndex}/{groupTotal?.value}</span>
+        {docIndex + 1}/{groupTotal?.value}</span>
       <ClickableIcon 
         label="Neste (shift + høgre piltast)" 
         className={`btn btn-outline btn-compact rounded-l-none ${isMobile ? 'rounded-full' : ''}`} 
