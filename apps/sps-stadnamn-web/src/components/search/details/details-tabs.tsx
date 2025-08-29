154d21e9a9c59d2b41c5de378d9074e6983ca7a0
Henrik Askjer <Henrik.Askjer@uib.no>
2025-08-29 17:14:42 +0200

    feat: Update icon usage in DetailsTabs for improved visual consistency

--- apps/sps-stadnamn-web/src/components/search/details/details-tabs.tsx
+++ apps/sps-stadnamn-web/src/components/search/details/details-tabs.tsx
@@ -2,7 +2,7 @@ import Clickable from "@/components/ui/clickable/clickable";
 import useDocData from "@/state/hooks/doc-data";
 import useGroupData from "@/state/hooks/group-data";
 import { useSearchParams } from "next/navigation";
-import { PiBookOpenFill, PiBookOpenLight, PiCardsFill, PiCardsLight, PiCaretLeftBold, PiListBullets, PiListBulletsLight, PiListLight } from "react-icons/pi";
+import { PiBookOpenFill, PiBookOpenLight, PiCardsFill, PiCardsLight, PiCaretLeftBold, PiListBullets, PiListBulletsFill, PiListBulletsLight, PiListLight } from "react-icons/pi";
 
 
 export default function DetailsTabs() {
@@ -19,7 +19,7 @@ export default function DetailsTabs() {
         add={{ details: "doc" }}
         aria-selected={details == "doc" || (details == "group" && !groupData)}
         className="flex h-10 whitespace-nowrap items-center basis-1 gap-2 no-underline w-full lg:w-auto p-1 pr-4 pl-3 text-neutral-900 aria-selected:bg-neutral-100 aria-selected:shadow-inner">
-        {details == "doc" ? <PiCardsFill className="text-accent-800" aria-hidden="true" /> : <PiCardsLight className="text-accent-900" aria-hidden="true" />}
+        {details == "doc" ? <PiCardsFill className="text-accent-800" aria-hidden="true" /> : <PiCardsLight className="text-neutral-900" aria-hidden="true" />}
         <span className="text-neutral-900 sr-only 2xl:not-sr-only whitespace-nowrap">Paginering</span>
       </Clickable>
 
@@ -28,7 +28,7 @@ export default function DetailsTabs() {
         add={{ details: "group" }}
         aria-selected={details == "group"}
         className="flex whitespace-nowrap group relative items-center basis-1 gap-2 no-underline w-full lg:w-auto p-1 px-3 aria-selected:bg-neutral-100 aria-selected:text-neutral-900 aria-selected:shadow-inner">
-        {details == "group" ? <PiCardsFill className="text-accent-800" aria-hidden="true" /> : <PiCardsLight className="text-accent-900" aria-hidden="true" />}
+        {details == "group" ? <PiListBulletsFill className="text-accent-800" aria-hidden="true" /> : <PiListBullets className="text-neutral-900" aria-hidden="true" />}
         <span className="text-neutral-900 hidden xl:flex flex-nowrap whitespace-nowrap">Oversikt</span>
 
       </Clickable>}
