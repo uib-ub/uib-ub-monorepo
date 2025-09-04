2cfbb92daa1221034fb64ab670237579a475f602
Henrik Askjer <Henrik.Askjer@uib.no>
2025-09-04 12:27:28 +0200

    contrast

--- apps/sps-stadnamn-web/src/components/search/details/doc/doc-toolbar.tsx
+++ apps/sps-stadnamn-web/src/components/search/details/doc/doc-toolbar.tsx
@@ -8,7 +8,7 @@ export default function DocToolbar( {docData}: {docData: any} ) {
 
     return <div className="flex h-10">
   <IconLink label="Opne permanent infoside" href={`${process.env.NODE_ENV == 'development' ? '': 'https://purl.org/stadnamn'}/uuid/${docData?._source?.uuid}`} className="btn btn-outline border-r-0 rounded-r-none btn-compact flex items-center gap-2">
-      <PiLinkSimple aria-hidden="true" className="text-lg text-neutral-800"/>
+      <PiLinkSimple aria-hidden="true" className="text-lg"/>
     </IconLink>    
     <CopyLink uuid={docData?._source?.uuid} isIconButton={true} className="btn btn-outline rounded-l-none btn-compact border-r"/> 
     
