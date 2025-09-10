ed69e2c78fde15227ff634986da32f93bd0fcdc3
Jan Ole Bangen <Jan.Bangen@uib.no>
2025-09-10 09:13:15 +0200

    feat(tpw): add tooltip component

---
+++ apps/sps-termportal-web/components/content/ToolTip.vue
@@ -0,0 +1,30 @@
+<template>
+  <HoverCard>
+    <HoverCardTrigger
+      class="underline hover:decoration-2"
+      tabindex="0"
+    >
+      {{ label }}
+    </HoverCardTrigger>
+    <HoverCardContent>
+      <div class="font-semibold mb-2">
+        {{ title }}
+      </div>
+
+      {{ contentId }}
+    </HoverCardContent>
+  </HoverCard>
+</template>
+
+<script setup lang="ts">
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+
+defineProps<{
+  label: string;
+  title: string;
+  contentId: string; }>();
+</script>
