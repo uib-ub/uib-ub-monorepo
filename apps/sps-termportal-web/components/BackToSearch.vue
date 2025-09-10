9171bbd30a326254fe3e3fdd45eb6b2db590249b
Jan Ole Bangen <Jan.Bangen@uib.no>
2025-09-10 18:55:26 +0200

    fix(tpw): display of back to search icon

--- apps/sps-termportal-web/components/BackToSearch.vue
+++ apps/sps-termportal-web/components/BackToSearch.vue
@@ -4,12 +4,13 @@
       class="group flex items-center space-x-2 text-lg"
       to="/search"
     >
-      <Icon
-        name="ion:return-up-back-sharp"
-        size="1.7em"
-        aria-hidden="true"
-        class="h-7 w-12 rounded bg-tpblue-400 text-white group-hover:bg-blue-700"
-      />
+      <div class="bg-tpblue-400 group-hover:bg-blue-800 rounded h-7 w-10 flex items-center justify-center text-white">
+        <Icon
+          name="ion:return-up-back-sharp"
+          size="1.6em"
+          aria-hidden="true"
+        />
+      </div>
       <div class="text-gray-500 group-hover:text-black group-hover:underline">
         {{ $t("id.tilbake") }}
       </div>
