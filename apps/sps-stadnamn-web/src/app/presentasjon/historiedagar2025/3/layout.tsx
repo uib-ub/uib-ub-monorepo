1df0fc6ac2691a3f719938858dde6f98ecf7bd8b
Henrik Askjer <Henrik.Askjer@uib.no>
2025-09-17 15:49:17 +0200

    remove presentation

--- apps/sps-stadnamn-web/src/app/presentasjon/historiedagar2025/3/layout.tsx
+++
@@ -1,9 +0,0 @@
-import SlideLayout from '../slide-layout'
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <SlideLayout slideNumber={3}>
-      {children}
-    </SlideLayout>
-  )
-}
