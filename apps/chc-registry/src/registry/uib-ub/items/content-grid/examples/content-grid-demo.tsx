f82b475de82a2c810d70fc31a9a02c2e56c22ec8
Tarje Lavik <tarje.lavik@gmail.com>
2025-09-24 19:51:38 +0200

    fix(chc-registry): update content grid structure and global styles
    - Changed the CSS selector for grid display from 'p:first-child' to '.content' for better styling consistency.
    - Refactored the ContentGridDemo component to use semantic HTML elements, replacing 'div' with 'figure' and 'figcaption' for improved accessibility and structure.

--- apps/chc-registry/src/registry/uib-ub/items/content-grid/examples/content-grid-demo.tsx
+++ apps/chc-registry/src/registry/uib-ub/items/content-grid/examples/content-grid-demo.tsx
@@ -13,12 +13,14 @@ export default function ContentGridDemo() {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
 
-          <div className='feature'>
-            <Skeleton className='img h-96 border rounded-md p-3'>Figure</Skeleton>
-            <Skeleton className='caption rounded-md border p-3'>
-              Caption
-            </Skeleton>
-          </div>
+          <figure>
+            <Skeleton className='content h-96 border rounded-md p-3'>Figure</Skeleton>
+            <figcaption>
+              <Skeleton className='caption rounded-md border p-3'>
+                Caption
+              </Skeleton>
+            </figcaption>
+          </figure>
 
           <h3 className='font-bold'>Content Grid</h3>
           <p>
