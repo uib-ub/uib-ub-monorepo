f03ce634cd6f19c642186b626e463ebdcdea096d
Henrik Askjer <Henrik.Askjer@uib.no>
2025-10-27 09:56:30 +0100

    revert debugging

--- apps/sps-stadnamn-web/src/components/map/map-toolbar.tsx
+++ apps/sps-stadnamn-web/src/components/map/map-toolbar.tsx
@@ -36,7 +36,7 @@ export default function MapToolbar() {
                 <div
                     role="status"
                     aria-live="polite"
-                    className="bg-neutral-900 rounded-md h-12 px-4 text-white opacity-90 flex gap-2 items-center w-fit absolute left-1 z-[5001] transition-opacity duration-300"
+                    className="bg-neutral-900 rounded-md h-12 px-4 text-white opacity-90 flex gap-2 items-center w-fit absolute left-1 z-[3001] transition-opacity duration-300"
                     style={{
                         top: isMobile ? 
                             currentPosition <= MAP_DRAWER_BOTTOM_HEIGHT_REM ? "4rem" : 
@@ -52,7 +52,7 @@ export default function MapToolbar() {
             
         }
         <div
-            className="flex gap-2 flex-col xl:flex-row absolute right-3 xl:right-[calc(25svw+1.5rem)] z-[9000]"
+            className="flex gap-2 flex-col xl:flex-row absolute right-3 xl:right-[calc(25svw+1.5rem)] z-[5000]"
             style={{
                 top: isMobile ?  currentPosition <= MAP_DRAWER_BOTTOM_HEIGHT_REM ? "4rem" : `${4-currentPosition + MAP_DRAWER_BOTTOM_HEIGHT_REM}rem` : "0.5rem",
             }}
