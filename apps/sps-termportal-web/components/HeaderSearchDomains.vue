a1a032d2473e84a8f00f74ed3f1dfc0fe4495e15
Jan Ole Bangen <jobangen@gmail.com>
2025-10-30 15:20:46 +0100

    fix(tpw): streamline icon placment, a11y for icons

--- apps/sps-termportal-web/components/HeaderSearchDomains.vue
+++ apps/sps-termportal-web/components/HeaderSearchDomains.vue
@@ -56,16 +56,14 @@
       <div class="flex justify-center">
         <button
           v-if="domainSelected"
-          class="absolute mt-[6px] flex h-[1.1em] w-16 justify-center rounded-b-md border-2 border-gray-200 border-t-white bg-white"
+          class="cursor-pointer absolute mt-[6px] flex h-[1.1em] w-16 justify-center rounded-b-md border-2 border-gray-200 border-t-white bg-white"
+          :aria-label="$t('searchBar.expandDomainMenu')"
           @click="panel = !panel"
         >
-          <Icon
-            name="mdi:chevron-down"
+          <IconChevronDown
             size="1.6em"
-            class="mt-[-7px] text-gray-600"
-            aria-hidden="true"
+            class="-translate-y-1.5"
           />
-          <span class="sr-only">{{ $t("searchBar.expandDomainMenu") }}</span>
         </button>
       </div>
     </div>
@@ -78,23 +76,18 @@
         <div class="absolute right-0 top-0 mr-1 mt-1 flex space-x-2">
           <button
             v-if="subdomainSpecified"
-            class="rounded-sm border border-transparent p-0.5 text-gray-600 hover:border-gray-300 hover:bg-gray-100 hover:text-gray-800"
+            class="cursor-pointer rounded-xs border border-transparent p-0.5 text-gray-600 hover:border-gray-300 hover:bg-gray-100 hover:text-gray-800"
+            :aria-label="$t('searchBar.resetDomainOptions')"
             @click="resetSubdomainOptions()"
           >
-            <IconReset
-              class="text-lg"
-              size="1.35em"
-            />
-            <span class="sr-only">{{
-              $t("searchBar.resetDomainOptions")
-            }}</span>
+            <IconReset size="1.3em" />
           </button>
           <button
-            class="flex justify-center rounded-sm border border-transparent text-gray-600 hover:border-gray-300 hover:bg-gray-100 hover:text-gray-800"
+            class="cursor-pointer flex justify-center rounded-xs border border-transparent text-gray-600 hover:border-gray-300 hover:bg-gray-100 hover:text-gray-800"
+            :aria-label="$t('searchBar.closeDomainMenu')"
             @click="panel = false"
           >
-            <IconClose class="text-lg" />
-            <span class="sr-only">Close</span>
+            <IconClose class="translate-y-[1px]" />
           </button>
         </div>
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
---
+++ packages/termportal-ui/components/icon/IconReset.vue
@@ -0,0 +1,15 @@
+<template>
+  <Icon
+    name="ri:reset-right-fill"
+    :size="size"
+    aria-hidden="true"
+  />
+</template>
+
+<script setup lang="ts">
+withDefaults(defineProps<{
+  size?: string | number
+}>(), {
+  size: '1.5em',
+})
+</script>
