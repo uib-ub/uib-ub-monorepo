52d7ca06ca5ec55757ebe1c26af54f7df2304860
Henrik Askjer <Henrik.Askjer@uib.no>
2025-11-27 13:50:00 +0100

    cleanup

--- apps/sps-stadnamn-web/src/app/layout.tsx
+++ apps/sps-stadnamn-web/src/app/layout.tsx
@@ -1,19 +1,12 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Menu from "./menu";
-import NavBar from "./nav-bar";
 import { Source_Sans_3, Source_Serif_4 } from 'next/font/google'
-import { Suspense } from "react";
 import PlausibleProvider from 'next-plausible'
 import { userAgent } from "next/server";
 import { headers } from "next/headers";
-import SearchForm from "@/components/search/form/search-form";
 import GlobalProvider from "../state/providers/global-provider";
 import { fetchVocab } from "./api/_utils/actions";
 import QueryProvider from "@/state/providers/query-provider";
-import Link from "next/link";
-import SearchTitle from "@/components/layout/search-title";
-import ModeSelector from "@/components/tabs/mode-selector";
  
 const serif = Source_Serif_4({
   subsets: ['latin'],
