13f82c2ce5b7c9da9a6f5023309b2c59dec6916f
Henrik Askjer <Henrik.Askjer@uib.no>
2025-11-28 12:51:13 +0100

    update sitemap

--- apps/sps-stadnamn-web/src/app/sitemap.ts
+++ apps/sps-stadnamn-web/src/app/sitemap.ts
@@ -5,32 +5,32 @@ export default function sitemap(): MetadataRoute.Sitemap {
     
     const metaPages = [
         {
-            url: 'https://stadnamnportalen.uib.no/',
+            url: 'https://stadnamn.no/',
             priority: 1
         },
         {
-            url: 'https://stadnamnportalen.uib.no/info',
+            url: 'https://stadnamn.no/info',
             priority: 1
         },
         {
-            url: 'https://stadnamnportalen.uib.no/help',
+            url: 'https://stadnamn.no/help',
             priority: 0.9
         },
         {
-            url: 'https://stadnamnportalen.uib.no/feedback',
+            url: 'https://stadnamn.no/feedback',
             priority: 0.9
         },
 
         {
-            url: 'https://stadnamnportalen.uib.no/datasets',
+            url: 'https://stadnamn.no/datasets',
             priority: 0.9
         }
     ]
     
     const datasets = Object.keys(datasetTitles).map((dataset) => {
         return {
-            url: `https://stadnamnportalen.uib.no/view/${dataset}/info`,
-            priority: dataset == 'search' ? 1 : dataset.includes('_') ? 0.5 : 0.7,
+            url: `https://stadnamn.no/info/datasets/${dataset}`,
+            priority: ["ssr", "ssr2016", "geonames", "wikidata"].includes(dataset) ? 0.5 : 0.9,
 
         }
     }
