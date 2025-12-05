c306c0dbe2d68c1ba4bc21ee11d3f2b0e48b8080
Henrik Askjer <Henrik.Askjer@uib.no>
2025-12-05 16:59:09 +0100

    sources => datasets

--- apps/sps-stadnamn-web/src/components/search/details/group/group-info.tsx
+++ apps/sps-stadnamn-web/src/components/search/details/group/group-info.tsx
@@ -237,7 +237,7 @@ const SourcesTab = ({ datasets, isFiltered, isInitGroup }: { datasets: Record<st
                         className="text-lg text-neutral-900 flex items-center gap-1"
                         onClick={() => setShowAll(!showAll)}
                     >
-                        {showAll ? 'Færre kjelder' : `Fleire kjelder (${datasetKeys.length - visibleCount})`}
+                        {showAll ? 'Færre datasett' : `Fleire datasett (+${datasetKeys.length - visibleCount})`}
                     </button>
                 </li>
             )}
