7ddb1a656a72f8b14281cdb68df75144e6a7e274
Tarje Lavik <tarje.lavik@gmail.com>
2025-12-05 15:46:18 +0100

    Remove unused actions.ts file from link shortener application

--- apps/link-shortener/app/actions.ts
+++
@@ -1,9 +0,0 @@
-// app/actions.ts
-"use server";
-import { neon } from "@neondatabase/serverless";
-
-export async function getData() {
-  const sql = neon(process.env.DATABASE_URL as string);
-  const data = await sql`...`;
-  return data;
-}
