e4865113b18876461e471598eb5aaaf1369852e1
Tarje Lavik <tarje.lavik@gmail.com>
2025-12-10 22:49:18 +0100

    Refactor proxy middleware
    - Rename middleware.ts to proxy.ts file that exports middleware from 'nextra/locales'.

--- apps/docs/src/proxy.ts
+++ apps/docs/src/proxy.ts
@@ -1,6 +1,6 @@
 export { middleware } from 'nextra/locales'
 
-export const config = {
+export const proxy = {
   // Matcher ignoring `/_next/` and `/api/`
   matcher: [
     '/((?!api|_next/static|_next/image|favicon.ico|icon.svg|apple-icon.png|manifest|_pagefind).*)'
