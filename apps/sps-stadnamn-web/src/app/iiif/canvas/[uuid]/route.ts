1e84b98d4f44a1f10fd2eef4a7b543c8845a0c8b
Henrik Askjer <Henrik.Askjer@uib.no>
2025-12-15 14:20:43 +0100

    try fixing iiif cors error

--- apps/sps-stadnamn-web/src/app/iiif/canvas/[uuid]/route.ts
+++ apps/sps-stadnamn-web/src/app/iiif/canvas/[uuid]/route.ts
@@ -1,5 +1,17 @@
 import { buildManifest } from "@/app/iiif/iiif-manifest-builder";
 
 export async function GET(request: Request) {
-    return buildManifest(request, 'collection');
+    return buildManifest(request, 'canvas');
+}
+
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Accept, Range, Authorization",
+      "Access-Control-Max-Age": "86400",
+    },
+  })
 }
