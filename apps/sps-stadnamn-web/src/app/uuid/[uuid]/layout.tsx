a899fdbe5684fc19063ebb35168ccfe8f8258516
Henrik Askjer <Henrik.Askjer@uib.no>
2026-02-12 13:26:39 +0100

    faktaark => infoside

--- apps/sps-stadnamn-web/src/app/uuid/[uuid]/layout.tsx
+++ apps/sps-stadnamn-web/src/app/uuid/[uuid]/layout.tsx
@@ -2,7 +2,7 @@ import ContentLayout from '@/components/layout/content-layout'
 
 export default function Page({ children }: { children: React.ReactNode }) {
   return (
-    <ContentLayout name="Faktaark">
+    <ContentLayout name="Infoside">
       {children}
     </ContentLayout>
   )
