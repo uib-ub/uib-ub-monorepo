b40b7e4737b9a6fe839d11c500e202f1df6975e2
Henrik Askjer <Henrik.Askjer@uib.no>
2026-03-16 17:28:11 +0100

    don't show filter button when in grouped mode

--- apps/sps-stadnamn-web/src/components/map/map-toolbar.tsx
+++ apps/sps-stadnamn-web/src/components/map/map-toolbar.tsx
@@ -154,7 +154,7 @@ export default function MapToolbar() {
                 )}
 
                 
-                {isMobile && (
+                {isMobile && sourceView && (
                     <FilterButton />
                 )}
             </div>
