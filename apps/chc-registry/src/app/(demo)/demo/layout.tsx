966ea3ab4acda4d04be194fef4b630eec368e907
Tarje Lavik <tarje.lavik@gmail.com>
2026-05-05 13:23:30 +0200

    Remove use of local font

--- apps/chc-registry/src/app/(demo)/demo/layout.tsx
+++ apps/chc-registry/src/app/(demo)/demo/layout.tsx
@@ -1,7 +1,6 @@
 
 import type { Metadata } from 'next';
 import { Open_Sans, EB_Garamond } from 'next/font/google';
-import localFont from 'next/font/local'
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import { CloverI18nProvider } from '@/lib/clover-i18n';
 import { PaletteProvider } from '@/components/theme/palette-provider';
@@ -9,26 +8,6 @@ import { TailwindIndicator } from '@/components/tailwind-indicator';
 import { PaletteSwitcher } from '@/components/theme/palette-switcher';
 
 import "../../(fuma)/globals.css";
-
-const myriadPro = localFont({
-  src: [
-    {
-      path: '../../../../public/fonts/MyriadPro/MyriadPro-Regular.otf',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../../../../public/fonts/MyriadPro/MyriadPro-It.otf',
-      weight: '400',
-      style: 'italic',
-    },
-    {
-      path: '../../../../public/fonts/MyriadPro/MyriadPro-SemiBold.otf',
-      weight: '600',
-      style: 'normal',
-    }
-  ],
-})
 
 const openSans = Open_Sans({
   variable: '--font-open-sans',
@@ -49,7 +28,7 @@ export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html lang='en' suppressHydrationWarning>
       <body
-        className={`${myriadPro.className} ${openSans.className} ${garamond.className}  antialiased min-h-screen @container/body`}
+        className={`${openSans.variable} ${garamond.variable} antialiased min-h-screen @container/body`}
       >
         <CloverI18nProvider />
         <PaletteProvider>
